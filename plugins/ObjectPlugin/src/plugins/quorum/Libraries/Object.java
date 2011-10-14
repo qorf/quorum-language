7b1ab04333fe50d5b9d3f4af6a6e12ef2c4fea83
Andreas Stefik <stefika@gmail.com>
2011-10-14 20:58:27 +0000

    Added a new plugin for Libraries.Language.Object. There is an additional plugin here in a class called Stefik, here, but that's just for testing.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@126 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

---
+++ plugins/ObjectPlugin/src/plugins/quorum/Libraries/Object.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plugins.quorum.Libraries;
+
+/**
+ * 
+ * @author Andreas Stefik
+ */
+public class Object {
+    public java.lang.Object ___$$$Calling___$$$___Object$$$___ = null;
+    public int GetHashCode() {
+        return ___$$$Calling___$$$___Object$$$___.hashCode();
+    }
+}
