a542b0bb170dc5fd2d1d4db7a93eb652d64653af
Andreas Stefik <stefika@gmail.com>
2011-11-02 21:14:46 +0000

    Fixed a bug in the Object plugin.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@211 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- plugins/ObjectPlugin/src/plugins/quorum/Libraries/Language/Object.java
+++ plugins/ObjectPlugin/src/plugins/quorum/Libraries/Language/Object.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package plugins.quorum.Libraries;
+package plugins.quorum.Libraries.Language;
 
 /**
  * 
