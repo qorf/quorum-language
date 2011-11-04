783b35371e0292ca964d78d8f2e4d64b7e797c16
Jeff Wilson <jeff.adams.wilson@gmail.com>
2011-11-04 16:03:50 +0000

    test cases for fields and public/private modifiers (bytecode)
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@220 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- quorum/test/org/quorum/tests/compiler/publicprivate/PublicPrivateTester.java
+++ quorum/test/org/quorum/tests/compiler/publicprivate/PublicPrivateTester.java
@@ -5,6 +5,7 @@
 
 package org.quorum.tests.compiler.publicprivate;
 
+import org.quorum.execution.RunResult;
 import java.io.File;
 import org.quorum.execution.ExpressionValue;
 import org.junit.Test;
@@ -46,7 +47,7 @@ public class PublicPrivateTester {
     }
 
     @Test
-    public void test_simple_public_action(){
+    public void test_simple_public_action_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SimplePublicAction.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -60,6 +61,49 @@ public class PublicPrivateTester {
         }
     }
 
+    @Test
+    public void test_simple_public_action_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SimplePublicAction.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("555"));
+    }
+    
+    @Test
+    public void test_simple_private_action_execute(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SimplePrivateAction.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+
+        ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("i");
+        int a = variableValue.getResult().integer;
+        if(a != 555) {
+            fail();
+        }
+    }
+
+    @Test
+    public void test_simple_private_action_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SimplePrivateAction.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("555"));
+    }
+    
     @Test
     public void test_set_public_variable_from_child_execute(){
         File[] files = new File[3];
@@ -77,6 +121,25 @@ public class PublicPrivateTester {
         if(b!=2) {
             fail();
         }
+    }
+    
+    @Test
+    public void test_set_public_variable_from_child_bytecode(){
+        File[] files = new File[3];
+        files[2] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "APublicVariable.quorum");
+        files[1] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "BPublicVariable.quorum");
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPublicVariableFromChild.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("2"));
     }
 
     @Test
@@ -99,6 +162,25 @@ public class PublicPrivateTester {
     }
 
     @Test
+    public void test_set_private_variable_from_child_bytecode(){
+        File[] files = new File[3];
+        files[2] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "APrivateVariable.quorum");
+        files[1] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "BPrivateVariable.quorum");
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPrivateVariableFromChild.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("2"));
+    }
+    
+    @Test
     public void test_set_private_variable_Internally_execute(){
         File[] files = new File[2];
         files[1] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "APrivateVariable.quorum");
@@ -116,6 +198,24 @@ public class PublicPrivateTester {
         }
     }
 
+    @Test
+    public void test_set_private_variable_Internally_bytecode(){
+        File[] files = new File[2];
+        files[1] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "APrivateVariable.quorum");
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPrivateVariableInternally.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("2"));
+    }
+    
     @Test
     public void test_set_public_variable_externally_execute(){
         File[] files = new File[2];
@@ -135,7 +235,25 @@ public class PublicPrivateTester {
     }
 
     @Test
-    public void test_set_public_variable_internally(){
+    public void test_set_public_variable_externally_bytecode(){
+        File[] files = new File[2];
+        files[1] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "APublicVariable.quorum");
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPublicVariableExternally.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("2"));
+    }
+    
+    @Test
+    public void test_set_public_variable_internally_execute(){
         File[] files = new File[2];
         files[1] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "APublicVariable.quorum");
         files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPublicVariableInternally.quorum");
@@ -151,9 +269,242 @@ public class PublicPrivateTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_set_public_variable_internally_bytecode(){
+        File[] files = new File[2];
+        files[1] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "APublicVariable.quorum");
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPublicVariableInternally.quorum");
 
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("2"));
+    }
+
+    @Test
+    public void test_set_public_variable_inherited_execute(){
+        File[] files = new File[3];
+        files[2] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPublicInheritedHelper.quorum");
+        files[1] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "AWithPublicOnly.quorum");
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPublicInherited.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("value");
+        int b = variableValue.getResult().integer;
+        if(b!=2) {
+            fail();
+        }
+    }
+    
+    @Test
+    public void test_set_public_variable_inherited_bytecode(){
+        File[] files = new File[3];
+        files[2] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPublicInheritedHelper.quorum");
+        files[1] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "AWithPublicOnly.quorum");
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPublicInherited.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("2"));
+    }
+    
+    @Test
+    public void test_set_private_variable_inherited_execute(){
+        File[] files = new File[3];
+        files[2] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPrivateInheritedHelper.quorum");
+        files[1] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "BWithPrivateOnly.quorum");
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPrivateInherited.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("value");
+        int b = variableValue.getResult().integer;
+        if(b!=2) {
+            fail();
+        }
+    }
+    
+    @Test
+    public void test_set_private_variable_inherited_bytecode(){
+        File[] files = new File[3];
+        files[2] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPrivateInheritedHelper.quorum");
+        files[1] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "BWithPrivateOnly.quorum");
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SetPrivateInherited.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("2"));
+    }
+    
+    @Test
+    public void test_simple_set_field_execute() {
+        File[] files = new File[1];
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SimpleSetField.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("k");
+        int k = variableValue.getResult().integer;
+        if(k!=2) {
+            fail();
+        }        
+    }
+    
+    @Test
+    public void test_simple_set_field_bytecode() {
+        File[] files = new File[1];
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SimpleSetField.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("2"));
+    }
+    
+    @Test
+    public void test_simple_set_field_public_execute() {
+        File[] files = new File[1];
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SimpleSetFieldPublic.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("k");
+        int k = variableValue.getResult().integer;
+        if(k!=2) {
+            fail();
+        }        
+    }
+    
+    @Test
+    public void test_simple_set_field_public_bytecode() {
+        File[] files = new File[1];
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "SimpleSetFieldPublic.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("2"));
+    }
+    
+    @Test
+    public void test_assign_field_private_action_result_execute() {
+        File[] files = new File[1];
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "AssignFieldPrivateActionResult.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("k");
+        int k = variableValue.getResult().integer;
+        if(k!=2) {
+            fail();
+        }        
+    }
+    
+    @Test
+    public void test_assign_field_private_action_result_bytecode() {
+        File[] files = new File[1];
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "AssignFieldPrivateActionResult.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("2"));
+    }
+    
+    @Test
+    public void test_assign_field_public_action_result_execute() {
+        File[] files = new File[1];
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "AssignFieldPublicActionResult.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("k");
+        int k = variableValue.getResult().integer;
+        if(k!=2) {
+            fail();
+        }        
+    }
+    
+    @Test
+    public void test_assign_field_public_action_result_bytecode() {
+        File[] files = new File[1];
+        files[0] = CompilerTestSuite.getQuorumFile(CompilerTestSuite.PUBLIC_PRIVATE + CompilerTestSuite.PASS + "AssignFieldPublicActionResult.quorum");
+
+        CompilerTestSuite.build(files);
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("2"));
+    }
+    
     /***************************************************************************
      * Fail tests
+     * 
+     * Unless the test actually executes (and doesn't raise a compiler error),
+     * there is no need to create bytecode tests for these.
      **************************************************************************/
     @Test
     public void test_set_private_variable_externally_execute(){
