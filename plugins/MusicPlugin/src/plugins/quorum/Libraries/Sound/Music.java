0f25579de52d9141c89e88e8824975e1a951d6a2
Jeff Wilson <jeff.adams.wilson@gmail.com>
2011-11-17 19:09:14 +0000

    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@272 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

---
+++ plugins/MusicPlugin/src/plugins/quorum/Libraries/Sound/Music.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plugins.quorum.Libraries.Sound;
+
+/**
+ *
+ * @author jeff
+ */
+public class Music {
+    public java.lang.Object ___$$$Calling___$$$___Object$$$___ = null;
+    public void  AddNoteForChord(int note) { }
+    public void  PlayChord(double length, double volume, double lengthInSeconds) { }
+    public void  StartSong() { }
+    public void  PlayMidiNative(String path) { }
+    public void  StartTrack(int instrument) { }
+    public void  AddNoteToTrack(int note, double volume, int constantBend, int preBend, int preBendLength, double onPos, double offPos) { }
+    public void  PlaySongNative() { }
+    public int  GetTempoNative() { return 0; }
+    public void  SetTempoNative(int beatsPerMinute) { }
+    public String  GetCurrentInstrumentName() { return null; }
+    public int  GetCurrentInstrumentdouble() { return 0; }
+    public void  SetCurrentInstrument(int index) { }
+    public String  GetInstrumentName(int index) { return null; }
+    public void  StopNative()  { }
+}
