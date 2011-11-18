7d778b27f226f1521d39151f63d10c68d52e6bdd
Andreas Stefik <stefika@gmail.com>
2011-11-18 16:27:20 +0000

    Added in a plugin for Libraries.Language.Types.Text
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@279 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

---
+++ plugins/TextPlugin/src/plugins/quorum/Libraries/Language/Types/Text.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plugins.quorum.Libraries.Language.Types;
+
+/**
+ *
+ * @author astefik
+ */
+public class Text {
+    public java.lang.Object ___$$$Calling___$$$___Object$$$___ = null;
+    private String text = "";
+    
+    public void SetValueNative(String value) {
+        text = value;
+    }
+    
+    public String GetCharacterNative(int index) {
+        String ret = "" + text.charAt(index);
+        return ret;
+    }
+    
+    public String GetSubstringNative(int startIndex, int endIndex) {
+        return text.substring(startIndex, endIndex);
+    }
+    
+    public int CompareInt(String left, String right) {
+        return left.compareTo(right);
+    }
+    
+    public int GetLength() {
+        return text.length();
+    }
+}
