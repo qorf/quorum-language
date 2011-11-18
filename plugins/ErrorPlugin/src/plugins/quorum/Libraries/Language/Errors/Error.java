9638a43407e5d81e800e99344e0290a7788fee5c
Andreas Stefik <stefika@gmail.com>
2011-11-18 16:44:38 +0000

    Added in a stub class for Error.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@283 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

---
+++ plugins/ErrorPlugin/src/plugins/quorum/Libraries/Language/Errors/Error.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plugins.quorum.Libraries.Language.Errors;
+
+/**
+ *
+ * @author Andreas Stefik
+ */
+public class Error {
+    public java.lang.Object ___$$$Calling___$$$___Object$$$___ = null;
+    
+    public void SystemSetMessage(String message) {
+        
+    }
+    
+    public String GetClassName(int index) {
+        return null;
+    }
+    
+    public String GetMethodName(int index) {
+        return null;
+    }
+    
+    public String GetFileName(int index) {
+        return null;
+    }
+    
+    public int GetLineNumber(int index) {
+        return 0;
+    }
+    
+    public void InitStackTraceFromError() {
+        
+    }
+    
+    public String InitMessage() {
+        return null;
+    }
+    
+    public int GetStackSize() {
+        return 0;
+    }
+}
