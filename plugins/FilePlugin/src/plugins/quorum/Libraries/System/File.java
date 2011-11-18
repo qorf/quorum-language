1e5fb1202c6a4c3228dc1f14b0c09033a4209759
Jeff Wilson <jeff.adams.wilson@gmail.com>
2011-11-18 16:55:27 +0000

    work on file plugin - currently stubs
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@285 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

---
+++ plugins/FilePlugin/src/plugins/quorum/Libraries/System/File.java
@@ -0,0 +1,135 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plugins.quorum.Libraries.System;
+
+/**
+ *
+ * @author jeff
+ */
+public class File {
+    public java.lang.Object ___$$$Calling___$$$___Object$$$___ = null;
+    
+    /* -- class methods -- */
+    public double GetLastModifiedNative(String path) {
+        return 0;
+    }
+    
+    public String GetDirectoryListingNative(String path) {
+        return null;
+    }
+    
+    public int GetMode() {
+        return 0;
+    }
+    
+    public boolean IsDirectory(String path) {
+        return false;
+    }
+    
+    public boolean IsHidden(String pah) {
+        return false;
+    }
+    
+    public boolean Exists(String path) {
+        return false;
+    }
+    
+    public String GetName(String path) {
+        return null;
+    }
+    
+    public String GetExtension(String path) {
+        return null;
+    }
+    
+    public String GetParent(String path) {
+        return null;
+    }
+    
+    public String GetURI(String path) {
+        return null;
+    }
+    
+    public double GetFileSize(String path) {
+        return 0;
+    }
+    
+    public double GetFreeSpace(String path) {
+        return 0;
+    }
+    
+    public double GetTotalDiskSpace(String path) {
+        return 0;
+    }
+    
+    public boolean CreateDirectory(String path) {
+        return false;
+    }
+    
+    public boolean Delete(String path) {
+        return false;
+    }
+    
+    public boolean Move(String oldPath, String newPath) {
+        return false;
+    }
+    
+    public String GetSystemNewline() {
+        return null;
+    }
+    
+    
+    /* -- instance methods -- */
+    public void OpenNative(String path, int mode, boolean append, boolean write) {
+        
+    }
+    
+    public boolean IsEndOfFile() {
+        return false;
+    }
+    
+    public double GetPosition() {
+        return 0;
+    }
+    
+    public void SetPosition(double position) {
+        
+    }
+    
+    public String Read() {
+        return null;
+    }
+    
+    public String Read(int amount) {
+        return null;
+    }
+    
+    public String ReadLine() {
+        return null;
+    }
+    
+    public void Write(String StringToWrite) {
+        
+    }
+    
+    public void WriteLine(String StringToWrite) {
+        
+    }
+    
+    public void Rewind() {
+        
+    }
+    
+    public void ForceWriteContents() {
+        
+    }
+    
+    public boolean Close() {
+        return false;
+    }
+    
+
+
+}
