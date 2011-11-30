f866ae74710041455989940ccfd8c2480c0ff147
Jeff Wilson <jeff.adams.wilson@gmail.com>
2011-11-30 16:45:40 +0000

    math tests completed.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@320 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- quorum/test/org/quorum/tests/compiler/Math/MathTester.java
+++ quorum/test/org/quorum/tests/compiler/Math/MathTester.java
@@ -5,6 +5,7 @@
 
 package org.quorum.tests.compiler.Math;
 
+import org.quorum.execution.RunResult;
 import org.quorum.execution.ExpressionValue;
 import org.junit.Test;
 import org.junit.After;
@@ -44,7 +45,7 @@ public class MathTester {
     }
 
     @Test
-    public void test_AbsoluteValue_pass(){
+    public void test_AbsoluteValue_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "AbsoluteValue.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -58,11 +59,25 @@ public class MathTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_AbsoluteValue_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "AbsoluteValue.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("1.0"));
+    }
 
     // author James Daniel
 
-     @Test
-    public void test_Cosine_pass(){
+    @Test
+    public void test_Cosine_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "Cosine.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -78,7 +93,21 @@ public class MathTester {
     }
 
     @Test
-    public void test_HyperbolicCosine_pass(){
+    public void test_Cosine_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "Cosine.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("0.87758"));
+    }
+     
+    @Test
+    public void test_HyperbolicCosine_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "HyperbolicCosine.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -94,7 +123,21 @@ public class MathTester {
     }
 
     @Test
-    public void test_HyperbolicSine_pass(){
+    public void test_HyperbolicCosine_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "HyperbolicCosine.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("1.12763"));
+    }
+    
+    @Test
+    public void test_HyperbolicSine_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "HyperbolicSine.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -110,7 +153,21 @@ public class MathTester {
     }
 
     @Test
-    public void test_HyperbolicTangent_pass(){
+    public void test_HyperbolicSine_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "HyperbolicSine.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("0.5211"));
+    }
+    
+    @Test
+    public void test_HyperbolicTangent_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "HyperbolicTangent.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -126,7 +183,21 @@ public class MathTester {
     }
 
     @Test
-    public void test_InverseCosine_pass(){
+    public void test_HyperbolicTangent_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "HyperbolicTangent.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("0.46212"));
+    }
+    
+    @Test
+    public void test_InverseCosine_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "InverseCosine.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -142,7 +213,21 @@ public class MathTester {
     }
 
     @Test
-    public void test_InverseHyperbolicCosine_pass(){
+    public void test_InverseCosine_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "InverseCosine.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("1.0472"));
+    }
+    
+    @Test
+    public void test_InverseHyperbolicCosine_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "InverseHyperbolicCosine.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -156,9 +241,23 @@ public class MathTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_InverseHyperbolicCosine_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "InverseHyperbolicCosine.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("1.31696"));
+    }
 
     @Test
-    public void test_InverseHyperbolicSine_pass(){
+    public void test_InverseHyperbolicSine_pass_execute() {
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "InverseHyperbolicSine.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -172,9 +271,23 @@ public class MathTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_InverseHyperbolicSine_pass_bytecode() {
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "InverseHyperbolicSine.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("0.88137"));
+    }
 
     @Test
-    public void test_InverseHyperbolicTangent_pass(){
+    public void test_InverseHyperbolicTangent_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "InverseHyperbolicTangent.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -188,9 +301,23 @@ public class MathTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_InverseHyperbolicTangent_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "InverseHyperbolicTangent.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("0.54931"));
+    }
 
     @Test
-    public void test_InverseSine_pass(){
+    public void test_InverseSine_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "InverseSine.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -204,9 +331,23 @@ public class MathTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_InverseSine_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "InverseSine.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("0.5236"));
+    }
 
     @Test
-    public void test_InverseTangent_pass(){
+    public void test_InverseTangent_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "InverseTangent.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -220,9 +361,23 @@ public class MathTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_InverseTangent_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "InverseTangent.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("0.46365"));
+    }
 
     @Test
-    public void test_Logarithm_pass(){
+    public void test_Logarithm_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "Logarithm.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -236,9 +391,23 @@ public class MathTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_Logarithm_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "Logarithm.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("0.69897"));
+    }
 
     @Test
-    public void test_NaturalLogarithm_pass(){
+    public void test_NaturalLogarithm_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "NaturalLogarithm.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -252,9 +421,23 @@ public class MathTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_NaturalLogarithm_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "NaturalLogarithm.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("1.60944"));
+    }
 
     @Test
-    public void test_Sine_pass(){
+    public void test_Sine_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "Sine.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -268,9 +451,23 @@ public class MathTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_Sine_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "Sine.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("0.47943"));
+    }
 
     @Test
-    public void test_SquareRoot_pass(){
+    public void test_SquareRoot_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "SquareRoot.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -286,7 +483,21 @@ public class MathTester {
     }
 
     @Test
-    public void test_Tangent_pass(){
+    public void test_SquareRoot_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "SquareRoot.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("3.0"));
+    }
+    
+    @Test
+    public void test_Tangent_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "Tangent.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -300,9 +511,23 @@ public class MathTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_Tangent_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "Tangent.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("0.5463"));
+    }
 
     @Test
-    public void test_Power_pass(){
+    public void test_Power_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "RaiseToPower.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -316,9 +541,22 @@ public class MathTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_Power_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "RaiseToPower.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("9.0"));
+    }
 
     @Test
-    public void test_Round_pass(){
+    public void test_Round_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "RoundUp.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -332,9 +570,22 @@ public class MathTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_Round_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "RoundUp.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("3.0"));
+    }
 
-   @Test
-    public void test_roundToDecimal_pass(){
+    @Test
+    public void test_roundToDecimal_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "RoundUpToDecimal.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -348,9 +599,22 @@ public class MathTester {
             fail();
         }
     }
+    
+    @Test
+    public void test_roundToDecimal_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "RoundUpToDecimal.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("3.0"));
+    }
 
     @Test
-    public void test_roundDown_pass(){
+    public void test_roundDown_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "RoundDown.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -366,7 +630,20 @@ public class MathTester {
     }   
    
     @Test
-    public void test_roundDownToDecimal_pass(){
+    public void test_roundDown_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "RoundDown.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("3.0"));
+    }
+    
+    @Test
+    public void test_roundDownToDecimal_pass_execute(){
         CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "RoundDownToDecimal.quorum"));
         if (!vm.getCompilerErrors().isCompilationErrorFree()){
             fail();
@@ -379,5 +656,18 @@ public class MathTester {
         {
             fail();
         }
+    }
+    
+    @Test
+    public void test_roundDownToDecimal_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.MATH + CompilerTestSuite.PASS + "RoundDownToDecimal.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("3.5"));
     }
 }
