b3455dabde7c6ec511336177e581cb7a821a6699
Jeff Wilson <jeff.adams.wilson@gmail.com>
2011-12-01 17:12:42 +0000

    error plugin partially implemented. stack trace code is there, but some of the other features are incomplete.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@327 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- plugins/ErrorPlugin/src/plugins/quorum/Libraries/Language/Errors/Error.java
+++ plugins/ErrorPlugin/src/plugins/quorum/Libraries/Language/Errors/Error.java
@@ -4,42 +4,99 @@
  */
 package plugins.quorum.Libraries.Language.Errors;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Andreas Stefik
  */
 public class Error {
     public java.lang.Object ___$$$Calling___$$$___Object$$$___ = null;
+    private StackTraceElement[] stackTrace;
     
+    public Error() {
+    }
+    
+    /**
+     * This may not need to actually be implemented, since this is used for
+     * setting data inside the Quorum virtual machine.
+     * 
+     * @param message 
+     */
     public void SystemSetMessage(String message) {
-        
     }
     
     public String GetClassName(int index) {
-        return null;
+        String className;
+        
+        if (index > stackTrace.length) {
+            className = null;
+        }
+        else {
+            className = stackTrace[index].getClassName();
+        }
+        
+        return className;
     }
     
     public String GetMethodName(int index) {
-        return null;
+        String methodName;
+        
+        if (index > stackTrace.length) {
+            methodName = null;
+        }
+        else {
+            methodName = stackTrace[index].getMethodName();
+        }
+        
+        return methodName;
     }
     
     public String GetFileName(int index) {
-        return null;
+        String fileName;
+        
+        if (index > stackTrace.length) {
+            fileName = null;
+        }
+        else {
+            fileName = stackTrace[index].getFileName();
+        }
+        
+        return fileName;
     }
     
     public int GetLineNumber(int index) {
-        return 0;
-    }
-    
-    public void InitStackTraceFromError() {
+        int number;
         
+        if (index > stackTrace.length - 1) {
+            number = -1;
+        }
+        else {
+            number = stackTrace[index].getLineNumber();
+        }
+        
+        return number;
     }
     
+    /**
+     * Get the stack trace.
+     * 
+     * Note: This may be including the "Error" class itself, which we don't want,
+     * so this may need to be modified later to have a certain offset.
+     */
+    public void InitStackTraceFromError() {
+        stackTrace = Thread.currentThread().getStackTrace();
+    }
+    
+    /**
+     * TODO: This should return the last rnuntime error, if possible...
+     * @return 
+     */
     public String InitMessage() {
-        return null;
+        return "An Error has occurred.";
     }
     
     public int GetStackSize() {
-        return 0;
+        return stackTrace.length;
     }
 }
