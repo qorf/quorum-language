a3d04595532cfdec4c0398903da70e4b9c6a9eb0
Andreas Stefik <stefika@gmail.com>
2011-12-16 17:59:57 +0000

    Added documentation for the proposed command line arguments in Quorum.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@373 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- quorum/src/org/quorum/Main.java
+++ quorum/src/org/quorum/Main.java
@@ -21,11 +21,69 @@ import org.quorum.vm.interfaces.CompilerErrorManager;
  * ultimately allow you to pass command line arguments to Quorum, a series
  * of flags, or other attributes to control how you build source.
  * 
+ * 
+ * Command line arguments are as follows:
+ * 
+ * java -jar Quorum.jar [-flag]* [file]*
+ * 
+ * In other words, any number of flags beginning with a dash and then any number
+ * of files. Legal flags are
+ * 
+ * -interpret This causes the VM to run in interpreted mode
+ * -compiled This causes the VM to compile Quorum code to java bytecode. This is the default setting.
+ * -document This causes the VM to compile Quorum code normally, but instead of outputting
+ *          bytecode or interpreting the code, it outputs documentation for the code.
+ * -name [String] This sets the name which is output for the corresponding distribution files.
+ *                  An example might be -name Music. This would cause Quorum to output a 
+ *                  jar file by the name of Music.jar into the folder distribute.
+ * -help This causes help to be output to the command line.
+ * 
+ * After entering any flags desired, a list of files indicates to the compiler
+ * which files to interpret or compile. 
+ * 
+ * Examples:
+ * 
+ * The following would run one file, Main.quorum, in interpreted mode.
+ * java -jar Quorum.jar -interpret Main.quorum
+ * 
+ * The following would compile one file, Main.quorum, down to java bytecode. This
+ * file would put class files in the build folder and the file Main.jar into the 
+ * distribute folder.
+ * java -jar Quorum.jar -name Main Main.quorum
+ * 
+ * As -compiled is the default setting, this does exactly the same thing as the 
+ * previous example.
+ * java -jar Quorum.jar -compiled -name Main Main.quorum
+ * 
+ * This also does the same thing as the previous example, except since no 
+ * -name flag is passed, the distribute folder will contain a file called 
+ * 
+ * In this example, Quorum will compile Main.quorum and Test.quorum to Java bytecode,
+ * giving the distribution the name Default.jar.
+ * java -jar Quorum.jar Main.quorum Test.quorum
+ * 
+ * Instead of compiling, this command causes Quorum to output documentation
+ * in wiki-style format.
+ * java -jar Quorum.jar -document Main.quorum
+ * 
  * @author Andreas Stefik
  */
 public class Main {
-
+    /**
+     * This is the virtual machine that compiles Quorum programs.
+     */
     private static QuorumVirtualMachine vm;
+    
+    /**
+     * If this flag is true, any files passed should be compiled internally
+     * and executed in interpreted mode.
+     */
+    private static boolean interpret = false;
+    
+    /**
+     * This is the name given to the distribution that Quorum will output.
+     */
+    private static String name = "Default.jar";
     
     /**
      * @param args the command line arguments
@@ -123,7 +181,7 @@ public class Main {
             }
         }
         else {
-            vm.build("say \"Hello, world!\"");
+            vm.build("say \"Please pass the files you would to compile on the command line. For example, you might type java -jar Quorum.jar Main.quorum.\"");
             vm.run();
         }
     }
