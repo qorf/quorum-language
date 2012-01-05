8891f1324fa904d707ec889e85fb590e759d7379
Andreas Stefik <stefika@gmail.com>
2012-01-05 17:05:29 +0000

    A tiny amount of work on the command line arguments.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@376 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- quorum/src/org/quorum/Main.java
+++ quorum/src/org/quorum/Main.java
@@ -27,10 +27,10 @@ import org.quorum.vm.interfaces.CompilerErrorManager;
  * java -jar Quorum.jar [-flag]* [file]*
  * 
  * In other words, any number of flags beginning with a dash and then any number
- * of files. Legal flags are
+ * of files. Legal flags are as follows:
  * 
  * -interpret This causes the VM to run in interpreted mode
- * -compiled This causes the VM to compile Quorum code to java bytecode. This is the default setting.
+ * -compile This causes the VM to compile Quorum code to java bytecode. This is the default setting.
  * -document This causes the VM to compile Quorum code normally, but instead of outputting
  *          bytecode or interpreting the code, it outputs documentation for the code.
  * -name [String] This sets the name which is output for the corresponding distribution files.
@@ -53,10 +53,7 @@ import org.quorum.vm.interfaces.CompilerErrorManager;
  * 
  * As -compiled is the default setting, this does exactly the same thing as the 
  * previous example.
- * java -jar Quorum.jar -compiled -name Main Main.quorum
- * 
- * This also does the same thing as the previous example, except since no 
- * -name flag is passed, the distribute folder will contain a file called 
+ * java -jar Quorum.jar -compile -name Main Main.quorum
  * 
  * In this example, Quorum will compile Main.quorum and Test.quorum to Java bytecode,
  * giving the distribution the name Default.jar.
@@ -84,6 +81,51 @@ public class Main {
      * This is the name given to the distribution that Quorum will output.
      */
     private static String name = "Default.jar";
+    
+    /**
+     * A state variable for controlling which command line argument
+     * is currently being processed.
+     */
+    private static int argumentIndex = 0;
+    
+    /**
+     * A command line flag telling the computer to interpret the source.
+     */
+    private static final String INTERPRET = "-interpret";
+    
+    /**
+     * A flag telling the computer to compile down to java bytecode.
+     */
+    private static final String COMPILE = "-compile";
+    
+    /**
+     * A flag telling the computer to output documentation.
+     */
+    private static final String DOCUMENT = "-document";
+    /**
+     * A flag telling the computer the name of the file to distribute. This
+     * flag must be followed a string name, with no file extension. 
+     * 
+     * Example: -name Hello
+     * 
+     * this would output Hello.jar.
+     */
+    private static final String NAME = "-name";
+    
+    /**
+     * Adding in this flag causes all others to be ignored and help
+     * information is output to the command line.
+     */
+    private static final String HELP = "-help";
+    
+    
+    
+    
+    private static boolean isHelp = false;
+    private static boolean isInterpret = false;
+    private static boolean isCompile = false;
+    private static boolean isDocument = false;
+    
     
     /**
      * @param args the command line arguments
@@ -144,6 +186,12 @@ public class Main {
         loader.loadIntoVirtualMachine(vm);
         loader.checkConsistency(vm);
 
+        processFlags(args);
+        
+        if(isHelp) {
+            outputHelp();
+            System.exit(0);
+        }
         //now parse the command line arguments
         //TODO: this will need to be tweaked for command line arguments
         //and for correct file processing.
@@ -185,4 +233,95 @@ public class Main {
             vm.run();
         }
     }
+    
+    private static void outputHelp() {
+        System.out.println("\n\n"+
+  
+                
+" .d88888b.\n"+                                                   
+"d88P\" \"Y88b\n"+                                                  
+"888     888\n"+                                                  
+"888     888 888  888  .d88b.  888d888 888  888 88888b.d88b.\n"+  
+"888     888 888  888 d88\"\"88b 888P\"   888  888 888 \"888 \"88b\n"+ 
+"888 Y8b 888 888  888 888  888 888     888  888 888  888  888\n"+ 
+"Y88b.Y8b88P Y88b 888 Y88..88P 888     Y88b 888 888  888  888\n"+ 
+" \"Y888888\"   \"Y88888  \"Y88P\"  888      \"Y88888 888  888  888\n"+ 
+"       Y8b\n\n\n"+        
+                                                                                         
+
+"Below is the command line documentation for the general purpose programming language Quorum:\n\n"+
+                
+  "java -jar Quorum.jar [-flag]* [file]*\n"+
+  
+  "In other words, any number of flags beginning with a dash and then any number"+
+  "of files. Legal flags are as follows:\n\n"+
+  
+  "-interpret This causes the VM to run in interpreted mode.\n\n"+
+  "-compile This causes the VM to compile Quorum code to java bytecode. This is the default setting.\n\n"+
+  "-document This causes the VM to compile Quorum code normally, but instead of outputting"+
+  " bytecode or interpreting the code, it outputs documentation for the code.\n\n"+
+                
+  "-name [String] This sets the name which is output for the corresponding distribution files."+
+  "An example might be -name Music. This would cause Quorum to output a "+
+  "jar file by the name of Music.jar into the folder distribute.\n\n"+
+                
+  "-help This causes help to be output to the command line.\n\n"+
+  
+  "After entering any flags desired, a list of files indicates to the compiler "+
+  "which files to interpret or compile.\n\n "+
+  
+  "Examples:\n"+
+  
+  "The following would run one file, Main.quorum, in interpreted mode.\n"+
+  "java -jar Quorum.jar -interpret Main.quorum\n\n"+
+  
+  "The following would compile one file, Main.quorum, down to java bytecode. This "+
+  "file would put class files in the build folder and the file Main.jar into the "+
+  "distribute folder.\n"+
+  "java -jar Quorum.jar -name Main Main.quorum\n\n"+
+  
+  "As -compiled is the default setting, this does exactly the same thing as the "+
+  "previous example.\n"+
+  "java -jar Quorum.jar -compile -name Main Main.quorum\n\n"+
+  
+  "In this example, Quorum will compile Main.quorum and Test.quorum to Java bytecode, "+
+  "giving the distribution the name Default.jar.\n"+
+  "java -jar Quorum.jar Main.quorum Test.quorum\n"+
+  
+  "Instead of compiling, this command causes Quorum to output documentation "+
+  "in wiki-style format.\n"+
+  "java -jar Quorum.jar -document Main.quorum"
+       );
+    }
+    
+    private static void processFlags(String[] args) {
+        int index = 0;
+        boolean flagsFinished = false;
+        while(index < args.length && !flagsFinished) {
+            String arg = args[index];
+            if(!arg.startsWith("-")) {
+                argumentIndex = index;
+                return;
+            }
+            else if(arg.compareTo(HELP) == 0){
+                isHelp = true;
+            }
+            else if(arg.compareTo(DOCUMENT) == 0){
+                isDocument = true;
+            }
+            else if(arg.compareTo(INTERPRET) == 0){
+                isInterpret = true;
+            }
+            else if(arg.compareTo(COMPILE) == 0){
+                isCompile = true;
+            }
+            else if(arg.compareTo(NAME) == 0){
+                if((index + 1) < args.length) {
+                    name = args[(index + 1)];
+                }
+            }
+            index++;
+        }
+    }
+    
 }
