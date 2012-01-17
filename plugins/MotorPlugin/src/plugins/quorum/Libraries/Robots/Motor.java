e508c400bf1bea5f2fd3abe8f9be137b25e1fd3f
Jeff Wilson <jeff.adams.wilson@gmail.com>
2012-01-17 20:12:43 +0000

    added motorplugin
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@384 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

---
+++ plugins/MotorPlugin/src/plugins/quorum/Libraries/Robots/Motor.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plugins.quorum.Libraries.Robots;
+
+/**
+ *
+ * @author
+ */
+public class Motor {
+    public java.lang.Object ___$$$Calling___$$$___Object$$$___ = null;
+}
