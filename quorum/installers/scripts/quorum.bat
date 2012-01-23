245cb325a46ee3f58d9f3cc52a2a993922ae4791
Jeff Wilson <jeff.adams.wilson@gmail.com>
2012-01-23 18:16:13 +0000

    added windows quorum run script, quorum.bat
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@409 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

---
+++ quorum/installers/scripts/quorum.bat
@@ -0,0 +1,7 @@
+@echo off
+
+SET QUORUM_INSTALLATION_PATH=C:\Users\jeff\Documents\quorum\trunk\quorum\dist
+
+rem Run Quorum with all of our command line arguments
+
+java -jar "%QUORUM_INSTALLATION_PATH%\Quorum.jar" %*
