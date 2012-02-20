5fb971a8df4cc01023a1ce58bee4983c0a96b9fa
Jeff Wilson <jeff.adams.wilson@gmail.com>
2012-02-20 20:01:38 +0000

    Added demonstration Go() function to MotorPlugin.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@491 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- plugins/MotorPlugin/src/plugins/quorum/Libraries/Robots/Motor.java
+++ plugins/MotorPlugin/src/plugins/quorum/Libraries/Robots/Motor.java
@@ -10,4 +10,19 @@ package plugins.quorum.Libraries.Robots;
  */
 public class Motor {
     public java.lang.Object $me = null;
+    
+    public void Go() {
+        cbccore.motors.Motor m = new cbccore.motors.Motor(0);
+        m.moveAtVelocity(100);
+        m = new cbccore.motors.Motor(1);
+        m.moveAtVelocity(200);
+        m = new cbccore.motors.Motor(2);
+        m.moveAtVelocity(300);
+        m = new cbccore.motors.Motor(3);
+        m.moveAtVelocity(400);
+        try { Thread.sleep(2000l); } catch (Exception e) {}
+        cbccore.motors.Motor.allOff();
+        m.moveRelativePosition(500, 5000);
+        m.waitForDone();
+    }
 }
