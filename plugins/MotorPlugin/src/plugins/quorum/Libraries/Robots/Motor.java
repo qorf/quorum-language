5fbfc012c7f64c5bd42cb22facf6adce367eaadd
Sahana Tambi Sathyanarayana <stambis@siue.edu>
2012-02-22 21:53:34 +0000

    Motor initial implementation
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@499 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- plugins/MotorPlugin/src/plugins/quorum/Libraries/Robots/Motor.java
+++ plugins/MotorPlugin/src/plugins/quorum/Libraries/Robots/Motor.java
@@ -11,8 +11,10 @@ package plugins.quorum.Libraries.Robots;
 public class Motor {
     public java.lang.Object $me = null;
     
+    
     public void Go() {
         cbccore.motors.Motor m = new cbccore.motors.Motor(0);
+        m.backward();
         m.moveAtVelocity(100);
         m = new cbccore.motors.Motor(1);
         m.moveAtVelocity(200);
@@ -26,11 +28,79 @@ public class Motor {
         m.waitForDone();
     }
     
-    public void Move(double speed) {
-        
+    public void SetConnection(int motorInteger)
+    {
+        cbccore.motors.Motor m = new cbccore.motors.Motor(motorInteger);
+    }
+     
+    public void GetConnection()//int motorlocation)
+    {
+        //cbccore.motors.Motor m = new cbccore.motors.Motor(motorlocation);
+        //return m;
     }
     
-    public void Move(double speed, boolean forward) {
-        
+    public void ResetPosition(int motorInteger)
+    {
+        cbccore.motors.Motor m = new cbccore.motors.Motor(motorInteger);
+        m.clearPositionCounter();
+    }
+    
+    public int GetPosition(int motorInteger)
+    {
+        cbccore.motors.Motor m = new cbccore.motors.Motor(motorInteger);
+        return m.getPosition();
+    }
+     
+    public void Move(int motorInteger, int speed, boolean forward)
+    {
+        cbccore.motors.Motor m = new cbccore.motors.Motor(motorInteger);
+        speed = speed * 100;
+        if (forward == true)
+            m.moveAtVelocity(speed);
+        else
+            m.backward();
+    }
+    
+    public void MoveToPosition(double speed, boolean forward)
+    {
+    
+    }
+    
+    public void MoveFromHere(double speed, boolean forward)
+    {
+    
+    }
+    
+    public void Off(int motorInteger)
+    {
+        cbccore.motors.Motor m = new cbccore.motors.Motor(motorInteger);
+        m.off();
+    }
+    
+    public void Off()
+    {
+        cbccore.motors.Motor m = new cbccore.motors.Motor(0);
+        m.off();
+        m = new cbccore.motors.Motor(1);
+        m.off();
+        m = new cbccore.motors.Motor(2);
+        m.off();
+        m = new cbccore.motors.Motor(3);
+        m.off();
+    }
+    
+    
+    public void Wait(int motorInteger)
+    {
+       cbccore.motors.Motor m = new cbccore.motors.Motor(0);
+        m.waitForDone();   
+    }
+    
+    public boolean IsMotionComplete(int motorInteger)
+    {
+     cbccore.motors.Motor m = new cbccore.motors.Motor(motorInteger);
+     boolean sucess;
+     sucess = m.getDone();
+     return sucess;
     }
 }
