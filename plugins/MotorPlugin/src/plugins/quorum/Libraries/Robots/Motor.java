fd096471313dbeedcb4d0bfa44e7d4394cf33c59
Sahana Tambi Sathyanarayana <stambis@siue.edu>
2012-02-23 21:41:43 +0000

    Motor Class complete
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@502 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- plugins/MotorPlugin/src/plugins/quorum/Libraries/Robots/Motor.java
+++ plugins/MotorPlugin/src/plugins/quorum/Libraries/Robots/Motor.java
@@ -14,28 +14,9 @@ public class Motor {
     cbccore.motors.Motor m;
     private int motorInteger;
     private int motorPosition;
-    //boolean onOff;
-    //boolean busy;
-    
-    /*public void Go() {
-        cbccore.motors.Motor m = new cbccore.motors.Motor(0);
-        m.backward();
-        m.moveAtVelocity(100);
-        m = new cbccore.motors.Motor(1);
-        m.moveAtVelocity(200);
-        m = new cbccore.motors.Motor(2);
-        m.moveAtVelocity(300);
-        m = new cbccore.motors.Motor(3);
-        m.moveAtVelocity(400);
-        try { Thread.sleep(2000l); } catch (Exception e) {}
-        cbccore.motors.Motor.allOff();
-        m.moveRelativePosition(500, 5000);
-        m.waitForDone();
-    }*/
     
     public void SetConnection(int motorInt)
     {
-     //   cbccore.motors.Motor m = new cbccore.motors.Motor(motorInt);
         m = new cbccore.motors.Motor(motorInt);
         motorInteger = motorInt;
     }
@@ -47,14 +28,12 @@ public class Motor {
     
     public void ResetPosition()
     {
-        //cbccore.motors.Motor m = new cbccore.motors.Motor(motorInteger);
         m.clearPositionCounter();
         motorPosition = 0;
     }
     
     public int GetPosition()
     {
-       // cbccore.motors.Motor m = new cbccore.motors.Motor(motorInteger);
         return m.getPosition();
     }
      
@@ -83,7 +62,6 @@ public class Motor {
     
     public void Off(int motorInteger)
     {
-        //cbccore.motors.Motor m = new cbccore.motors.Motor(motorInteger);
         m.off();
     }
     
@@ -102,13 +80,11 @@ public class Motor {
     
     public void Wait()
     {
-       //cbccore.motors.Motor m = new cbccore.motors.Motor(motorInteger);
        m.waitForDone();   
     }
     
     public boolean IsMotionComplete()
     {
-        //cbccore.motors.Motor m = new cbccore.motors.Motor(motorInteger);
         boolean sucess;
         sucess = m.getDone();
         return sucess;
