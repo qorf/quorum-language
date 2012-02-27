08c1f324111dc05f3a99bf46343f59b75413921a
Sahana Tambi Sathyanarayana <stambis@siue.edu>
2012-02-27 21:06:32 +0000

    Robot Class modified
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@505 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- plugins/MotorPlugin/src/plugins/quorum/Libraries/Robots/Robot.java
+++ plugins/MotorPlugin/src/plugins/quorum/Libraries/Robots/Robot.java
@@ -14,24 +14,74 @@ public class Robot {
     public java.lang.Object $me = null;
     
     cbccore.create.Create r;
+    cbccore.create.CliffState c;
     //cbccore.create.CreateConnectException e;
     
+    private int rightCliff;
+    private int rightFrontCliff;
+    private int leftCliff;
+    private int leftFrontCliff;
+    private int rightCliffAmount;
+    private int rightFrontCliffAmount;
+    private int leftCliffAmount;
+    private int leftFrontCliffAmount;
+            
     public void SetConnection() throws CreateConnectException
     {
+        r = new cbccore.create.Create();
+        c = new cbccore.create.CliffState(rightCliff, rightFrontCliff, leftCliff, leftFrontCliff, rightCliffAmount, rightFrontCliffAmount, leftCliffAmount, leftFrontCliffAmount);
         r.connect();
-        //throws
         RuntimeException runtimeException = new RuntimeException("Compiled Code");
-        r.driveDirect(250, 250);
+        //r.driveDirect(250, 250);
     }
-    
-    //public void connect() throws CreateConnectException {
-      ///  compiled code
-        //throw new RuntimeException("Compiled Code");
-    //}
     
     public void Disconnect()
     {
         r.disconnect();
     }
+ 
+    public void StopWheel()
+    {
+        r.stop();
+    }
     
+    public void MoveStriaght(int speed)
+    {
+        r.driveStraight(speed);
+    }
+    
+    public void MoveAsCurve(int speed, int radius)
+    {
+        r.drive(speed, radius);
+    }
+    
+    public void RotateClockwise(int speed)
+    {
+        r.spinCW(speed);
+    }
+    
+    public void RotateCounterClockwise(int speed)
+    {
+        r.spinCCW(speed);
+    }
+    
+    public int IsLeftSensorTriggered()
+    {
+        return c.getLeftCliff();
+    }
+    
+    public int IsRightSensorTriggered()
+    {
+        return c.getRightCliff();
+    }
+    
+    public int IsLeftFrontSensorTriggered()
+    {
+        return c.getLeftFrontCliff();
+    }
+    
+    public int IsRightFrontSensorTriggered()
+    {
+        return c.getRightFrontCliff();
+    }
 }
