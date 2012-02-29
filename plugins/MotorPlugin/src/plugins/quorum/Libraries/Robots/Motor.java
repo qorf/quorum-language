5152f38df98348702851d7966a0ac6b24b12e40f
Sahana Tambi Sathyanarayana <stambis@siue.edu>
2012-02-29 22:37:37 +0000

    Motor class updates and testing
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@510 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- plugins/MotorPlugin/src/plugins/quorum/Libraries/Robots/Motor.java
+++ plugins/MotorPlugin/src/plugins/quorum/Libraries/Robots/Motor.java
@@ -13,64 +13,93 @@ public class Motor {
     public java.lang.Object $me = null;
     
     cbccore.motors.Motor m;
-    private int motorInteger;
-    private int motorPosition;
+    private int motorPort;
+    private int motorSpeed = 0;
+    //private int motorPosition;
     
-    public void SetConnection(int motorInt)
+    public void SetPort(int port)
     {
-        m = new cbccore.motors.Motor(motorInt);
-        motorInteger = motorInt;
+        m = new cbccore.motors.Motor(port);
+        motorPort = port;
     }
      
-    public int GetConnection()
+    public int GetPort()
     {
-        return motorInteger;
+        return motorPort;
     }
     
     public void ResetPosition()
     {
         m.clearPositionCounter();
-        motorPosition = 0;
     }
     
     public int GetPosition()
     {
         return m.getPosition();
     }
-     
-    public void Move(int speed, boolean forward)
+    
+    public void SetSpeed(int speed)
     {
-        //int setspeed = m.getDefaultSpeed();
-        //setspeed = setspeed * (speed / 100);
         m.motor(speed);
-        if (forward == true)
-            m.forward();
-        else
-            m.backward();
+        motorSpeed = speed;
     }
     
-    public void MoveToPosition(int speed, int position)
+    public int GetSpeed()
     {
-        //int setspeed = m.getDefaultSpeed();
-        //setspeed = setspeed * (speed / 100);
-        m.moveToPosition(speed, position);
-        motorPosition = position;
+        return motorSpeed;
     }
     
-    public void MoveFromHere(int speed, int position)
+    public void MoveForward()
     {
-        //int setspeed = m.getDefaultSpeed();
-        //setspeed = setspeed * (speed / 100);
-        m.moveToPosition(speed, position);
-        motorPosition = motorPosition + position;
+        m.forward();
+    }
+    
+    public void MoveForward(int speed)
+    {
+        m.motor(speed);
+        m.forward();
+    }
+    
+    public void MoveBackward()
+    {
+        m.backward();
+    }
+    
+    public void MoveBackward(int speed)
+    {
+        m.motor(speed);
+        m.backward();
+    }
+    
+    public void MoveToPosition(int position)
+    {
+        m.moveToPosition(motorSpeed, position);
+    }
+    
+    public void MoveToPosition(int position, int speed)
+    {
+        int setspeed = m.getDefaultSpeed();
+        setspeed = 2 * setspeed * (speed / 100);
+        m.moveToPosition(setspeed, position);
+    }
+    
+    public void MoveFromHere(int position)
+    {
+        m.moveRelativePosition(motorSpeed, position);
+    }
+    
+    public void MoveFromHere(int position, int speed)
+    {
+        int setspeed = m.getDefaultSpeed();
+        setspeed = 2 * setspeed * (speed / 100);
+        m.moveRelativePosition(speed, position);
     }
     
     public void Off()
     {
         m.off();
     }
-    
-    
+   
     public void Wait()
     {
        m.waitForDone();   
@@ -96,4 +125,14 @@ public class Motor {
         m = new cbccore.motors.Motor(3);
         m.off();
     }*/
-    
+
+    /*public void Move(int speed, boolean forward)
+    {
+        //int setspeed = m.getDefaultSpeed();
+        //setspeed = setspeed * (speed / 100);
+        m.motor(speed);
+        if (forward == true)
+            m.forward();
+        else
+            m.backward();
+    }*/
