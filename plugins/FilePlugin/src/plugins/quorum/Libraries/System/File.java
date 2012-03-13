346daeabfe69202264d265bd3293ff5b3279c157
Jeff Wilson <jeff.adams.wilson@gmail.com>
2012-03-13 20:01:55 +0000

    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@527 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- plugins/FilePlugin/src/plugins/quorum/Libraries/System/File.java
+++ plugins/FilePlugin/src/plugins/quorum/Libraries/System/File.java
@@ -11,34 +11,48 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Java bytecode plugin for the "File" class.
+ * Java bytecode plugin for the "File" class. This is merely a wrapper for the
+ * QuorumFilePlugin class.
  * 
- * TODO: How do we re-throw exceptions to be the Quorum type? For example,
- * an IllegalArgumentException has a different name in Quorum...
- * 
- * @author jeff
+ * @author Jeff Wilson
  */
 public class File {
     public java.lang.Object $me = null;
-    private QuorumFile inst = new QuorumFile();
+    private QuorumFilePlugin inst = new QuorumFilePlugin();
     
-    /* -- class methods -- */
-    public double GetLastModifiedNative() {
-        try {
-            return inst.GetLastModified();
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        return -1;
+    public long GetLastModifiedNative() {
+        return inst.GetLastModifiedNative();
     }
     
-    public String GetDirectoryListingNative() {
-        return inst.GetDirectoryListing();
+    public java.lang.String GetDirectoryListingNative() {
+        return inst.GetDirectoryListingNative();
+    }
+    public java.lang.String GetParentDirectoryNative() {
+        return inst.GetParentDirectoryNative();
     }
     
-    public int GetMode() {
-        return inst.GetMode();
+    public String GetPathNative() {
+        return inst.GetPathNative();
+    }
+    
+    public void SetPathNative(String newPath) {
+        inst.SetPathNative(newPath);
+    }
+    
+    public String GetWorkingDirectoryNative() {
+        return inst.GetWorkingDirectoryNative();
+    }
+    
+    public String GetSystemNewline() {
+        return inst.GetSystemNewline();
+    }
+    
+    public boolean Exists() {
+        return inst.Exists();
+    }
+    
+    public boolean IsFile() {
+        return inst.IsFile();
     }
     
     public boolean IsDirectory() {
@@ -49,174 +63,35 @@ public class File {
         return inst.IsHidden();
     }
     
-    public boolean Exists() {
-        return inst.Exists();
+    public String GetFileName() {
+        return inst.GetFileName();
     }
     
-    public String GetName() {
-        return inst.GetName();
+    public String GetFileExtension() {
+        return inst.GetFileExtension();
     }
     
-    public String GetExtension() {
-        return inst.GetExtension();
+    public double GetFreeDiskSpace() {
+        return inst.GetFreeDiskSpace();
     }
     
-    public String GetParent() {
-        return inst.GetParent();
-    }
-    
-    public String GetURI() {
-        return inst.GetURI();
+    public double GetTotalDiskSpace() {
+        return inst.GetTotalDiskSpace();
     }
     
     public double GetFileSize() {
         return inst.GetFileSize();
     }
     
-    public double GetFreeSpace() {
-        return inst.GetFreeSpace();
-    }
-    
-    public double GetTotalSpace() {
-        return inst.GetTotalSpace();
+    public boolean Delete() {
+        return inst.Delete();
     }
     
     public boolean CreateDirectory() {
         return inst.CreateDirectory();
     }
     
-    public boolean Delete() {
-        return inst.Delete();
-    }
-    
     public boolean Move(String newPath) {
         return inst.Move(newPath);
-    }
-    
-    public String GetSystemNewline() {
-        return System.getProperty("line.separator");
-    }
-    
-    public void SetPath(String path) {
-        inst.SetPath(path);
-    }
-    
-    public String GetPath() {
-        return inst.GetPath();
-    }
-    
-    /* -- instance methods -- */
-    public void OpenNative(int mode, boolean append, boolean write) {
-        try {
-            inst.Open(mode, append, write);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalArgumentException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public boolean IsEndOfFile() {
-        return inst.IsEOF();
-    }
-    
-    public double GetPosition() {
-        try {
-            return inst.GetPosition();
-        } catch (IOException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        return -1;
-    }
-    
-    public void SetPosition(double position) {
-        try {
-            inst.SetPosition((long)position);
-        } catch (IOException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalArgumentException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public String Read() {
-        try {
-            return inst.Read();
-        } catch (EOFException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        return null;
-    }
-    
-    public String Read(int amount) {
-        try {
-            return inst.Read(amount);
-        } catch (EOFException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        return null;
-    }
-    
-    public String ReadLine() {
-        try {
-            return inst.ReadLine();
-        } catch (EOFException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        return null;
-    }
-    
-    public void Write(String StringToWrite) {
-        try {
-            inst.Write(StringToWrite);
-        } catch (IOException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        }        
-    }
-    
-    public void WriteLine(String StringToWrite) {
-        try {
-            inst.WriteLine(StringToWrite);
-        } catch (IOException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public void Rewind() {
-        try {
-            inst.Rewind();
-        } catch (IOException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public void ForceWriteContents() {
-        try {
-            inst.ForceWriteContents();
-        } catch (IOException ex) {
-            Logger.getLogger(File.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public boolean Close() {
-        try {
-            inst.Close();
-        } catch (IOException ex) {
-            return false;
-        }
-        
-        return true;
     }
 }
