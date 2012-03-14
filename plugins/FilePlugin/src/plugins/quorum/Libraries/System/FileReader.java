6bfd8e3d2d0c283d1e71248e281b872a08a2151e
Jeff Wilson <jeff.adams.wilson@gmail.com>
2012-03-14 20:50:56 +0000

    FileReader added.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@532 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

---
+++ plugins/FilePlugin/src/plugins/quorum/Libraries/System/FileReader.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plugins.quorum.Libraries.System;
+
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author jeff
+ */
+public class FileReader {
+    public java.lang.Object $me = null;
+    private QuorumFileReader inst = new QuorumFileReader();
+    
+    public void OpenForReadNative(String path) throws FileNotFoundException {
+        inst.OpenForReadNative(path);
+    }
+    
+    public void Close() throws IOException {
+        inst.Close();
+    }
+
+    public String ReadNative() throws IOException {
+        return inst.ReadNative();
+    }
+
+    // TODO: Document
+    public String ReadNative(int numberOfBytes) throws EOFException, IOException {
+        return inst.ReadNative(numberOfBytes);
+    }
+    
+    public String ReadLineNative() throws EOFException, IOException {
+        return inst.ReadLineNative();
+    }
+    
+    public boolean IsAtEndOfFile() {
+        return inst.IsAtEndOfFile();
+    }
+}
---
+++ plugins/FilePlugin/src/plugins/quorum/Libraries/System/QuorumFileReader.java
@@ -0,0 +1,114 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plugins.quorum.Libraries.System;
+
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * The Java implementation for the "FileReader" Quorum class.
+ * 
+ * @author Jeff Wilson
+ */
+public class QuorumFileReader {
+    protected File file = null;
+    protected BufferedReader bufferedReader = null;
+    protected long fileSize = 0; // used for EOF detection.
+    protected boolean atEOF = false; // are we at the end of the file?
+    
+    public void OpenForReadNative(String path) throws FileNotFoundException {
+        file = new File(path);
+        bufferedReader = new BufferedReader(new FileReader(file));
+        fileSize = file.length();
+    }
+    
+    public void Close() throws IOException {
+        bufferedReader.close();
+    }
+
+    public String ReadNative() throws IOException {
+        if (atEOF)
+            throw new EOFException();
+        
+        // Read all remaining file contents.
+        StringBuilder stringBuff = new StringBuilder();
+        char[] buffer = new char[1024];
+        int numRead = 0;
+        long totalRead = 0;
+
+        do {
+            numRead = bufferedReader.read(buffer, 0, 1024);
+
+            if (numRead > 0) {
+                stringBuff.append(buffer, 0, numRead);
+                totalRead += numRead;
+            }
+        } while (totalRead < fileSize && numRead > 0);
+
+        if (totalRead > 0)
+        {
+            // We will have reached the end of the file here. Don't throw an exception this time.
+            atEOF = true;
+            return stringBuff.toString();
+        }
+        else
+        {
+            throw new IOException("Read() could not read any data from the file.");
+        }
+    }
+
+    // TODO: Document
+    public String ReadNative(int numberOfBytes) throws EOFException, IOException {
+        if (atEOF)
+            throw new EOFException();
+        
+        // Read appropriate number of bytes from the file.
+        char[] buffer = new char[numberOfBytes];
+        int numRead = 0;
+
+        numRead = bufferedReader.read(buffer, 0, numberOfBytes);
+
+        if (numRead == 0 || numRead == -1) {
+            atEOF = true;
+            throw new EOFException();
+        }
+
+        if (numRead > 0)
+        {
+            if (numRead >= fileSize) {
+                // Reached end of file. Don't raise an exception this time.
+                atEOF = true;
+            }
+            return String.valueOf(buffer);
+        }
+        else
+        {
+            throw new IOException("Read() could not read any data from the file.");
+        }
+    }
+    
+    public String ReadLineNative() throws EOFException, IOException {
+        String line = bufferedReader.readLine();
+
+        if (line == null) {
+            if (atEOF) {
+                throw new EOFException();
+            }
+            
+            atEOF = true;
+            line = "";
+        }
+
+        return line;
+    }
+    
+    public boolean IsAtEndOfFile() {
+        return this.atEOF;
+    }
+}
