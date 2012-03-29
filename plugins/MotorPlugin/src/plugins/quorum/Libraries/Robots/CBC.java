38dd8e7dc60ed2a62e8baafde6e7fe269093b81e
Sahana Tambi Sathyanarayana <stambis@siue.edu>
2012-03-29 19:40:22 +0000

    CBC
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@590 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- plugins/MotorPlugin/src/plugins/quorum/Libraries/Robots/CBC.java
+++ plugins/MotorPlugin/src/plugins/quorum/Libraries/Robots/CBC.java
@@ -3,13 +3,40 @@
  * and open the template in the editor.
  */
 package plugins.quorum.Libraries.Robots;
+import cbccore.Botball;
+import cbccore.Device;
 
 /**
  *
  * @author sahana
  */
 public class CBC {
-    //sleep
-    //  cbc_display_clear();     
+    public java.lang.Object $me = null;
+    cbccore.Botball cbc = new cbccore.Botball();
+    cbccore.sensors.analog.Analog s = null;     
     
+    public void ShutDownIn(double seconds)//tested and works 
+    {
+        cbc.shutDownIn(seconds);
+    }
+    
+    public void Sleep(double s) //tested and works //throws InterruptedException
+    {
+        try{
+            int seconds = (int) s;
+            Thread.currentThread().sleep(seconds*1000);}
+        catch(InterruptedException ie){}
+    }
+    
+    //public void RunFor(double seconds)
+    //{
+      //  Device.
+                //LowCreateController().get_create_lbump(lag1);
+   // }
+    
+    public void IsLightinPort(int port) //tested and works 
+    {
+        s = new cbccore.sensors.analog.Analog(port);
+        cbc.waitForLight(s);
+    }
 }
