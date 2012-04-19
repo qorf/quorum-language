da67a90a72883a09e3430401eb5bc893c145153c
Jeff Wilson <jeff.adams.wilson@gmail.com>
2012-04-19 16:25:24 +0000

    (potentially unreasonable) fix to Console class to handle the situation where SWING and/or AWT are not available on a system
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@635 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- plugins/SystemPlugin/src/plugins/quorum/Libraries/System/Console.java
+++ plugins/SystemPlugin/src/plugins/quorum/Libraries/System/Console.java
@@ -18,17 +18,28 @@ import javax.swing.JOptionPane;
  */
 public class Console {
     public java.lang.Object $me = null;
-    private static JOptionPane optionPane = new JOptionPane();;
+    private static JOptionPane optionPane;
     private static JDialog dialog;
+    private static boolean useDialog = false;
     
     static {
         //initialize an input dialog that can be used application wide.
-        optionPane.setOptionType(JOptionPane.OK_CANCEL_OPTION);
-        optionPane.setMessageType(JOptionPane.QUESTION_MESSAGE);
-        optionPane.setWantsInput(true);
-        dialog = optionPane.createDialog("Input Dialog");
-        dialog.setModalityType(ModalityType.APPLICATION_MODAL);
-        dialog.pack();
+        try {
+            optionPane = new JOptionPane();
+            optionPane.setOptionType(JOptionPane.OK_CANCEL_OPTION);
+            optionPane.setMessageType(JOptionPane.QUESTION_MESSAGE);
+            optionPane.setWantsInput(true);
+            dialog = optionPane.createDialog("Input Dialog");
+            dialog.setModalityType(ModalityType.APPLICATION_MODAL);
+            dialog.pack();
+            
+            // Allow dialog to be used.
+            useDialog = true;
+        } catch (Throwable e) {
+            // If SWING or AWT not available, we will use standard input
+            // only.
+            useDialog = false;
+        }
     }
     
     public Console() {
@@ -43,7 +54,7 @@ public class Console {
     }
 
     public String Input(String message) {
-        System.out.println(message);
+        System.out.print(message + " ");
         return grabInput();
     }
     
@@ -72,18 +83,38 @@ public class Console {
      * @return 
      */
     public static String StaticInput(String text) {
-        String answer = "";
-        optionPane.setMessage(text);
-        dialog.setVisible(true); 
-        answer = (String) optionPane.getInputValue();
-        return answer;
+        if (useDialog) {
+            String answer = "";
+            optionPane.setMessage(text);
+            dialog.setVisible(true); 
+            answer = (String) optionPane.getInputValue();
+            return answer;
+        } else {
+            // TODO refactor
+            System.out.print(text + " " );
+            StringBuilder builder = new StringBuilder();
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                String str = "";
+                while ((str = in.readLine()) != null) {
+
+                    builder.append(str);
+                    if(!str.isEmpty()) {
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+            }
+            return builder.toString();
+        }
     }
 
     public static void Load() {
     }
     
     public static void Unload() {
-        dialog.dispose();
+        if (useDialog)
+            dialog.dispose();
     }
     
     public static void main(String args[]) {
