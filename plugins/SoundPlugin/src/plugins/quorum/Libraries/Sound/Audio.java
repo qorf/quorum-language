7a26d598846779e3a900e5c4abb71d529f9c1686
Andreas Stefik <stefika@gmail.com>
2012-05-21 22:18:09 +0000

    Added in a sound plugin.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@656 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

---
+++ plugins/SoundPlugin/src/plugins/quorum/Libraries/Sound/Audio.java
@@ -0,0 +1,129 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plugins.quorum.Libraries.Sound;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+/**
+ *
+ * @author astefik
+ */
+public class Audio {
+    public java.lang.Object $me = null;
+    private final int BUFFER_SIZE = 128000;
+    private AudioInputStream audioStream;
+    private AudioFormat audioFormat;
+    private SourceDataLine sourceLine;
+    
+    private Thread playThread = null;
+    private boolean playThreadShouldRun = false;
+    
+    public void PlayNative(String path, boolean block) {
+        final String finPath = path;
+        if(block) {
+            PlayInternal(path);
+        }
+        else {
+            playThread = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    playThreadShouldRun = true;
+                    PlayInternal(finPath);
+                    playThreadShouldRun = false;
+                }
+            });
+            playThread.start();
+        }
+    }
+    
+    private void PlayInternal(String stringPath) {
+        try {
+            File path = new File(stringPath);
+            audioStream = AudioSystem.getAudioInputStream(path);
+            
+            audioFormat = audioStream.getFormat();
+
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+            try {
+                sourceLine = (SourceDataLine) AudioSystem.getLine(info);
+                sourceLine.open(audioFormat);
+            } catch (LineUnavailableException ex) {
+                Logger.getLogger(Audio.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (Exception ex) {
+                Logger.getLogger(Audio.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            if(false == playThreadShouldRun) {
+                finishOffAudio();
+                return;
+            }
+        
+            sourceLine.start();
+
+            int nBytesRead = 0;
+            byte[] abData = new byte[BUFFER_SIZE];
+            while (nBytesRead != -1) {
+                try {
+                    if(false == playThreadShouldRun) {
+                        finishOffAudio();
+                        return;
+                    }
+                    nBytesRead = audioStream.read(abData, 0, abData.length);
+                } catch (Exception ex) {
+                    Logger.getLogger(Audio.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                if (nBytesRead >= 0) {
+                    sourceLine.write(abData, 0, nBytesRead);
+                }
+            }
+
+            sourceLine.drain();
+            sourceLine.close();
+
+        } catch (UnsupportedAudioFileException ex) {
+            Logger.getLogger(Audio.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Audio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private void finishOffAudio() {
+        audioStream = null;
+        audioFormat = null;
+        if(sourceLine != null) {
+            sourceLine.drain();
+            sourceLine.close();
+            sourceLine = null;
+        }
+    }
+    
+    public void StopPlayingNative() {
+        playThreadShouldRun = false;
+    }
+    public void RecordNative(String path) {
+        
+    }
+    
+    public void StopRecordingNative() {
+    
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
