154027b8b2b9edce6ac0d3d83c55cff7ac728f2e
Jeff Wilson <jeff.adams.wilson@gmail.com>
2012-07-10 19:03:54 +0000

    began chapter 6 import
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@777 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- quorum/libraries/documents/curriculum/chapter6/chapter6.php
+++ quorum/libraries/documents/curriculum/chapter6/chapter6.php
@@ -1,40 +1,39 @@
-<?php include("documents/include/header.php"); ?>
+<?php include("../../include/header.php"); ?>
 <script type="text/javascript">
     document.title = 'Chapter 6: Inheritance';
 </script>
 <h1>Chapter 6: Inheritance</h1>
 <h2>Questions</h2>
 <ol>
- 1. What is the keyword used to inherit a class?
-</ol>
- 2. List at least three pros of inheritance.
- 3. Create a class that inherits another class.
- 4. What is the difference between a derived class and a base class(a parent and child class)?
- 5. What is a blueprint and what does it do?
- 6. What is the keyword for creating a blueprint, and where does it go in an action?
- 7. Are you able to instantiate an object of a class with only blueprint actions? Why or why not?
- 8. What’s the keyword &quot;parent&quot; used for?
- 9. What's the keyword &quot;me&quot; used for?
+<li>What is the keyword used to inherit a class?</li>
+<li>List at least three pros of inheritance.</li>
+<li>Create a class that inherits another class.</li>
+<li>What is the difference between a derived class and a base class(a parent and child class)?</li>
+<li>What is a blueprint and what does it do?</li>
+<li>What is the keyword for creating a blueprint, and where does it go in an action?</li>
+<li>Are you able to instantiate an object of a class with only blueprint actions? Why or why not?</li>
+<li>What's the keyword &quot;parent&quot; used for?</li>
+<li>What's the keyword &quot;me&quot; used for?</li>
+<li>What is polymorphism?</li>
+<li>Give a small example of polymorphism in action.</li>
+<li>How is it that polymorphism enables you to program &quot;in the general&quot; rather than &quot;in the specific&quot;?</li>
+<li>What is the difference between inheritance and instantiation?</li>
+<li>What is the advantage of creating a class with blueprint actions?</li>
+<li>A class Animal has a subclass Mammal.  Which of the following is false:</li>
 <ol>
- 10. What is polymorphism?
- 11. Give a small example of polymorphism in action.
- 12. How is it that polymorphism enables you to program &quot;in the general&quot; rather than &quot;in the specific&quot;?
- 13. What is the difference between inheritance and instantiation?
- 14. What is the advantage of creating a class with blueprint actions?
- 15. A class Animal has a subclass Mammal.  Which of the following is false:
-</ol>
- a) Mammal can have subclasses.
- b) Mammal can have other parents than Animal.
- c) Animal can have only one subclass.
- d) Mammal can have siblings.
+ <li>Mammal can have subclasses.</li>
+ <li>Mammal can have other parents than Animal.</li>
+ <li>Animal can have only one subclass.</li>
+ <li>Mammal can have siblings.</li>
+</ol></ol>
 <h2>Essay Questions</h2>
 <ol>
- 1.  Explain some of the uses inheritance has and why programmers use it.
+    <li> Explain some of the uses inheritance has and why programmers use it.</li>
+    <li> Explain what polymorphism is and how it benefits coders.  How does polymorphism relate to and work with inheritance?</li>
 </ol>
- 2.  Explain what polymorphism is and how it benefits coders.  How does polymorphism relate to and work with inheritance?
 <h2>In Class Lab Assignments</h2>
 <ul>
-<li> <a href="curriculum/chapter6/lab6_1.php">Lab Manual 6.1</a></li>
+<li> <a href="lab6_1.php">Lab Manual 6.1</a></li>
 <ul>
 <li>Introduce the fundamentals of Inheritance</li>
 <li>Inheriting actions from a parent class</li>
@@ -42,7 +41,7 @@
 </ul>
 </ul>
 <ul>
-<li> <a href="curriculum/chapter6/lab6_2.php">Lab Manual 6.2</a></li>
+<li> <a href="lab6_2.php">Lab Manual 6.2</a></li>
 <ul>
 <li>Learn about blueprints</li>
 <li>Create and use blueprints</li>
@@ -50,7 +49,7 @@
 </ul>
 </ul>
 <ul>
-<li><a href="curriculum/chapter6/lab6_3.php">Lab Manual 6.3</a></li>
+<li><a href="lab6_3.php">Lab Manual 6.3</a></li>
 <ul>
 <li>Learn about polymorphism</li>
 <li>Apply the concept of inheritance across multiple classes</li>
@@ -59,21 +58,21 @@
 </ul>
 <h2>Short Programming Projects</h2>
 <ul>
-<li><a href="curriculum/chapter6/assignment6_1.php">Assignment Manual 6.1</a></li>
+<li><a href="assignment6_1.php">Assignment Manual 6.1</a></li>
 <ul>
 <li>Creating Class actions and using derived actions</li>
 <li>Using Inheritance</li>
 </ul>
 </ul>
 <ul>
-<li><a href="curriculum/chapter6/assignment6_2.php">Assignment Manual 6.2</a></li>
+<li><a href="assignment6_2.php">Assignment Manual 6.2</a></li>
 <ul>
 <li>Creating and using blueprint actions</li>
 <li>Using derived actions with blueprint actions</li>
 </ul>
 </ul>
 <ul>
-<li><a href="curriculum/chapter6//assignment6_3.php">Assignment Manual 6.3</a></li>
+<li><a href="assignment6_3.php">Assignment Manual 6.3</a></li>
 <ul>
 <li>Using inheritance</li>
 <li>Using polymorphism</li>
@@ -82,9 +81,9 @@
 </ul>
 <h2>Challenge Programming Projects</h2>
 <ul>
-<li><a href="curriculum/chapter6/assignment6_4.php">Assignment Manual 6.4</a></li>
+<li><a href="assignment6_4.php">Assignment Manual 6.4</a></li>
 <ul>
 <li>Build a Super Hero factory</li>
 <li>Use the Libraries.Sound.Music</li>
 <li>Use inheritance and blueprint actions</li>
-<?php include("documents/include/footer.php"); ?>
+<?php include("../../include/footer.php"); ?>
