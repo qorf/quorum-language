4c09821f48fa2bf4e93d760f5292dc92359e501f
Andreas Stefik <stefika@gmail.com>
2012-11-05 22:22:03 +0000

    Added a separate package for the Quorum server.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@936 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

---
+++ quorum/src/org/quorum/web/QuorumServer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.quorum.web;
+
+/**
+ *
+ * @author Louis Koester
+ */
+public class QuorumServer {
+    
+}
