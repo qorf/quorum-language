c143f1d911cc5b64f9106938304b494aeec8969c
Melissa Stefik <harrismel@gmail.com>
2012-11-13 22:29:55 +0000

    Added test cases for the HashTable.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@958 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- quorum/test/org/quorum/tests/compiler/HashTable/HashTableTester.java
+++ quorum/test/org/quorum/tests/compiler/HashTable/HashTableTester.java
@@ -80,4 +80,365 @@ public class HashTableTester {
         assert(r.getLine(0).equals("23"));
         assert(r.getLine(1).equals("65"));
     } 
+    
+    @Test
+    public void test_GetKey_pass_execute(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "GetKey.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        
+        if(vm.getExceptions().hasExceptions()){
+            fail();
+        }else{
+            ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("age");
+            String r1 = variableValue.getResult().text;
+            variableValue = vm.getDataEnvironment().getVariableValue("height");
+            String r2 = variableValue.getResult().text;
+
+            if( !r1.equalsIgnoreCase("age") || !r2.equalsIgnoreCase("height") )
+            {
+                fail();
+            }
+        }
+        vm.stop();
+    }
+    
+    @Test
+    public void test_GetKey_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "GetKey.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("age"));
+        assert(r.getLine(1).equals("height"));
+    }
+    
+    @Test
+    public void test_GetKeyIterator_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "GetKeyIterator.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("height"));
+        assert(r.getLine(1).equals("age"));
+        
+    }
+    
+    @Test
+    public void test_GetValueIterator_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "GetValueIterator.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("65"));
+        assert(r.getLine(1).equals("23"));
+    }
+    
+    @Test
+    public void test_GetSize_pass_execute(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "GetSize.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        
+        if(vm.getExceptions().hasExceptions()){
+            fail();
+        }else{
+            ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("size");
+            int r1 = variableValue.getResult().integer;
+
+            if( r1 != 2)
+            {
+                fail();
+            }
+        }
+        vm.stop();
+    }
+    
+    @Test
+    public void test_GetSize_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "GetSize.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("2"));
+    }
+    
+    @Test
+    public void test_Empty_pass_execute(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "Empty.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        
+        if(vm.getExceptions().hasExceptions()){
+            fail();
+        }else{
+            ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("isEmpty");
+            boolean r1 = variableValue.getResult().boolean_value;
+
+            if( !r1 )
+            {
+                fail();
+            }
+        }
+        vm.stop();
+    }
+    
+    @Test
+    public void test_Empty_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "Empty.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("true"));
+    }
+    
+    @Test
+    public void test_HasValue_pass_execute(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "HasValue.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        
+        if(vm.getExceptions().hasExceptions()){
+            fail();
+        }else{
+            ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("isEmpty");
+            boolean r1 = variableValue.getResult().boolean_value;
+
+            if( !r1 )
+            {
+                fail();
+            }
+        }
+        vm.stop();
+    }
+    
+    @Test
+    public void test_HasValue_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "HasValue.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("true"));
+    }
+    
+    @Test
+    public void test_HasKey_pass_execute(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "HasKey.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        
+        if(vm.getExceptions().hasExceptions()){
+            fail();
+        }else{
+            ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("isEmpty");
+            boolean r1 = variableValue.getResult().boolean_value;
+
+            if( !r1 )
+            {
+                fail();
+            }
+        }
+        vm.stop();
+    }
+    
+    @Test
+    public void test_HasKey_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "HasKey.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("true"));
+    }
+    
+    @Test
+    public void test_RemoveKey_pass_execute(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "RemoveKey.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        
+        if(vm.getExceptions().hasExceptions()){
+            fail();
+        }else{
+            ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("age");
+            int r1 = variableValue.getResult().integer;
+
+            if( r1 != 23 )
+            {
+                fail();
+            }
+        }
+        vm.stop();
+    }
+    
+    @Test
+    public void test_RemoveKey_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "RemoveKey.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("23"));
+    }
+    
+    @Test
+    public void test_RemoveValue_pass_execute(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "RemoveValue.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        
+        if(vm.getExceptions().hasExceptions()){
+            fail();
+        }else{
+            ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("age");
+            String r1 = variableValue.getResult().text;
+
+            if( !r1.equalsIgnoreCase("age") )
+            {
+                fail();
+            }
+        }
+        vm.stop();
+    }
+    
+    @Test
+    public void test_RemoveValue_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "RemoveValue.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("age"));
+    }
+    
+    @Test
+    public void test_RemoveAllKey_pass_execute(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "RemoveAllKey.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        
+        if(vm.getExceptions().hasExceptions()){
+            fail();
+        }else{
+            ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("age");
+            boolean r1 = variableValue.getResult().boolean_value;
+
+            if( !r1 )
+            {
+                fail();
+            }
+        }
+        vm.stop();
+    }
+    
+    @Test
+    public void test_RemoveAllKey_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "RemoveAllKey.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("true"));
+    }
+    
+    @Test
+    public void test_RemoveAllValue_pass_execute(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "RemoveAllValue.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+        vm.blockRun();
+        
+        if(vm.getExceptions().hasExceptions()){
+            fail();
+        }else{
+            ExpressionValue variableValue = vm.getDataEnvironment().getVariableValue("age");
+            boolean r1 = variableValue.getResult().boolean_value;
+            ExpressionValue variableValue2 = vm.getDataEnvironment().getVariableValue("isEmpty");
+            boolean r2 = variableValue2.getResult().boolean_value;
+
+            if( !r1  || r2)
+            {
+                fail();
+            }
+        }
+        vm.stop();
+    }
+    
+    @Test
+    public void test_RemoveAllValue_pass_bytecode(){
+        CompilerTestSuite.build(CompilerTestSuite.getQuorumFile(CompilerTestSuite.HASHTABLE + CompilerTestSuite.PASS + "RemoveAllValue.quorum"));
+        if (!vm.getCompilerErrors().isCompilationErrorFree()){
+            fail();
+        }
+
+        RunResult r = CompilerTestSuite.runQuorumFile();
+        if (!r.isSuccessful())
+            fail();
+        
+        assert(r.getLine(0).equals("true"));
+        assert(r.getLine(1).equals("false"));
+    }
 }
