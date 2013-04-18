e50944b85fd14162c6561f4bf49cbafb08aa7ef7
Kerry Ritter <kerrylritter@gmail.com>
2013-04-18 00:20:27 +0000

    Epic fail. Overwrote the controller with the model, causing the error. Sigh. Fixed.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@1282 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- quorum/libraries/controllers/user.controller.php
+++ quorum/libraries/controllers/user.controller.php
@@ -1,7 +1,7 @@
 <?php
 	require("../models/user.model.php");
 
-	function controllerRegister() {
+	function register() {
 		$email = $_POST['registration-email'];
 		$username = $_POST['registration-username'];
 		$password = $_POST['registration-password'];
@@ -22,7 +22,7 @@
 		}
 	}
 
-	function controllerLogin() {
+	function login() {
 		$username = $_POST['login-username'];
 		$password = $_POST['login-password'];
 
@@ -42,7 +42,7 @@
 		}
 	}
 
-	function controllerSignOut() {
+	function signOut() {
 		header("Location: " . $_GET['url']);
 
 		ob_start();
@@ -54,9 +54,9 @@
 
 	function router($action) {
 		switch ($action) {
-			case "login": controllerLogin(); break;
-			case "register": controllerRegister(); break;
-			case "signout": controllerSignOut(); break;
+			case "login": login(); break;
+			case "register": register(); break;
+			case "signout": signOut(); break;
 		}
 	}
 
--- quorum/libraries/models/user.model.php
+++ quorum/libraries/models/user.model.php
@@ -1,64 +1,69 @@
 <?php
-	require("../models/user.model.php");
+	require_once("data.model.php");
+	require_once("../config.php");
 
-	function register() {
-		$email = $_POST['registration-email'];
-		$username = $_POST['registration-username'];
-		$password = $_POST['registration-password'];
-		$google_id = $_POST['registration-identity'];
-		$user = new User($email, $username, $password, $google_id);
-		
-		if ($user->insert() == true) {
+	class User extends QuorumDataModel {
+		private $table_name = "sodbeans_users";
+		public $email = "";
+		public $username = "";
+		public $password = "";
+		public $google_id = "";
+		private $ip;
+
+		function __construct($email, $username, $password, $google_id) {
+			parent::__construct();
+
+			if ($password == null || $password == "") {
+				$password = sha1(secretGooglePassword());
+			}
+
+			$this->email = $email;
+			$this->username = $username;
+			$this->password = sha1(saltPassword($password));
+			$this->google_id = $google_id;
+			$this->ip = $_SERVER['SERVER_ADDR'];
+		}
+
+		public function insert() {
+			$sqlQuery = "INSERT INTO " . $this->table_name . " (email, username, password, reset_password_key, reset_password_expire, google_id) VALUES (?, ?, ?, NULL, NULL, ?)";
+
+			$valuesToPrepare = array($this->email, $this->username, $this->password, $this->google_id);
+
 			try {
-				$user->createCookies();
-				print true;
+				$preparedStatement = $this->connection->prepare($sqlQuery);
+				$preparedStatement->execute($valuesToPrepare);
+				return true;
 			}
 			catch (Exception $ex) {
-				print false;
+				return false;
 			}
-		}
-		else {
-			print false;
-		}
-	}
 
-	function login() {
-		$username = $_POST['login-username'];
-		$password = $_POST['login-password'];
+			return false;
+		}
 
-		$user = new User(null, $username, $password, null);
-		
-		if ($user->checkLogin() == true) {
-			try {
-				$user->createCookies();
-				print true;
+		public function checkLogin() {
+			if ($google_id == null) { // Regular user
+				//var_dump($this);
+				$sqlQuery = "SELECT * FROM " . $this->table_name . " WHERE username=? AND password=? AND google_id IS NULL";
+				$valuesToPrepare = array($this->username, $this->password);
 			}
-			catch (Exception $ex) {
-				print false;
+			else { // Google user
+				$sqlQuery = "SELECT * FROM " . $this->table_name . " WHERE username=? AND google_id=?";
+				$valuesToPrepare = array($this->username, $this->google_id);
 			}
+
+			$preparedStatement = $this->connection->prepare($sqlQuery);
+			$preparedStatement->execute($valuesToPrepare);
+
+			return ($preparedStatement->rowCount() == 1) ? true : false; // if we have one row, we are logged in.
 		}
-		else {
-			print false;
-		}
-	}
 
-	function signOut() {
-		header("Location: " . $_GET['url']);
-
-		ob_start();
-		$timeUntilExpire = time() - 3600; // expire an hour in the past
-		setcookie( "username", "", $timeUntilExpire, '/');
-		setcookie( "password", "", $timeUntilExpire, '/');
-		ob_end_flush();
-	}
-
-	function router($action) {
-		switch ($action) {
-			case "login": login(); break;
-			case "register": register(); break;
-			case "signout": signOut(); break;
+		public function createCookies() {
+			ob_start();
+			$timeUntilExpire = time()+(60*60*24*30); // one month
+			setcookie( "username", $this->username, $timeUntilExpire, '/');
+			setcookie( "password", $this->password, $timeUntilExpire, '/');
+			ob_end_flush();
 		}
 	}
-
-	router($_GET['action']);
 ?>
