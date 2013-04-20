f8b3bfa75b771bdceff77e3e79c744a6107a4b9f
Kerry Ritter <kerrylritter@gmail.com>
2013-04-20 15:43:16 +0000

    Need more sleep. Overwrote the wrong file.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@1292 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- quorum/libraries/models/ratings.model.php
+++ quorum/libraries/models/ratings.model.php
@@ -1,87 +1,221 @@
 <?php
-	require("../models/user.model.php");
+	require_once("data.model.php");
 
-	function register() {
-		$email = $_POST['registration-email'];
-		$username = $_POST['registration-username'];
-		$password = $_POST['registration-password'];
-		$google_id = $_POST['registration-identity'];
+	class Rating extends QuorumDataModel {
+		function __construct() {
+			parent::__construct();
+		}
 
-		$user = new User($email, $username, $password, $google_id);
-		
-		if ($user->insert() == true) {
+		public function addRatingToDatabase($ratingToUpdate) {
+			$updateResult = $this->update($ratingToUpdate);
+
+			if (is_int($updateResult)) {
+				// if no rows are updated, try to insert and return the result of the insertion.
+				// otherwise the update was successful so return true.
+				return ($updateResult == 0) ? $this->insert() : 1;
+			}
+
+			return $updateResult; // if we got here, we know the update threw an exception
+		}
+	}
+
+	class ClassRating extends Rating {
+		private $table_name = "class_ratings";
+		public $static_key = "";
+		public $user = "";
+		public $rating_name = "";
+		public $rating_example = "";
+		public $rating_description = "";
+
+		function __construct($static_key, $user, $rating_name, $rating_example, $rating_description) {
+			parent::__construct();
+			$this->static_key = $static_key;
+			$this->user = $user;
+			$this->rating_name = $rating_name;
+			$this->rating_example = $rating_example;
+			$this->rating_description = $rating_description;
+		}
+
+		public function insert() {
 			try {
-				$user->createCookies();
-				print "1";
+				$sqlQuery = "INSERT INTO " . $this->table_name	 										
+						  . " (static_key, user, rating_name, rating_example, rating_description)"
+						  . " VALUES (?, ?, ?, ?, ?)";
+
+				$valuesToPrepare = array($this->static_key, $this->user, $this->rating_name, $this->rating_example, $this->rating_description);
+				print "<br /><br />"; var_dump($this);	print "<br /><br />"; var_dump($valuesToPrepare);
+				$preparedStatement = $this->connection->prepare($sqlQuery);
+				$preparedStatement->execute($valuesToPrepare);
+
+				return $preparedStatement->rowCount();
 			}
-			catch (Exception $ex) {
-				print "-1";
+			catch (Exception $e) {
+				return $e;
 			}
 		}
-		else {
-			print "0";
-		}
-	}
 
-	function standardLogin() {
-		$username = $_POST['login-username'];
-		$password = $_POST['login-password'];
-
-		$user = new User(null, $username, $password, null);
-		
-		print (login($user) == true) ? "1" : "0";
-	}
-
-	function login($user) {
-		if ($user->checkLogin() == true) {
+		public function update($ratingToUpdate) {
 			try {
-				$user->createCookies();
-				return true;
+				$sqlQuery = null;
+				$valuesToPrepare = null;
+
+				switch ($ratingToUpdate) {
+					case "name": 
+						$sqlQuery = "UPDATE " . $this->table_name . " SET rating_name=? WHERE static_key = ? AND user = ?";
+						$valuesToPrepare = array($this->rating_name, $this->static_key, $this->user);
+						break;
+					case "example":
+						$sqlQuery = "UPDATE " . $this->table_name . " SET rating_example=? WHERE static_key = ? AND user = ?";
+						$valuesToPrepare = array($this->rating_example, $this->static_key, $this->user);
+						break;
+					case "description":
+						$sqlQuery = "UPDATE " . $this->table_name . " SET rating_description=? WHERE static_key = ? AND user = ?";
+						$valuesToPrepare = array($this->rating_description, $this->static_key, $this->user);
+						break;
+				}
+
+				$preparedStatement = $this->connection->prepare($sqlQuery);
+				$preparedStatement->execute($valuesToPrepare);
+
+				return $preparedStatement->rowCount();
 			}
-			catch (Exception $ex) {
-				return false;
+			catch (Exception $e) {
+				return $e;
 			}
 		}
-		else {
-			return false;
+	}
+
+	class ActionRating extends Rating {
+		private $table_name = "action_ratings";
+		public $class_static_key = "";
+		public $static_key = "";
+		public $user = "";
+		public $rating_name = "";
+		public $rating_example = "";
+		public $rating_description = "";
+
+		function __construct($class_static_key, $static_key, $user, $rating_name, $rating_example, $rating_description) {
+			parent::__construct();
+			$this->class_static_key = $class_static_key;
+			$this->static_key = $static_key;
+			$this->user = $user;
+			$this->rating_name = $rating_name;
+			$this->rating_example = $rating_example;
+			$this->rating_description = $rating_description;
+		}
+
+		public function insert() {
+			try {
+				$sqlQuery = "INSERT INTO " . $this->table_name		 										
+						  . " (class_static_key, static_key, user, rating_name, rating_example, rating_description)"
+						  . " VALUES (?, ?, ?, ?, ?, ?)";
+
+				$valuesToPrepare = array($this->class_static_key, $this->static_key, $this->user, $this->rating_name, $this->rating_example, $this->rating_description);
+				$preparedStatement = $this->connection->prepare($sqlQuery);
+
+				$preparedStatement->execute($valuesToPrepare);
+
+				return $preparedStatement->rowCount();
+			}
+			catch (Exception $e) {
+				return $e;
+			}
+		}
+
+		public function update($ratingToUpdate) {
+			try {
+				$sqlQuery = null;
+				$valuesToPrepare = null;
+
+				switch ($ratingToUpdate) {
+					case "name": 
+						$sqlQuery = "UPDATE " . $this->table_name . " SET rating_name=? WHERE static_key = ? AND user = ? AND class_static_key = ?";
+						$valuesToPrepare = array($this->rating_name, $this->static_key, $this->user, $this->class_static_key);
+						break;
+					case "example":
+						$sqlQuery = "UPDATE " . $this->table_name . " SET rating_example=? WHERE static_key = ? AND user = ? AND class_static_key = ?";
+						$valuesToPrepare = array($this->rating_example, $this->static_key, $this->user, $this->class_static_key);
+						break;
+					case "description":
+						$sqlQuery = "UPDATE " . $this->table_name . " SET rating_description=? WHERE static_key = ? AND user = ? AND class_static_key = ?";
+						$valuesToPrepare = array($this->rating_description, $this->static_key, $this->user, $this->class_static_key);
+						break;
+				}
+				
+				$preparedStatement = $this->connection->prepare($sqlQuery);
+				$preparedStatement->execute($valuesToPrepare);
+
+				return $preparedStatement->rowCount();
+			}
+			catch (Exception $e) {
+				return $e;
+			}
 		}
 	}
 
-	function signOut() {
-		header("Location: " . $_GET['url']);
+	class ParameterRating extends Rating {
+		private $table_name = "parameter_ratings";
+		public $class_static_key = "";
+		public $action_static_key = "";
+		public $static_key = "";
+		public $user = "";
+		public $rating_name = "";
+		public $rating_description = "";
 
-		ob_start();
-		$timeUntilExpire = time() - 3600; // expire an hour in the past
-		setcookie( "username", "", $timeUntilExpire, '/');
-		setcookie( "password", "", $timeUntilExpire, '/');
-		ob_end_flush();
-	}
+		function __construct($class_static_key, $action_static_key, $static_key, $user, $rating_name, $rating_description) {
+			parent::__construct();
+			$this->class_static_key = $class_static_key;
+			$this->action_static_key = $action_static_key;
+			$this->static_key = $static_key;
+			$this->user = $user;
+			$this->rating_name = $rating_name;
+			$this->rating_description = $rating_description;
+		}
 
-	function cancelGoogle() {
-		session_start();
-		$_SESSION['openIdData'] = null;
-		header('Location: ' . $_GET['returnurl']);
-	}
+		public function insert() {
+			try {
+				$sqlQuery = "INSERT INTO " . $this->table_name			 										
+						  . " (class_static_key, action_static_key, static_key, user, rating_name, rating_description)"
+						  . " VALUES (?, ?, ?, ?, ?, ?)";
 
-	function googleUserLoginAuthenticate() {
-		session_start();
+				$valuesToPrepare = array($this->class_static_key, $this->action_static_key, $this->static_key, $this->user, $this->rating_name, $this->rating_description);
+				
+				$preparedStatement = $this->connection->prepare($sqlQuery);
+				$preparedStatement->execute($valuesToPrepare);
 
-		$openIdData = $_SESSION['openIdData'];
-		$user = new User($openIdData['email'], null, null, $openIdData['identity']);
-		$user->getUsernameFromGoogleId();
 
-		print (login($user) == true) ? "1" : "0";
-	}
+				return $preparedStatement->rowCount();
+			}
+			catch (Exception $e) {
+				return $e;
+			}
+		}
 
-	function router($action) {
-		switch ($action) {
-			case "login": standardLogin(); break;
-			case "register": register(); break;
-			case "signout": signOut(); break;
-			case "cancelgoogle": cancelGoogle(); break;
-			case "googleUserLoginAuthenticate": googleUserLoginAuthenticate(); break;
+		public function update($ratingToUpdate) {
+			try {
+				$sqlQuery = null;
+				$valuesToPrepare = null;
+
+				switch ($ratingToUpdate) {
+					case "name": 
+						$sqlQuery = "UPDATE " . $this->table_name . " SET rating_name=? WHERE static_key = ? AND user = ? AND class_static_key = ? AND action_static_key = ?";
+						$valuesToPrepare = array($this->rating_name, $this->static_key, $this->user, $this->class_static_key, $this->action_static_key);
+						break;
+					case "description":
+						$sqlQuery = "UPDATE " . $this->table_name . " SET rating_description=? WHERE static_key = ? AND user = ? AND class_static_key = ? AND action_static_key = ?";
+						$valuesToPrepare = array($this->rating_description, $this->static_key, $this->user, $this->class_static_key, $this->action_static_key);
+					 	break;
+				}
+				
+				$preparedStatement = $this->connection->prepare($sqlQuery);
+				$preparedStatement->execute($valuesToPrepare);
+
+				return $preparedStatement->rowCount();
+			}
+			catch (Exception $e) {
+				return $e;
+			}
 		}
 	}
 
-	router($_GET['action']);
 ?>
