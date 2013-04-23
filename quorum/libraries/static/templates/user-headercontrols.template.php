e886834cc5be04e7ce848adcc17ea8b9bd23e732
Kerry Ritter <kerrylritter@gmail.com>
2013-04-23 18:13:44 +0000

    Updated header. Fixed signout.
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@1304 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- quorum/libraries/static/templates/user-headercontrols.template.php
+++ quorum/libraries/static/templates/user-headercontrols.template.php
@@ -1,10 +1,19 @@
 <?php 
 	$username = $_COOKIE['username'];
 	$url = ($_SERVER['PHP_SELF'] == "/static/templates/user-headercontrols.template.php") ? $_SERVER['PHP_SELF'] : "";
-	$signOutLink = "<a href=\"/controllers/user.controller.php?action=signout&url=" . $url . "\">Sign Out</a>"; 
+	$signOutLink = "<a class=\"btn btn-primary\" href=\"/controllers/user.controller.php?action=signout\">Sign Out</a>"; 
+
+	function Truncate($string, $length, $stopanywhere=false) {
+		if (strlen($string) > $length) {
+			return substr($string,0,$length) . "...";
+		}
+		else {
+			return $string;
+		}
+	}
 ?>
 
 <div class="user-controls-loggedin">
-	<h3>Welcome, <?php print $_COOKIE['username']; ?>!</h3> 
+	<h3>Welcome, <?php print Truncate($_COOKIE['username'], 8); ?>!</h3> 
 	<h4><?php print $signOutLink; ?></h4>
 </div>
