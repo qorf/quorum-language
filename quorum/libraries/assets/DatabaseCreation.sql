ca31356506822072a3ad6775c9a8e0dc021759c4
rajjr12 <rajjr12@un.known>
2013-04-29 23:18:13 +0000

    fixed type errors in MySQL command and corrected command ordering issue
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@1331 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- quorum/libraries/assets/DatabaseCreation.sql
+++ quorum/libraries/assets/DatabaseCreation.sql
@@ -1,6 +1,6 @@
 CREATE TABLE run_code
 (
-quorum_version VARCHAR(65535) NOT NULL,
+quorum_version TEXT NOT NULL,
 code TEXT NOT NULL,
 output TEXT NOT NULL,
 times_requested BIGINT
@@ -8,46 +8,44 @@ times_requested BIGINT
 
 CREATE TABLE class_ratings
 (
-static_key VARCHAR(65535) NOT NULL,
+static_key TEXT NOT NULL,
 user VARCHAR(255) NOT NULL,
 rating_name TINYINT,
 rating_example TINYINT,
-rating_description TINYINT,
-PRIMARY KEY (user)
+rating_description TINYINT
 );
 
 CREATE TABLE action_ratings
 (
-class_static_key VARCHAR(65535) NOT NULL,
-static_key VARCHAR(65535) NOT NULL,
+class_static_key TEXT NOT NULL,
+static_key TEXT NOT NULL,
 user VARCHAR(255) NOT NULL,
 rating_name TINYINT,
 rating_example TINYINT,
-rating_description TINYINT,
-PRIMARY KEY (user)
+rating_description TINYINT
 );
 
 CREATE TABLE parameter_ratings
 (
-class_static_key VARCHAR(65535) NOT NULL,
-action_static_key VARCHAR(65535) NOT NULL,
-static_key VARCHAR(65535) NOT NULL,
+class_static_key TEXT NOT NULL,
+action_static_key TEXT NOT NULL,
+static_key TEXT NOT NULL,
 user VARCHAR(255) NOT NULL,
 rating_name TINYINT,
 rating_example TINYINT,
-rating_description TINYINT,
-PRIMARY KEY (user)
+rating_description TINYINT
 );
-
-
-ALTER TABLE class_ratings ADD FOREIGN KEY user_fk_00 (user) REFERENCES sodbeans_users (username);
-
-ALTER TABLE action_ratings ADD FOREIGN KEY user_fk_01 (user) REFERENCES sodbeans_users (username);
-
-ALTER TABLE parameter_ratings ADD FOREIGN KEY user_fk_02 (user) REFERENCES sodbeans_users (username);
 
 ALTER TABLE sodbeans_users ADD COLUMN google_id varchar(255);
 
 ALTER TABLE sodbeans_users ADD COLUMN email varchar(255);
 
 ALTER TABLE sodbeans_users ADD UNIQUE(email);
+
+ALTER TABLE sodbeans_users ADD UNIQUE(username);
+
+ALTER TABLE class_ratings ADD FOREIGN KEY user_fk_00 (user) REFERENCES sodbeans_users (username);
+
+ALTER TABLE action_ratings ADD FOREIGN KEY user_fk_01 (user) REFERENCES sodbeans_users (username);
+
+ALTER TABLE parameter_ratings ADD FOREIGN KEY user_fk_02 (user) REFERENCES sodbeans_users (username);
