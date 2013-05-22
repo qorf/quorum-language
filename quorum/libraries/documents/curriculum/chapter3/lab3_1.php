697331575d540fc8e8f5c53f474f3c0202d37eff
Albatross Cafe <albatrosscafe@un.known>
2013-05-22 02:23:32 +0000

    fixed multiple typos
    git-svn-id: https://svn.code.sf.net/p/quorum/code/trunk@1373 df4a1cb3-768c-44ce-9dd6-df0ee0bb247f

--- quorum/libraries/documents/curriculum/chapter3/lab3_1.php
+++ quorum/libraries/documents/curriculum/chapter3/lab3_1.php
@@ -39,7 +39,7 @@ Run the program and there should not be any errors.  The input dialog will appea
 Enter a value of 1 in the input dialog.  Nothing should have happened.  Add the code to tell Sodbeans to speak the price right after the line starting with <tt>integer price =</tt>. Run the program again. This time, notice that the number "20,000" is only spoken if you enter the number 1.
 </p>
 <p>
-One problem with this solution is the user only knows the price of their order if they order a single car. To correct this, move the say statement you wrote to after the line starting with <tt>end</tt>. This puts the code outside of the if statement, so it will always execute. Run the program. Whoops!  Your are getting the following compiler errors:
+One problem with this solution is the user only knows the price of their order if they order a single car. To correct this, move the say statement you wrote to after the line starting with <tt>end</tt>. This puts the code outside of the if statement, so it will always execute. Run the program. Whoops! You are getting the following compiler errors:
 </p>
 <p><pre class="code"><code>
 The variable ‘price’ has not been defined.  Did you spell the variable’s name correctly?
@@ -64,7 +64,7 @@ Run the program again, and note that the else statement is executed when you ent
 What you have written so far isn’t enough for the car dealership to meet its business requirements.  Your will need to make changes to your current code to comply with the business rules.  Use the business rules below and translate the descriptions in plain English to the code.
 </p>
 <ol><li>If the client only requests one car, then the car should be sold at the price of $20,000.
-</li><li>If the client wants to place an order for two cars at the same time, then the car can be sold at a slight discount.  Both cars should be sold at the price of $19,950 a peice.  The client should receive the total price of the order (2 * 19,950).
+</li><li>If the client wants to place an order for two cars at the same time, then the car can be sold at a slight discount.  Both cars should be sold at the price of $19,950 apiece.  The client should receive the total price of the order (2 * 19,950).
 </li><li>If the client wants to purchase three cars, then each car is sold at the slightly larger discount.  Each car should be sold for $19,900 and the client should receive the total price of the order.
 </li><li>If the client wants to order more than three cars, then each car should be sold at the price of $19,800.  The client will need to know the total price of all cars.
 </li></ol>
@@ -91,7 +91,7 @@ Second, when a client tries to place an order for 1,000 cars, there is no way fo
 Last but not least, the user does not know when a mistake has been made.  For example, if the user enters 100 instead of 10 for the number of cars, the program will automatically reset the number of cars from 100 to 20 without informing the user of the mistake.  To fix this problem, you will need to add code in the body of the if-statements for two problems that you have solved above.  Use the following output to have the program announce the user’s mistake with a reason for the error.
 </p>
 <p><pre class="code"><code>
-Error: The number of cars must be betyouen 1 and 20. The value has been reset to 1. You entered -1
+Error: The number of cars must be between 1 and 20. The value has been reset to 1. You entered -1
 The price for the order of 1 car(s) is $20000
 </code></pre></p>
 <p><pre class="code"><code>
