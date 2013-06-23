2bfdea7b746f99654c91847695174230bd0940ab
coleslawvinnii <nbumho@siue.edu>
2013-06-23 11:55:56 -0700

    lab 6.5 updated

--- quorum/libraries/documents/curriculum/chapter6/lab6_5.php
+++ quorum/libraries/documents/curriculum/chapter6/lab6_5.php
@@ -20,7 +20,7 @@
   </li>
 </ul>
 <h2 id="Overview">Overview</h2>
-  In <a href="lab6_4.php">lab 6_4</a> you built a screen reader that told you what key on the keyboard you pressed. In Quorum, you can write screen readers that do even more. In this lab you will make another screen reader that will tell you when a new window, button, or any other component has gained focus.
+  In <a href="lab6_4.php">lab 6_4</a> we built a screen reader that told us what key on the keyboard was pressed. In Quorum, we can write screen readers that do even more. In this lab we will make another screen reader that will tell us when a new window, button, or any other component has gained focus.
 <p>
 </p>
 <h2>Task 1: Getting Started</h2>
@@ -28,30 +28,57 @@
   Start Sodbeans. Create a new “Quorum Application” project, and name it <strong>Lab6_5</strong>.  In the <tt>Main.quorum</tt> file, it should contain a <tt>Main</tt> class and <tt>Main</tt> action.
 </p>
 <p>
-  You will create an additional class in this project.  In the New File dialog, create a new file by selecting “Quorum” and “Quorum Class” in the Categories and File Types windows, respectively.  Then, name  the new file <strong>Observer</strong> in the New Quorum Class dialog.
+  We will need to create an additional class in this project.  In the New File dialog, create a new file by selecting “Quorum” and “Quorum Class” in the Categories and File Types windows, respectively.  Then, name  the new file <strong>Observer</strong> in the New Quorum Class dialog.
 </p>
 <p>
-  Don't forget to include the appropriate libraries.
+  Don't forget to include the appropriate libraries!
 </p>
 <h2>Task 2: Inheriting from the FocusObserver Class</h2>
 <p>
-  Just like in <a href="lab6_4.php">lab 6_4</a>, your <strong>Observer</strong> class will need to inherit the variables and actions of an observer class. To get the events that involve the focus changing we need to inherit from the <tt>FocusObserver</tt> class.
+  Just like in <a href="lab6_4.php">lab 6_4</a>, our <strong>Observer</strong> class will need to inherit the variables and actions of an observer class. The class we need to include in order to get the events that involve the focus changing is the <tt>FocusObserver</tt> class. Just as in the last lab, use the <tt>is</tt> keyword to inherit from the <tt>FocusObserver</tt> class.
 </p>
 <pre class="code">
   use Libraries.Accessibility.all 
   use Libraries.Sound.Speech
+  
   class Observer is FocusObserver
   end
 </pre>
 <p>
-  Just as in <a href="lab6_4.php">lab 6_4</a>, your <tt>Observer</tt> class is going to need an instance of the <tt>Speech</tt> class to say things quickly. 
+  Just as in <a href="lab6_4.php">lab 6_4</a>, our <tt>Observer</tt> class is going to need an instance of the <tt>Speech</tt> class to say things quickly. 
 </p>
 <p>
-  You also need to implement the blueprint action <tt>ReceiveEvent</tt>. In <a href="lab6_4.php">lab 6_4</a> we used the <tt>GetDescription</tt> action to explain the event, but you can also get the individual aspects of the event and make your own sentence to describe the event. There are 4 other actions that you can call on an event to get a description of the event. <tt>GetCategory</tt> will give you the "category" of the event. This will be Focus, Keyboard, Mouse, Window, Notification, Menu, or PropertyChange. <tt>GetName</tt> returns the name of the object that created the action, this will be things like "K" for the k key on the keyboard or "Sodbeans 3.5" for the Sodbeans window. <tt>GetAction</tt> action will tell you what happened in the event, and if there is more description for the event you can use the <tt>GetMoreActionInformation</tt> action. Use any or all of these actions to create a sentence to describe the event and use the <tt>Speech</tt> object to say your sentence.
+  We will also need to implement the blueprint action <tt>ReceiveEvent</tt> in our <tt>Observer</tt> class. In <a href="lab6_4.php">lab 6_4</a> we used the <tt>GetDescription</tt> action to explain the event, but you can also get the individual aspects of the event and make your own sentence to describe the event. The FocusEvent class has quite a few actions that you can use to get individual information about the event.
+</p>
+<ul>
+  <li>
+    <strong>GetCategory():</strong> For FocusEvents this will always be "Focus".
+  </li>
+  <li>
+    <strong>GetAction():</strong> This returns a short indication of what was focused (e.g. Desktop, SwitchWindow, MouseCaptureStart).
+  </li>
+  <li>
+    <strong>GetComponent():</strong> This returns a short description of the type of element that was focused (e.g. Button, Window, MenuItem).
+  </li>
+  <li>
+    <strong>GetComponentName():</strong> This returns the name of the component that was focused(e.g. Cancel, Sodbeans 3.5, File).
+  </li>
+  <li>
+    <strong>GetKeyboardShortcut():</strong> This returns the keyboard shortcut of the component that was focused.
+  </li>
+  <li>
+    <strong>GetChildCount():</strong> This returns the number of children the event has For example, when you focus on a list it will tell you the number of items that list has in it.
+  </li>
+  <li>
+    <strong>GetChild(integer index):</strong> This returns an AccessibleChild representing the child at the index of the parameter, if it is a valid index.
+  </li>
+</ul>
+<p>
+  Use any or all of these actions to create a description of the event and use the <tt>Speech</tt> object to say it.
 </p>
 <pre class="code">
   action ReceiveEvent(AccessibilityEvent event)
-	speech:Say("The " + event:GetMoreActionInformation() + ", " + event:GetName() + ", was focused.")
+	speech:Say("The " + event:GetComponent() + ", " + event:GetComponentName() + ", was focused. Its keyboard shortcut is " + event:GetKeyboardShortcut() + ".")
   end
 </pre>
 <p>
@@ -60,11 +87,11 @@
 
 <h2>Task 3: Using the Observer class</h2>
 <p>
-  Now that you have created the <tt>Observer</tt> class, you can add an instance of it to an instance of <tt>AccessibilityManager</tt>. You can then start the <tt>AccessibilityManager</tt> and begin listening for events just as you did in <a href="lab6_4.php">lab 6_4</a>.
+  Now that we have implemented the <tt>Observer</tt> class, we can add an instance of it to our of <tt>AccessibilityManager</tt>. You can then start the <tt>AccessibilityManager</tt> and begin listening for events just as you did in <a href="lab6_4.php">lab 6_4</a>.
 </p>
 <h2>Sample Output</h2>
 <p>
-  When you run the program it should not do anything until you focus something (open a new window or select a button or menu). Once you do that it should say out loud the sentence you created.  
+  When you run the program it should not do anything until you focus on something (open a new window or click a button or menu). Once you do that it should say out loud the sentence you created.  
 </p>
 <p>
   When you are done, debug and fix any errors, then show your code to your instructor.
