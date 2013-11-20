fb4182331c696f59ae2f7f205e5edd064d11c8bf
Andreas Stefik <stefika@gmail.com>
2013-11-19 22:18:48 -0800

    Deleted the Documentation folder and run folder, which should not have been committed.

--- quorum/Documentation/static/templates/user-signinheader.template.php
+++
@@ -1,7 +0,0 @@
-<div class="user-controls-loggedout">
-	<span class="signin-signup-message">
-		<a class="btn btn-primary" data-toggle="modal" data-target="#modal-login">Sign In</a>
-		<em>or</em>
-  		<a class="btn" data-toggle="modal" data-target="#modal-registration">Sign Up</a>
-	</span>
-</div>
