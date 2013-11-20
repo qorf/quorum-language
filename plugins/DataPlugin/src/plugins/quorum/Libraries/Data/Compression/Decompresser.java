6b464c2ce537121566ba953fdd286846df2b45ea
stefika <stefika@stefik02.cs.unlv.edu>
2013-11-20 14:52:08 -0800

    Added a first version of the decompression plugin.

---
+++ plugins/DataPlugin/src/plugins/quorum/Libraries/Data/Compression/Decompresser.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package plugins.quorum.Libraries.Data.Compression;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipInputStream;
+import quorum.Libraries.System.File$Interface;
+import java.util.zip.InflaterInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+/**
+ *
+ * @author stefika
+ */
+public class Decompresser {
+    public java.lang.Object $me = null;
+    boolean isGZip = true;
+    
+    public void DecompressGZip(File$Interface input, File$Interface output) {
+        try {
+            String path = input.GetAbsolutePath();
+            String outPath = output.GetAbsolutePath();
+            GZIPInputStream stream = new GZIPInputStream(new FileInputStream(path));
+            write(outPath, stream);
+            stream.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Decompresser.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Decompresser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public void DecompressZip(File$Interface input) {
+        try {
+            String path = input.GetAbsolutePath();
+            ZipFile zip = new ZipFile(path);
+            Enumeration<? extends ZipEntry> entries = zip.entries();
+            while(entries.hasMoreElements()) {
+                ZipEntry next = entries.nextElement();
+                String name = next.getName();
+                //ignore the junk on mac if it exists
+                if(!name.startsWith("__MACOSX")) {
+                    String entryPath = input.GetWorkingDirectory() + "/" + next.getName();
+                    InputStream stream = zip.getInputStream(next);
+                    write(entryPath, stream);
+                    stream.close();
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Decompresser.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Decompresser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private void write(String outFilePath, InputStream inputStream) {
+        try {
+            OutputStream out = new FileOutputStream(outFilePath);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = inputStream.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            
+            out.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Decompresser.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Decompresser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static String Unzip(String inFilePath) throws Exception {
+        ZipInputStream gzipInputStream = new ZipInputStream(new FileInputStream(inFilePath));
+        String outFilePath = inFilePath.replace(".gz", ".xml");
+        OutputStream out = new FileOutputStream(outFilePath);
+
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = gzipInputStream.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+
+        gzipInputStream.close();
+        out.close();
+
+        new File(inFilePath).delete();
+
+        return outFilePath;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            quorum.Libraries.System.File input = new quorum.Libraries.System.File();
+            input.SetWorkingDirectory("/Users/stefika/Desktop/ziptest");
+            //input.SetWorkingDirectory("/Users/stefika/Desktop/");
+            input.SetPath("test.zip");
+            //input.SetPath("Final.pdf.gz");
+            
+            quorum.Libraries.System.File output = new quorum.Libraries.System.File();
+            output.SetWorkingDirectory("/Users/stefika/Desktop/");
+            //file.SetPath("472 Fall.odt.zip");
+            output.SetPath("Test.pdf");
+            // TODO code application logic here
+            Decompresser decompress = new Decompresser();
+            //decompress.DecompressGZip(input, output);
+            decompress.DecompressZip(input);
+            
+            
+        } catch (Exception ex) {
+            Logger.getLogger(Decompresser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
