bbebb3fcff43ca24479e19a1dd8826b965ca558e
Andreas Stefik <stefika@gmail.com>
2014-05-16 11:37:18 -0700

    Added in a way to run Javascript from Quorum for the unit tester.

--- plugins/ParserPlugin/src/plugins/quorum/Libraries/Language/Compile/Test/CompilerTestSuite.java
+++ plugins/ParserPlugin/src/plugins/quorum/Libraries/Language/Compile/Test/CompilerTestSuite.java
@@ -3,8 +3,13 @@ package plugins.quorum.Libraries.Language.Compile.Test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import quorum.Libraries.System.File$Interface;
 import quorum.Libraries.Language.Compile.Test.CompilerTestResult;
 import quorum.Libraries.Language.Compile.Test.CompilerTestResult$Interface;
@@ -54,6 +59,30 @@ public class CompilerTestSuite {
                 result.Get$Libraries$Language$Compile$Test$CompilerTestResult$lines().Add(text);
             }
         } while (line != null);
+        return result;
+    }
+    
+    public CompilerTestResult$Interface RunJavaScript(String script) {
+        CompilerTestResult$Interface result = new CompilerTestResult();
+        ScriptEngineManager factory = new ScriptEngineManager();
+        ScriptEngine engine = factory.getEngineByName("JavaScript");
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            engine.getContext().setWriter(pw);
+            Object eval = engine.eval(script);
+            
+            String output = sw.toString();
+            String lines[] = output.split("\\r?\\n");
+            for(int i = 0; i < lines.length; i++) {
+                Text text = new Text();
+                text.SetValue(lines[0]);
+                result.Get$Libraries$Language$Compile$Test$CompilerTestResult$lines().Add(text);
+            }
+            result.Set$Libraries$Language$Compile$Test$CompilerTestResult$ranWithoutError(true);
+        } catch (ScriptException ex) {
+            Logger.getLogger(CompilerTestSuite.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return result;
     }
 }
