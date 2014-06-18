c0c909c668f5eb4d0367ac2589a2d8671296bea9
evanpierzina <evanpierzina@131.216.23.167>
2014-06-18 16:10:54 -0700

    Slight Adjustments to Part 6 of the Hour of Code.

--- quorum/libraries/documents/hourofcode/part6.php
+++ quorum/libraries/documents/hourofcode/part6.php
@@ -4,7 +4,7 @@
     
     //slide array
     var slideArray = new Array();
-    slideArray[0] = "<h5 role=\"heading\">It's my birthday!</h5><ul><li role=\"listitem\">Sadly lsat week, our lab pet Rufus didn't make it.</li><li>But today is my birthday and I am trying to think positive, for starters Dr. Day remembered my birthday.</li><li>For my birthday he told me I can pick a couple more letters to insert into the corresponding nucleotides into the small specimens around the lab.</li><li>He said this will change life as we know it, which means I get to create superpowers!</li>";
+    slideArray[0] = "<h5 role=\"heading\">It's my birthday!</h5><ul><li role=\"listitem\">Sadly last week, our lab pet Rufus didn't make it.</li><li>But today is my birthday and I am trying to think positive, for starters Dr. Day remembered my birthday.</li><li>For my birthday he told me I can pick a couple more letters to insert into the corresponding nucleotides into the small specimens around the lab.</li><li>He said this will change life as we know it, which means I get to create superpowers!</li>";
     slideArray[1] = "<h5 role=\"heading\">Today's Notes.</h5><ul><li role=\"listitem\"><li>I chose to use letters X and Y to add into the nucleotides.</li><li>It looks like GATTACA is the only DNA structure compatible to create superbugs, so that will stay the same.</li><li>DNA is also only capable of accepting one of the synthetic nucleotides before it becomes unstable… so I can't just add X's and Y's everywhere.</li><li>I just have to inject an X or a Y into the mutant DNA sequence and see how the Superbug will turn out.</li>";
     slideArray[2] = "<h5 role=\"heading\">Today's Notes.</h5><ul><li role=\"listitem\"><li>To find out how the Superbug will turn out I put the DNA sequence into the GenerateBug action.</li><li>I put the DNA into the generator by typing the GenerateBug action with my DNA variable inside the parentheses.</li>";
     slideArray[3] = "";
@@ -26,7 +26,15 @@
     <h2>Try It! <span class="hour-of-code-IDE-subtitle">Enter Quorum code below and press "Run" to execute it</span></h2>
     <div id="hour-of-code-IDE-controller">
     </div>
-        <textarea id="hour-of-code-IDE-input" class="ide inputArea" role="textbox" aria-multiline="true"></textarea>
+        <textarea id="hour-of-code-IDE-input" class="ide inputArea" role="textbox" aria-multiline="true">Biology bio
+
+text mutantBug = "XGATTACA"
+
+text opponentBug = "GATTAXCA"
+
+text result = bio:BattleBugs(mutantBug, opponentBug)
+
+output result</textarea>
         <pre id="hour-of-code-IDE-output" class="outputArea" role="log" aria-live="polite"></pre><div id="run-button" class="btn-group">
                 <a class="btn btn-success" href="#">Run</a>
         </div></div><div id="video-container">
