b2f571c4e616a8d891afb7bac1f8b7f41f1cb1a6
stefika <stefika@192.168.17.17>
2014-10-03 15:11:56 -0700

    More plugins for Quorum 3.

---
+++ Quorum3Plugins/SystemPlugin/src/plugins/quorum/Libraries/System/Properties.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plugins.quorum.Libraries.System;
+
+/**
+ *
+ * @author astefik
+ */
+public class Properties {
+    public java.lang.Object $me = null;
+    public String GetPropertyNative(String key) {
+        String property = System.getProperty(key);
+        return property;
+    }
+    public boolean HasPropertyNative(String key) {
+        String property = System.getProperty(key);
+        if (property == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    public String GetEnvironmentVariableNative(String key) {
+        String env = System.getenv(key);
+        return env;
+    }
+    
+    public boolean HasEnvironmentVariableNative(String key) {
+        String property = System.getenv(key);
+        if (property == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
