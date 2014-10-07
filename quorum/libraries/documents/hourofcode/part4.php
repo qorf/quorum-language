05a763b1136a5f301f961a06c55fe7b80ab5e8cc
Patrick Daleiden <daleiden@unlv.nevada.edu>
2014-10-07 16:36:44 -0700

    Modifications to Part 4

--- quorum/libraries/documents/hourofcode/part4.php
+++ quorum/libraries/documents/hourofcode/part4.php
@@ -4,15 +4,46 @@
     
     //slide array
     var slideArray = new Array();
-    slideArray[0] = "<h5>Control Structures: Repeat</h5><ul><li>Let's say we want to list some even numbers.  We could use output statements for each one, but what if we had 100 numbers to output or even 1000?  Sometimes it makes more sense to tell the computer to do the repetitive work for us.</li><li>We can accomplish this with the use of the word <code>repeat</code>.</li><li>Just like with the <code>if</code> statements when we told the computer where the end of the code block was, we must always use the word <code>end</code> to indicate where the section of code being repeated ends.</li><li>In this example, we will explore three different ways to control the repitition of code: <code>repeat times</code>, <code>repeat while</code> and <code>repeat until</code>.</li>";
+    slideArray[0] = "<h5>Control Structures: Repeat</h5>\n\
+        <ul>\n\
+        <li>If we want the computer to repeat a task multiple times, we can use a loop in our program.  This is usually a lot less work than typing the instruction repeatedly and more flexible because we can have the instructions executed a variable number of times.</li>\n\
+        <li>In Quorum, we create a loop using the keyword <code>repeat</code>.</li>\n\
+        <li>We mark the end of a loop in the same way we marked the end of an <code>if</code> statement, by using the keyword <code>end</code>.</li>\n\
+        <li>In this example, we will explore three different ways to control loops using the statements: <code>repeat {number} times</code>, <code>repeat while {condition}</code> and <code>repeat until {condition}</code>.</li>\n\
+        </ul?";
     
-    slideArray[1] = "<h5>Control Structures: Repeat Times</h5><ul><li>When it comes to repeating, sometimes we know how many times we want the code to run.</li><li>If we want to do something 5 times, we can just tell the computer <code>repeat 5 times</code> and then the computer knows to execute any lines of code in the repeat block 5 times.</li><span class=\"title\">Try it!</span><div class =\"task\"><p>Output five even numbers:<br><code>number evenNumber = 0</code><br><code>repeat 5 times</code><br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<code>output evenNumber</code><br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<code>evenNumber = evenNumber + 2</code><br><code>end</code></p></div></ul>";
+    slideArray[1] = "<h5>Control Structures: Repeat Times</h5>\n\
+        <ul>\n\
+        <li>The most basic type of loop involves just telling the computer how many times to repeat something.</li>\n\
+        <li>In Quorum, if we want to do something 5 times, we can type: <code>repeat 5 times</code> or if we have a numeric variable, we can place that in the \"5\" position like: <code>repeat x times</code>.</li>\n\
+        <span class=\"title\">Try it!</span>\n\
+        <div class =\"task\"><p>Output five even numbers:<br><code>number evenNumber = 2</code><br><code>repeat 5 times</code><br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<code>output evenNumber</code><br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<code>evenNumber = evenNumber + 2</code><br><code>end</code></p>\n\
+        </div>\n\
+        </ul>";
     
-    slideArray[2] = "<h5>Control Structures: Repeat While</h5><ul><li>Sometimes we only want to repeat while a certain condition is true.</li><li>We can get the same result as the other repeat statement since we know it should keep going while evenNumber is less than 10.</li><li>The way we denote \"less than\" in Qurorum is with the operator <code><</code>.</li><li>Notice that evenNumber will hold the value 10 at the end, but never gets output because our repeat condition is false.</li><span class=\"title\">Try it!</span><div class =\"task\"><p>Output five even numbers by changing the repeat statement to: <code>repeat while evenNumber < 10</code></p></div></ul>";
+    slideArray[2] = "<h5>Control Structures: Repeat While</h5>\n\
+        <ul>\n\
+        <li>An alternative type of loop is to repeat a block of code while a certain condition is met.</li>\n\
+        <li>In Quorum we do this by using a <code>repeat while</code> statement. After the keyword <code>while</code>, we include a condition, just like we did with an <code>if</code> statement.</li>\n\
+        <li>If we want to mimic the behavior of the last example, we could just repeat while our variable is less than or equal to 10.</li>\n\
+        <li>Note that the value of the variable will be 12, but the output statement is skipped when the repeat condition is false.</li>\n\
+        <span class=\"title\">Try it!</span>\n\
+        <div class =\"task\"><p>Output five even numbers again by changing the repeat statement to: <code>repeat while evenNumber <= 10</code></p></div>\n\
+        </ul>";
     
-    slideArray[3] = "<h5>Control Structures: Repeat Until</h5><ul><li>When we know exactly the condition(s) under which the repeat should stop, we can use <code>repeat until</code>.</li><li>Whereas <code>repeat while</code> stops once the given condition is false, <code>repeat until</code>, stops once the given condition is true.</li><li>Following the other repeat examples, we can get the equivalent output by telling our program to repeat until evenNumber hits 10.</li><span class=\"title\">Try it!</span><div class =\"task\"><p>Output five even numbers by changing the repeat statement to: <code>repeat until evenNumber = 10</code></p></div></ul>";
+    slideArray[3] = "<h5>Control Structures: Repeat Until</h5>\n\
+        <ul>\n\
+        <li>The final way of representing a loop in Quorum is to use a <code>repeat until</code> statement, which is very similar to the <code>repeat while</code> loop.</li>\n\
+        <li>The difference is that the <code>repeat until</code> loop stops when a specific condition is met instead of repeating while a condition is met.</li>\n\
+        <li>You can represent the same logical control with either format, so it is your choice which one you use, but sometimes one is more natural than the other.</li>\n\
+        <span class=\"title\">Try it!</span>\n\
+        <div class =\"task\"><p>Output five even numbers by changing the repeat statement to: <code>repeat until evenNumber > 10</code></p></div>\n\
+        </ul>";
     
-    slideArray[4] = "<h5>Control Structures: Additional Information</h5><ul><li>More documentation on <code>repeat</code> can be found <a href=\"http://quorumlanguage.com/documents/syntax/repeat.php\">here</a>.</li></ul>";
+    slideArray[4] = "<h5>Control Structures: Additional Information</h5>\n\
+        <ul>\n\
+        <li>More documentation on <code>repeat</code> can be found <a href=\"http://quorumlanguage.com/documents/syntax/repeat.php\">here</a>.</li>\n\
+        </ul>";
     
     $(document).ready(function(){
         $('#IDE-input').text('number evenNumber = 0\nrepeat 5 times\n        output evenNumber\n        evenNumber = evenNumber + 2\nend');
