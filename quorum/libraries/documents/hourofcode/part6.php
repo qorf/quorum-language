b23b6dd4c4fdb4e060e97d64db3d9ae94cbd8121
Patrick Daleiden <daleiden@unlv.nevada.edu>
2014-10-08 13:42:57 -0700

    Modifications to Part 6

--- quorum/libraries/documents/hourofcode/part6.php
+++ quorum/libraries/documents/hourofcode/part6.php
@@ -4,17 +4,64 @@
     
     //slide array
     var slideArray = new Array();
-    slideArray[0] = "<h5>Actions</h5><ul><li>Sometimes when we're programming, we may find ourselves using the same code over and over again in different parts of our program.</li><li>With actions, we can eleminate the duplicate code.</li><li>In a nutshell, actions hold code that can be executed from anywhere in your program, whenever you need it.</li><li>In fact, we have already been using an action all along called <code>Main</code>.  Quorum automatically generates this for us if we don't need to use any other actions (like in the previous excercises).</li><li>The computer always starts executing our programs from the <code>Main</code> action.</li></ul>";
+    slideArray[0] = "<h5>Actions</h5>\n\
+        <ul>\n\
+        <li>Often times inside a program, we want to run the same code sections many times in different places. We usually want to eliminate this duplicate code to save time, reduce errors and make things easier to change.</li>\n\
+        <li>We do this by writing procedures that we call whenever we want code to run.  In Quorum, these are called actions.</li>\n\
+        <li>You didn't realize it, but we have actually already been using an action called <code>Main</code>. The computer always starts running our program from our <code>Main</code> action.  We haven't had to use it so far, because Quorum automatically puts our code inside a <code>Main</code> action if we don't use any other actions.</li>\n\
+        <li>Now that we are going to use other actions though, we always need to include it.</li>\n\
+</ul>";
     
-    slideArray[1] = "<h5>Creating Actions: Part 1</h5><ul><li>We create actions by starting a line with the word <code>action</code> followed by the name of the action.  Unlike variable names, action names should start with a capital letter.</li><li>Since actions can contain however many lines of code we want in them, they will also need an <code>end</code> so the computer knows where the action stops.</li><span class=\"title\">Try it!</span><div class =\"task\"><p>Create the Main action by typing <code>action Main</code> with an appropriate <code>end</code> below it.</p></div></ul>";
+    slideArray[1] = "<h5>Creating Actions: Part 1</h5>\n\
+        <ul>\n\
+        <li>In Quorum to create an action, we use the keyword <code>action</code> followed by the name of the action.  We usually capitalize the first letter of an action name, but it is not required. Naming rules for actions are the same as variables, it must start with a letter.</li>\n\
+        <li>Since we are going to have a code block inside the action, we also need to include an <code>end</code> to mark where the action ends.</li>\n\
+        <span class=\"title\">Try it!</span><div class =\"task\"><p>Lets start a new program by creating a Main action:<br>\n\
+        <code>action Main</code><br>\n\
+        <code>end</code></p>\n\
+        </div>\n\
+        </ul>";
     
-    slideArray[2] = "<h5>Creating Actions: Part 2</h5><ul><li>Now that the Main action has been created, we can type all of the code we're used to from before inside of it.</li><li>This also means we can start creating more actions to perform a set of instructions for us at any point.</li><span class=\"title\">Try it!</span><div class =\"task\"><p>Create an action called <code>OutputTest</code> underneath the Main action (below the <code>end</code>).</p></div></ul>";
+    slideArray[2] = "<h5>Creating Actions: Part 2</h5>\n\
+        <ul>\n\
+        <li>You can put any type of code that you've learned so far inside the <code>Main</code> action or call other actions from it.</li>\n\
+        <li>We are going to create another action now that we will call from the <code>Main</code> action.</li>\n\
+        <li>To do this, just create a new action after the <code>end</code> statement of the <code>Main</code> action.</li>\n\
+            <span class=\"title\">Try it!</span><div class =\"task\"><p>Create an action called <code>PrintMsg</code> after the <code>Main</code> action:<br>\n\
+        <code>action PrintMsg</code>.<br>\n\
+        <code>end</code>.</p></div>\n\
+        </ul>";
     
-    slideArray[3] = "<h5>Creating Actions: Part 3</h5><ul><li>With both of our actions set up, it's time to put some code in them.</li><span class=\"title\">Try it!</span><div class =\"task\"><p>Add an output statement in each action.</p></div></ul>";
+    slideArray[3] = "<h5>Creating Actions: Part 3</h5>\n\
+        <ul>\n\
+        <li>At the moment, we have two actions in our program, but no code to run inside the actions.  We'll learn how to call an action in a moment, but first, let's write some code inside the <code>PrintMsg</code> action. </li>\n\
+        <li>To keep our example simple, we are going to write a simple output message to the screen.</li>\n\
+        <span class=\"title\">Try it!</span>\n\
+        <div class =\"task\"><p>Insert an <code>output</code> statement inside the <code>PrintMsg</code> action:<br>\n\
+        <code>output \"Programming in Quorum is Fun!!!\"</p></div>\n\
+        </ul>";
     
-    slideArray[4] = "<h5>Calling Actions</h5><ul><li>If we run our code now, we should notice that the code in our <code>OutputTest</code> action never executes.  This is because the program always starts at the <code>Main</code> action and we never told it to run the code in our other action.</li><li>The last thing we need to do now is call the <code>OutputTest</code> action from our <code>Main</code> action.</li><li>We can accomplish this by adding the line <code>OutputTest()</code> in our Main action.  The parentheses are necessary at the end of the action name for the computer to know it's an action.</li><span class=\"title\">Try it!</span><div class =\"task\"><p>Add a call to OutputTest within the Main action with the code <code>OutputTest()</code> and try running it.</p></div></ul>";
+    slideArray[4] = "<h5>Calling Actions</h5>\n\
+        <ul>\n\
+        <li>Now, in order to complete this program we need to call our <code>PrintMsg</code> action from our <code>Main</code> action so that it will execute.</li>\n\
+        <li>In Quorum, this is very easy to do, we just insert a line of code in the <code>Main</code> action that redirects the computer to the other action.  We do this adding the line <code>OutputTest()</code> to the <code>Main</code> action.</li>\n\
+        <li>The parentheses are necessary after the action name to notify the computer that it should run an action.</li>\n\
+        <span class=\"title\">Try it!</span>\n\
+        <div class =\"task\"><p>Call <code>OutputTest</code> from <code>Main</code>:<br>\n\
+        <code>OutputTest()</code></p></div>\n\
+        </ul>";
     
-    slideArray[5] = "<h5>Actions: More Information</h5><ul><li>We've covered the very basics of actions up to this point.</li><li>Actions for the online version of Quorum are currently limited, but the offline version can do things such as pass parameters and return values.</li><li>For more information on actions, click <a href=\"http://quorumlanguage.com/documents/syntax/actions.php\">here</a></li></ul>";
+    slideArray[5] = "<h5>Actions: More Information</h5>\n\
+        <ul>\n\
+        <li>Here is our full example:<br>\n\
+        <code>action Main</code><br>\n\
+        <code>&nbsp&nbsp&nbsp&nbspPrintMsg()</code><br>\n\
+        <code>end</code><br><br>\n\
+        <code>action PrintMsg</code><br>\n\
+        <code>&nbsp&nbsp&nbsp&nbspoutput \"Programming in Quorum is Fun!!!\"</code><br>\n\
+        <code>end</code><br>\n\</li>\n\<li>Actions are an important and useful concept in programming, but we've only covered the basics so far. You can also do things like pass variables to an action and return values from an action.</li>\n\
+        <li>For more information on actions, click <a href=\"http://quorumlanguage.com/documents/syntax/actions.php\">here</a></li>\n\
+        </ul>";
     
     $(document).ready(function(){
         $('#IDE-input').text('');
