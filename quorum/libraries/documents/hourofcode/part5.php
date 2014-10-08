0542676442229804f2b07a536e00b9ec5b5c818b
Patrick Daleiden <daleiden@unlv.nevada.edu>
2014-10-08 13:42:49 -0700

    Modifications to Part 5

--- quorum/libraries/documents/hourofcode/part5.php
+++ quorum/libraries/documents/hourofcode/part5.php
@@ -1,21 +1,55 @@
 <?php require_once("../../static/templates/pageheader.template.php"); ?>
 <script type="text/javascript">
-    document.title = 'Hour of Code: Part 4 | Quorum Programming Language';
+    document.title = 'Hour of Code: Part 5 | Quorum Programming Language';
     
     //slide array
     var slideArray = new Array();
-    slideArray[0] = "<h5>Putting It All Together</h5><ul><li>We've learned quite a bit up to this point.  In this exercise, we're going to use all of our past knowledge to make a program that can detect if a number is even or odd.</li><li>Our first step is to set our starting number and tell the computer which number we want to stop at.</li><span class=\"title\">Try it!</span><div class =\"task\"><p>Create a number variable called <code>testNumber</code> that holds a starting number of your choosing.<br>Next, set up a repeat statement using <code>repeat until</code> and tell the computer what number it should stop at.  Also, don't forget to <code>end</code> the repeat.</p></div></ul>";
+    slideArray[0] = "<h5>Putting It All Together</h5>\n\
+        <ul>\n\
+        <li>We've learned quite a bit up to this point and now it's time use all of our new skills to write a program that will detect if a number is even or odd.</li>\n\
+        <li>We are going to test a range of numbers, so we need to loop from a starting number to a stopping number.</li>\n\
+        <span class=\"title\">Try it!</span>\n\
+        <div class =\"task\"><p>First create a number variable called <code>testNumber</code> and set it to your starting number.<br>Next, set up a repeat loop using <code>repeat until</code> and for the condition specify the number to stop at.<br>Finally, don't forget to include <code>end</code> to mark the end of the loop.</p></div>\n\
+        </ul>";
     
-    slideArray[1] = "<h5>Implementing the Logic: Part 1</h5><ul><li>Now that we have our repeat statement set up, we need to tell the computer what it should do while it's repeating.</li><li>The way we will decide if a number is even or odd is with the modulus (%) operator</li><li>Since the modulus operator gives us the remainder from diving two numbers, and any even number is divisible by 2 (meaning it has a remainder of 0), then we can use the modulus operator to tell if a number is even or odd.</li><span class=\"title\">Try it!</span><div class =\"task\"><p>Create an if statement using the condition <code>if testNumber % 2 = 0</code>.<br>Also, don't forget to <code>end</code> the if statement</p></div></ul>";
+    slideArray[1] = "<h5>Implementing the Logic: Part 1</h5>\n\
+        <ul>\n\
+        <li>Now that we have the <code>repeat</code> loop set up, we need to complete the code block inside the loop that will be repeated.</li>\n\
+        <li>We will use the modulus operator <code>%</code> to determine if a number is even or odd. Remember that the modulus operator returns the remainder when dividing two numbers, so any even number divided by two will have a zero remainder and any odd number will have a remainder of 1.</li>\n\
+        <li>So all we need to do is check if the remainder is zero.</li>\n\
+        <span class=\"title\">Try it!</span>\n\
+        <div class =\"task\"><p>Create an <code>if</code> statement inside the <code>repeat</code> loop checking the modulus of the variable:<br><code>if testNumber % 2 = 0</code>  End the statement with <code>end</code>.</div>\n\
+        </ul>";
     
-    slideArray[2] = "<h5>Implementing the Logic: Part 2</h5><ul><li>We've created our condition for even numbers, meaning anything that makes it through is guaranteed to be even.</li><li>Of course, if a number is not even then we know it is always an odd number instead, so we can use the conditionless else statement to know when we have an odd number.</li><span class=\"title\">Try it!</span><div class =\"task\"><p>In between the <code>if</code> statement to catch even numbers and the <code>end</code> of it, place an <code>else</code> clause to catch the odd numbers.<br>Also be sure to add 1 to <code>currentNumber</code> between the if's <code>end</code> and the repeat's <code>end</code>, otherwise the computer will repeat forever!</p></div></ul>";
+    slideArray[2] = "<h5>Implementing the Logic: Part 2</h5>\n\
+        <ul>\n\
+        <li>Our <code>if</code> statement is now set up for even numbers.  If we want alternative instructions to run if the number is odd, we need some kind of <code>else</code> condition as well.</li>\n\
+        <li>Since there are only two possibilities (even or odd), we can use the simple default <code>else</code> statement here.</li>\n\
+        <span class=\"title\">Try it!</span>\n\
+        <div class =\"task\"><p>Before the <code>end</code> in the <code>if</code> statement, insert a line with <code>else</code> to set up the block for odd numbers.<br>\n\
+Finally, we need to increment our testing variable by one on each pass of the loop or it will repeat forever.  To do this, insert a line <code>testNumber = testNumber + 1</code> just after the end of the <code>if</code> block.</p></div>\n\
+        </ul>";
     
-    slideArray[3] = "<h5>Being Creative</h5><ul><li>Since we now know how to check a number's parity (whether it's even or odd), we can proceed to do whatever we want with the even numbers and the odd numbers.</li><li>One thing that may be useful is to output whether the number is even or odd.  On the next slide there's an example of the code for doing just that.</li><li>Feel free to add or change code and experiment for yourself!</li></ul>";
+    slideArray[3] = "<h5>Being Creative</h5>\n\
+        <ul>\n\
+        <li>So now we have a logical code structure set up to execute different instructions if a number is even or if it is odd inside a repeat loop that will test a range of numbers.</li>\n\
+        <li>All we need to do now is write some instructions inside the code blocks!</li>\n\
+        <li>For this example, lets just use an <code>output</code> statement to print a message depending on whether the number is even or odd. You can also try a <code>say</code> statement if you want to hear it. Try something like <code>output testNumber + \" is even.\"</code> in the first block.</li>\n\
+        <li>The next slide has the full code for this section...experiment with other ideas yourself.</li>\n\
+        </ul>";
     
-    slideArray[4] = "<h5>Example Code</h5><p class =\"code\">number testNumber = 0<br>repeat until testNumber = 10<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspif testNumber % 2 = 0<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspoutput testNumber + \" is even\"<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspelse<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspoutput testNumber + \" is odd\"<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsptestNumber = testNumber + 1<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspend<br>end</p>";
+    slideArray[4] = "<h5>Example Code</h5><p class =\"code\">number testNumber = 0<br>\n\
+        repeat until testNumber = 10<br>\n\
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspif testNumber % 2 = 0<br>\n\
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspoutput testNumber + \" is even\"<br>\n\
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspelse<br>\n\
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspoutput testNumber + \" is odd\"<br>\n\
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspend<br>\n\
+        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsptestNumber = testNumber + 1<br>\n\
+        end</p>";
     
     $(document).ready(function(){
-        $('#IDE-input').text('number testNumber = 0\nrepeat until testNumber = 10\n        if testNumber % 2 = 0\n                output testNumber + \" is even\"\n        else\n                output testNumber + \" is odd\"\n        testNumber = testNumber + 1\n        end\nend');
+        $('#IDE-input').text('number testNumber = 0\nrepeat until testNumber = 10\n    if testNumber % 2 = 0\n        output testNumber + \" is even\"\n    else\n        output testNumber + \" is odd\"\n    end\n    testNumber = testNumber + 1\nend');
     });
 </script>
 <?php include("slideshow.php"); ?>
