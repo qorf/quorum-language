3734d4285483bd8b32b404bf94a945ad7ab3a8b8
Patrick Daleiden <daleiden@unlv.nevada.edu>
2014-10-08 13:43:06 -0700

    Modifications to Part 7

--- quorum/libraries/documents/hourofcode/part7.php
+++ quorum/libraries/documents/hourofcode/part7.php
@@ -4,15 +4,55 @@
     
     //slide array
     var slideArray = new Array();
-    slideArray[0] = "<h5>Hour of Code Review - Variables</h5><ul><li>Variables are just containers for information.</li><li>To create a variable, we must declare the type of variable and give it a name.</li><li>There are many type of variables, each for the specific kinds of data we want to store in them.</li><li>We can use <code>text</code>, <code>integer</code>, <code>number</code> and <code>boolean</code> variables when needed.</li><li>When creating a variable name, start with a lower-case letter.</li><li>More information on variables can be found <a href=\"http://quorumlanguage.com/documents/syntax/types.php\">here</a>.</li></ul>";
+    slideArray[0] = "<h5>Hour of Code Review - Variables</h5>\n\
+        <ul>\n\
+        <li>Variables are containers for information.</li>\n\
+        <li>To create a variable, declare the type of the variable followed by the name.</li>\n\
+        <li>To assign a value into a variable, use the <code>=</code> operator.</li>\n\
+        <li>There are four main types of variables in Quorum, each for specific kinds of data:<br>\n\
+        &nbsp&nbsp&nbsp&nbsp<code>text</code> for text strings<br>\n\
+        &nbsp&nbsp&nbsp&nbsp<code>integer</code> for positive and negative whole numbers<br>\n\
+        &nbsp&nbsp&nbsp&nbsp<code>number</code> for real numbers (decimals)<br>\n\
+        &nbsp&nbsp&nbsp&nbsp<code>boolean</code> for true/false values.</li>\n\
+        <li>When creating a variable name, always start it with a letter (usually lower case, but this is not required).</li>\n\
+        <li>More information on variables can be found <a href=\"http://quorumlanguage.com/documents/syntax/types.php\">here</a>.</li>\n\
+        </ul>";
     
-    slideArray[1] = "<h5>Hour of Code Review - Output/Say Statements</h5><ul><li>An <code>output</code> statement will produce text on the screen for us to read.</li><li>On the other hand, <code>say</code> statements will speak the words aloud.</li><li>Some browsers do not support the <code>say</code> feature in Quorum's online mode.</li></ul>";
+    slideArray[1] = "<h5>Hour of Code Review - Output/Say Statements</h5>\n\
+        <ul>\n\
+        <li>An <code>output</code> statement will print text on the screen.</li>\n\
+        <li>A <code>say</code> statements will instruct the computer to speak the expression aloud.</li>\n\
+        <li>Note that some browsers do not support the <code>say</code> feature in Quorum's online mode.</li>\n\
+        </ul>";
     
-    slideArray[2] = "<h5>Hour of Code Review - If Statements</h5><ul><li>With <code>if</code> statements, we can let the computer decide whether or not to run certain code.</li><li>Every <code>if</code> statement requires a condition (variable > 5, variable = 5, etc.) and an <code>end</code>.</li><li>Within an <code>if</code> statement, we can type <code>elseif</code> to list another condition under which the computer should run code.  After that, we can then type the word <code>else</code> which tells the computer, \"None of the elseif conditions were met, so just run this code instead.\"</li><li>More information on if statements can be found <a href=\"http://quorumlanguage.com/documents/syntax/if.php\">here</a>.</li></ul>";
+    slideArray[2] = "<h5>Hour of Code Review - If Statements</h5>\n\
+        <ul>\n\
+        <li>Use an <code>if</code> statement to have the computer decide whether or not to run certain code.</li>\n\
+        <li>An <code>if</code> statement requires a condition to be evaluated (variable > 5, variable = 5, etc.) and an <code>end</code> statement to mark the end of the code block.</li>\n\
+        <li>An <code>if</code> statement can optionally contain an <code>elseif</code> statement to test another condition or a default <code>else</code> statement (or both) to handle any other possibility not listed.</li>\n\
+        <li>More information on <code>if</code> statements can be found <a href=\"http://quorumlanguage.com/documents/syntax/if.php\">here</a>.</li>\n\
+        </ul>";
     
-    slideArray[3] = "<h5>Hour of Code Review - Repeat</h5><ul><li>When we want to run some code multiple times, we can use a <code>repeat</code> statement.</li><li>Each <code>repeat</code> statement requires a condition for stopping and an <code>end</code>.</li><li>We state the condtion for stopping by using <code>repeat times</code>, <code>repeat while</code> or <code>repeat until</code>.</li><li>More information on repeats can be found <a href=\"http://quorumlanguage.com/documents/syntax/repeat.php\">here</a>.</li></ul>";
+    slideArray[3] = "<h5>Hour of Code Review - Repeat</h5>\n\
+        <ul>\n\
+        <li>When we want to run a section of code repeatedly, we use a <code>repeat</code> statement to create a loop.</li>\n\
+        <li>Like an <code>if</code> statement, a <code>repeat</code> statement requires a condition for stopping and an <code>end</code> statement.</li>\n\
+        <li>We specify the condition for stopping the loop by using one of these formats: <br>\n\
+        <code>repeat {number} times</code>,<br>\n\
+        <code>repeat while {condition}</code> or <br>\n\
+        <code>repeat until {condition}</code>.</li>\n\
+        <li>More information on repeat loops can be found <a href=\"http://quorumlanguage.com/documents/syntax/repeat.php\">here</a>.</li>\n\
+        </ul>";
     
-    slideArray[4] = "<h5>Hour of Code Review - Actions</h5><ul><li>We use actions to reduce the amount of duplicate code in our programs.</li><li>To create one, we type <code>action</code> followed by the action's name, which should start with a capital letter.</li><li>Remember that if we want to create our own actions, we must also declare the action <code>Main</code> somewhere in our program, because the computer always starts there.</li><li>To execute an action, we type the name of the action followed by parentheses.</li><li>More information on actions can be found <a href=\"http://quorumlanguage.com/documents/syntax/actions.php\">here</a></li></ul>";
+    slideArray[4] = "<h5>Hour of Code Review - Actions</h5>\n\
+        <ul>\n\
+        <li>Actions are used to reduce the amount of duplicate code in a program and to make it easier to read.</li>\n\
+        <li>To create an action, use the keyword <code>action</code> followed by the action's name, which usually starts with a capital letter.</li>\n\
+        <li>The computer always starts executing a program at the <code>Main</code> action, but if the program has only one action, Quorum will automatically insert the program code into a <code>Main</code> action.</li>\n\
+        <li>If a program has actions besides <code>Main</code>, the action <code>Main</code> must be defined somewhere in the program.</li>\n\
+        <li>To execute an action at a particular point in the program, type the name of the action followed by parentheses.</li>\n\
+        <li>More information on actions can be found <a href=\"http://quorumlanguage.com/documents/syntax/actions.php\">here</a></li>\n\
+        </ul>";
     
     $(document).ready(function(){
         $('#IDE-input').text('');
