d3933cd465fa63ebf33e473fbc4c5847370ff095
Patrick Daleiden <daleiden@unlv.nevada.edu>
2014-10-29 14:43:31 -0700

    Hour of Code slide fixes

--- quorum/libraries/documents/hourofcode/part2.php
+++ quorum/libraries/documents/hourofcode/part2.php
@@ -6,18 +6,18 @@
     var slideArray = new Array();
     slideArray[0] = "<h5 role=\"heading\">Number Variables</h5>\n\
         <ul>\n\
-        <li>Another type of variable often used in programming is a real number. In Quorum to create this type of variable we use the keyword <code>number</code>.</li>\n\
+        <li>Another type of variable often used in programming is a real (or decimal) number. In Quorum to create this type of variable we use the keyword <code>number</code>.</li>\n\
         <li>A <code>number</code> variable holds numeric values that can include decimal places such as 8.3439 or 42.0 or even 19.</li>\n\
         <li>Unlike text variables, we never use double quotes when storing a number, we just type it.</li>\n\
         <span class=\"title\">Try it!</span>\n\
-        <div class =\"task\">Create a number variable with the following code: <code>number pi = 3.14159</code>.</div>\n\
+        <div class =\"task\">Create a number variable with the following code: <code>number pi = 3.14159</code> then move to the next slide.</div>\n\
         </ul>";
     
     slideArray[1] = "<h5 role=\"heading\">Arithmetic with Number Variables</h5>\n\
         <ul>\n\
         <li>We can do arithmetic calculations with <code>number</code> variables, just like we would on the numbers themselves.</li>\n\
         <li>The computer substitutes the actual number in the variable into the formula and performs the calculation.</li>\n\
-        <li>For example, if we make a new variable that holds the radius of a circle, then we can calculate its area using the formula area = pi X radius X radius.  On a computer, the multiplication operater is the <code>*</code>.</li>\n\
+        <li>For example, if we make a new variable that holds the radius of a circle, then we can calculate its area using the formula area = pi X radius X radius.  On a computer, the multiplication operator is the <code>*</code>.</li>\n\
         <span class=\"title\">Try it!</span>\n\
         <div class =\"task\">Create a variable to hold your circle's radius: <code>number radius = 3</code> and then calculate the area: <code>number area = pi * radius * radius</code></div>\n\
         </ul>";
@@ -25,10 +25,10 @@
     slideArray[2] = "<h5 role=\"heading\">Make the Computer Talk with: Say</h5>\n\
         <ul>\n\
         <li>Quorum has a built in feature to instruct the computer to say things out loud.  This is done by using the keyword <code>say</code> followed by whatever we want it to say.</li>\n\
-        <li>Just like with output statements, the computer can say the contents of a stored variable or a string that we type in.</li>\n\
+        <li>Just like with output statements, the computer can say the contents of a variable or a string that we type in.</li>\n\
         <li><span class = \"bold\">Note:</span> Say statements may not work with all web browsers.</li>\n\
         <span class=\"title\">Try it!</span>\n\
-        <div class =\"task\">Tell the computer to say the result of the area calculation you just completed by typing a new line: <code>say \"The area is \" + area</code></div>\n\
+        <div class =\"task\">Tell the computer to say the result of the area calculation you just completed by typing a new line: <code>say \"The area is \" + area</code> and then Run your code.</div>\n\
         </ul>"; //operators, concat
     
     slideArray[3] = "<h5 role=\"heading\">Additional Information</h5>\n\
