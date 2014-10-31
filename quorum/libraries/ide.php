d026559eaffc506a7caa036ee17ab9659061c293
evanpierzina <evanpierzina@euler.cs.unlv.edu>
2014-10-30 17:07:13 -0700

    Made some edits that allow the output to be checked. Made various accessibility edits to the page header and front page.

--- quorum/libraries/ide.php
+++ quorum/libraries/ide.php
@@ -1,7 +1,8 @@
 <div id="IDE" class="small-IDE">
-<h2>Try Quorum!<span class="IDE-subtitle small-IDE-subtitle">Enter Quorum code below and press "Run" to execute it</span></h2>
-
-   <textarea id="IDE-input" class="ide inputArea small-IDE-input" role="textbox" aria-multiline="true"></textarea><div id="IDE-output-container">
+<h1>Try Quorum!<span class="IDE-subtitle small-IDE-subtitle">Enter Quorum code below and press "Run" to execute it</span></h1>
+    <label for="IDE-input" class="hidden">Code Area</label>
+    <textarea id="IDE-input" class="ide inputArea small-IDE-input" role="textbox" aria-multiline="true"></textarea><div id="IDE-output-container">
+        <h2 id="output-label" class="hidden">Output</h2>
         <pre id="IDE-output" class="outputArea  small-IDE-output" role="log" aria-live="polite"></pre><div id="run-button" class="btn-group">
                 <button class="btn btn-success small-IDE-run-button" href="#">Run</button>
         </div>
