fa05e7ca80c6fd9818abafeb05c2fccf9fde2be1
stefika <stefika@192.168.17.4>
2014-11-08 16:38:11 -0800

    Finished the first draft of the about page.

--- quorum/libraries/about.php
+++ quorum/libraries/about.php
@@ -1,3 +1,67 @@
 <?php include("static/templates/pageheader.template.php"); ?>
+<div class="hero-unit">
+	<div class="hero-unit-container">
+		<h1>About Quorum</h1>
+		<p>What is the Quorum Programming Language?</p>
+	</div>
+</div>
 
+<div class="content index-content">
+<h4>What kind of programming language is Quorum?</h4>
+<p align="justify">
+    Quorum is a general purpose tool that we call an "evidence-based" 
+    programming language. It started as an interpreted
+    language originally designed to be easier to hear through screen readers
+    for blind or visually impaired users. Eventually, Quorum became a general
+    purpose programming language designed for any user. Current versions compile to 
+    Java Bytecode and run on the Java Virtual Machine, similarly to JRuby, Jython, or Scala. 
+    Quorum 3.0 also compiles to JavaScript and can be run from the web. 
+</p>
+<h4>What are the high level technical details of Quorum?</h4>
+<p align="justify">
+    Quorum has many features. It is object-oriented, but this is largely hidden
+    from new users. It has a general purpose type system, with generics for 
+    containers (e.g., arrays, hash tables, lists). Quorum also has a 
+    standard library, which contains many additions to the language, like
+    math libraries, containers, web components, or other features.
+</p>
+
+<h4>How is Quorum funded?</h4>
+<p align="justify">
+    Quorum's initial funding started as part of a National Science Foundation 
+    project (NSF CNS-0940521). Current funding for Quorum also comes from the 
+    National Science Foundation (NSF CNS-1440878). Other funders include the Washington
+    State School for the Blind, Southern Illinois University Edwardsville, the 
+    University of Nevada, Las Vegas, the Do it program at the University of Washington, 
+    amongst others.
+</p>
+<h4>Who were the original developers of Quorum?</h4>
+<p align="justify">
+    The first version of Quorum was developed by Andreas Stefik. The modern version
+    of the language was written predominately by Andreas Stefik and Melissa Stefik, 
+    with important contributions by Jeff Wilson. Many others have made
+    contributions to the project.
+</p>
+<h4>What platforms does Quorum support?</h4>
+<p align="justify">
+    Quorum can be run on most platforms that support the Java Virtual Machine. We have
+    run Quorum on various versions of Windows, Mac, and Linux. 
+</p>
+<h4>How can I contribute to the Quorum project?</h4>
+<p align="justify">
+    There are many ways to contribute. If you have technical skills
+    and want to contribute to the standard library, you can either make a pull request
+    or get in touch with our development team. If you prefer to make a <strong>tax-exempt</strong>
+    donation to the Quorum project, you can do so through the Quorum Outreach and Research
+    Foundation by getting in touch with the development team. 
+    An easy way to make a financial contributions online is in the works.
+</p>
+<h4>Can I attend the annual Quorum conference (Experience Programming in Quorum)?</h4>
+<p align="justify">
+    We do hold an annual conference on Quorum. While we do accept applications to
+    attend, usually in March, this conference is invitation only. If we receive 
+    a sufficient number of requests, we may eventually open the conference up to
+    the general public.
+</p>
+</div>
 <?php include("static/templates/pagefooter.template.php"); ?>
