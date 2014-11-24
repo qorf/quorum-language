f106cf543ddf2bb35bd0da4ed07ff68b7fe61ae0
Phillip Uesbeck <uesbeck@unlv.nevada.edu>
2014-11-24 14:21:35 -0800

    added a timestamp column that should set to the current time by default

--- quorum/libraries/assets/Release 5 changes.sql
+++ quorum/libraries/assets/Release 5 changes.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `run_code` ADD pageURL text;
 ALTER TABLE `run_code` ADD google text;
 ALTER TABLE `run_code` DROP COLUMN times_requested;
+ALTER TABLE `run_code` ADD insertionTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
