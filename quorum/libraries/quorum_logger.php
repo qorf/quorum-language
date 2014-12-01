e0157507875af95221c3a6e223bd1e04ae89002b
stefika <stefika@stefik02.cs.unlv.edu>
2014-12-01 15:20:39 -0800

    Added better IP support to the logger.

--- quorum/libraries/quorum_logger.php
+++ quorum/libraries/quorum_logger.php
@@ -10,7 +10,14 @@ require_once("models/codeSample.model.php");
     $codeSample->setOutput($_POST['resultCode']);
     $codeSample->setCompleted($_POST['completed']);
 
-    $codeSample->setIP($_SERVER['REMOTE_ADDR']);
+    
+    $ip = $_SERVER['REMOTE_ADDR'];
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    $codeSample->setIP($ip);
 
     $codeSample->setUUID($_POST['uuid']);
     $codeSample->setPagenr($_POST['pagenumber']);
