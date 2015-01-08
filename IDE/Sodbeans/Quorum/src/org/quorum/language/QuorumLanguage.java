3759980709073711850f40d0ad6f7c1c792e0926
stefika <stefika@192.168.17.4>
2015-01-07 23:43:23 -0800

    Added in some stuff for language.

---
+++ IDE/Sodbeans/Quorum/src/org/quorum/language/QuorumLanguage.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.quorum.language;
+
+import org.netbeans.modules.csl.spi.DefaultLanguageConfig;
+
+/**
+ *
+ * @author stefika
+ */
+public class QuorumLanguage extends DefaultLanguageConfig{
+
+    @Override
+    public org.netbeans.api.lexer.Language getLexerLanguage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getDisplayName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
