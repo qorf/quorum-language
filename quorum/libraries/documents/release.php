66981bdd38e2a1f3e5f02737919e417f3cdabc89
stefika <stefika@192.168.17.4>
2015-01-30 10:21:58 -0800

    Started the release notes for Quorum 3.

--- quorum/libraries/documents/release.php
+++ quorum/libraries/documents/release.php
@@ -7,6 +7,129 @@
 	</div>
 </div>
 <div class="content index-content">
+<h2>Quorum 3.0 TBD 2015</h2>    
+<p>Quorum 3.0 represents the most significant change to the Quorum programming
+language since its inception and is a near-complete rewrite. When the Quorum project
+first began, it was written in Java, was interpreted, and was created as a toy 
+language as part of a National Science Foundation project for blind individuals. 
+While we have stayed true to helping this with disabilities learn to program, we 
+are pleased to announce that Quorum 3.0, now written in a special version of 
+Quorum 2.2 (which was never released), is now a fully general purpose programming language.</p>
+
+<p>Given our new goal to make Quorum as general purpose as possible, we have rewritten
+the language from scratch with a number of technical goals in mind. We highlight 
+some of the most significant alterations below:</p>
+<ul> <p>Major language changes</p>
+    <li>Quorum now uses an ANTLR 4 backend. Compiler phases in the language
+        are now considerably faster.</li>
+    <li>Quorum's type system has been significantly revised, removing
+        some inconsistencies and significantly simplifying tiling. This approach is
+        also somewhat more efficient than in previous releases and works for any
+        future cross-compiling we do.
+    </li>
+    <li>Quorum's exception (called Errors in Quorum) system has been not just rewritten, but also redesigned
+        from the ground up. In the new approach, users will always see
+        Quorum errors, never the Java equivalent, providing a unified approach. 
+        More than this, Quorum is now much better at dealing with internal system
+        errors, and in gathering better information about what happened at runtime.
+        We encourage users to try it out and give feedback on the new approach.
+    </li>
+    <li>Quorum can now generate Java bytecode directly using the excellent ASM
+        bytecode library used as a backend by most, if not all, Java Virtual Machine Languages.</li>
+    <li>Quorum's backend now supports a number of potential formats, not just
+        the Java Virtual Machine. In post 3.0, we plan to create a first additional 
+        backend with a Quorum to Javascript converter. A prototype using this
+        technology can be found at <a href="http://quorumlanguage.com/documents/hourofcode/part1.php">
+            quorumlanguage.com in our hour of code tutorial</a>.</li>
+    <li>Quorum now supports action chaining (e.g., a:b():c()).</li>
+    <li>A significant optimization pass has occurred for programs generated in Quorum, 
+        making generated programs have faster execution and bootup times under some
+        circumstances.</li>
+    <li>Quorum is now fully written, and compiles in, itself. In the first release,
+        Quorum 3.0 beta was compiled in Quorum 3.0 beta.</li>
+    <li>Quorum's compiler error messages have been redesigned using evidence gathered
+        <a href="http://dl.acm.org/citation.cfm?id=2016911.2016934&coll=DL&dl=GUIDE&CFID=622754201&CFTOKEN=67063107">Lee and Ko</a>
+        as a loose guide. More work is needed here, as the language and messages are different than Lee/Ko's.
+        Ours need to be vetted using evidence on each, which will occur over time.
+    </li>
+</ul>
+<ul> <p>Type system/variable changes</p>
+    <li>Removed a number of cases where Quorum was being slightly "too flexible" 
+    in its conversions between various types.</li>
+    <li>Fixed a host of issues where Quorum was making strange decisions in what it
+    was allowing users to do (e.g., action Test returns text would allow return false).</li>
+    <li>Fixed issues with not being able to pass undefined values to text fields.</li>
+    <li>Fixed a couple bugs with how fields and variables were implemented in bytecode.</li>
+    <li>Carefully fixed the amount of type inference allowed on the inside of actions. 
+    The value is somewhat conservative and needs to be evaluated in empirical studies.
+    Currently, there are no known randomized controlled trials on how much inference
+    to allow.</li>
+    <li>Made auto-boxing as transparent as possible, with the hopes that we can
+    eventually disallow use of capital letter types (e.g., Text vs. text). Our goals to-date
+    have been to keep this system as efficient as possible, while hiding/reducing object 
+    conversions to only when they are absolutely necessary.</li>
+    <li>In almost all cases now, actions can be called, and chained, on primitive
+    values without any auto-boxing. In other words, calling actions on primitives
+    is quite efficient in Quorum, as values have low memory footprints, but yet still
+    "look like" calls to objects in other languages, even though they technically are not.</li>
+</ul>
+<ul> <p>Exceptions (Error) system changes</p>
+    <li>This system has been fully redesigned.</li>
+    <li>In the new system Quorum Error classes, those that act like exceptions in 
+    other languages, must inherit from Error, and only Error. The reason is due to 
+    a quirk in Quorum's inheritance system, but allows the runtime system to have
+    considerable flexibility in how it handles errors.</li>
+    <li>Errors generated by the underlying backend are now caught and automatically
+        converted to Quorum style. This allows us flexibility in the system, so that
+        we can study aspects of it in future empirical studies. Previously, we could
+        not modify these internal aspects, making studies less useful.</li>
+    <li>Detect blocks can still contain multiple types (e.g., detect e is CastError), 
+    but the compiler now guarantees these are unique. This was previously a bug.</li>
+    <li>Detect blocks may now be rewritten to a new location by the bytecode generator. 
+    This will not impact the semantics of a program, but we have it in the notes
+    in case it is confusing to someone that looks at the generated bytecode.</li>
+    <li>Leaving off a type for a detect block makes it automatically of type Error,
+    which is now a catch-all for all possible kinds of system or Quorum errors. This
+    error is guaranteed to be written last in the bytecode generation. In other words, 
+    any block of type Error is written last.</li>
+    <li>Users that find a Java/other error that is not caught and converted properly
+    by Quorum, instead showing as a generic error with no obvious semantics, should
+    get in touch with the team and let us know, as these issues are now truly trivial
+    to manage and adjust as new compiler releases occur.</li>
+    <li>Fixed a bug that caused Quorum to run forever and need to be killed if an 
+        exception occurred in a generated program. </li>
+    <li>Adjusted the API for the Error classes making them more consistent, generally, 
+    and also making them conform to more modern naming schemes used in Quorum.</li>
+    <li>Considerably improved the stack trace information presented from errors.</li>
+</ul>
+<ul> <p>Control Structure changes</p>
+    <li>If statements now properly short circuit. This was previously a bug.</li>
+    <li>Included a number of optimizations at various points in the control
+    structure pipeline.</li>
+    <li>Simplified the bytecode generation for various kinds of expressions.</li>
+    <li>Fixed a bug in if statements that very rarely caused a bytecode verification error</li>
+</ul>
+<ul> <p>Compiler Support</p>
+    <li>Added preliminary support for the ASM bytecode library to Quorum. This initial
+    version is only enough functionality to implement Quorum, but will likely be expanded 
+    over time.
+    </li>
+    <li>Added parsing/semantic analysis libraries for Quorum. Others can inherit 
+    from these and garner full ANTLR 4 support for reading Quorum source. </li>
+    <li>All symbol tables, analyzers, and generators are public and easily modified for those
+    wanting to write support for Quorum into IDEs or tools, either online or offline.</li>
+    <li>Bytecode generation/semantic analysis is now done in two-passes, which considerably
+    simplifies finding/fixing bugs and the generation process.</li>
+</ul>
+<ul> <p>Other changes</p>
+    <li>Fixed a host of minor bugs throughout the language.</li>
+    <li>Fixed a bug causing compiled Quorum programs to load very slowly.</li>
+    <li>Input commands now work from the command line, but do not pop-up a window. 
+    The window popping up behavior will be retained only in an IDE.</li>
+    <li>Added a separate command called input, with no parameters, which allows
+        the user to request input without outputting information first.
+    </li>
+</ul>
 <h2>Quorum 2.1 December 16th, 2013</h2>
 <p>Quorum 2.1 </p>
 <ul> <p>Web changes</p>
