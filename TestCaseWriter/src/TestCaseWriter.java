2c3ea7e8c25eb9a2a7d38cfdc57365135528ad98
Patrick Daleiden <Patrick Daleiden@Patrick-Office>
2015-02-18 10:13:41 -0800

    TestCaseWriter - test generator program

---
+++ TestCaseWriter/src/TestCaseWriter.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class TestCaseWriter {
+    public static void main(String[] args) throws IOException {
+//        TemplatingTests.main(new String[0]);
+//        ChainingTests.main(new String[0]);
+        TypeCheckTests.main(new String[0]);
+    }
+}
