d21f5570562d90c07e58d075e8f27fa732e2de6e
alleew <wallee777@hotmail.com>
2015-10-08 15:47:38 -0700

    Made plugins to handle necessarily static members for Attribute and ColorAttribute.

---
+++ Quorum3Plugins/GamePlugin/JavaGame/src/plugins/quorum/Libraries/Game/Graphics/ColorAttribute.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plugins.quorum.Libraries.Game.Graphics;
+
+
+
+/**
+ *
+ * @author alleew
+ */
+public class ColorAttribute extends Attribute
+{
+    java.lang.Object me_ = null;
+    
+    public final static int DIFFUSE = Attribute.RegisterStatic("diffuseColor");
+    public final static int SPECULAR = Attribute.RegisterStatic("specularColor");
+    public final static int AMBIENT = Attribute.RegisterStatic("ambientColor");
+    public final static int EMISSIVE = Attribute.RegisterStatic("emissiveColor");
+    public final static int REFLECTION = Attribute.RegisterStatic("reflectionColor");
+    public final static int AMBIENT_LIGHT = Attribute.RegisterStatic("ambientLightColor");
+    public final static int FOG = Attribute.RegisterStatic("fogColor");
+    
+    public final static int MASK = DIFFUSE | SPECULAR | AMBIENT | EMISSIVE | REFLECTION | AMBIENT_LIGHT | FOG;
+    
+    public boolean SupportsAttribute(int type)
+    {
+        return (type & MASK) != 0;
+    }
+}
