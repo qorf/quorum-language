b4fa89077565c5412f3e24e96e3891fe4b57bcea
Bill <Bill@192.168.0.20>
2015-11-05 11:49:32 -0800

    Added the necessary libraries for IOSGraphics in the GameEngineCPlugins static library project for iOS.

--- Quorum3Plugins/GamePlugin/Native/iOS/GameEngineCPlugins/Source/IOSGraphics.cpp
+++ Quorum3Plugins/GamePlugin/Native/iOS/GameEngineCPlugins/Source/IOSGraphics.cpp
@@ -3,8 +3,8 @@
 
 #ifdef TARGET_OS_IPHONE
 #include "IOSGraphics.h"
-#include <OpenGLES/ES2/gl.h>
-#include <OpenGLES/ES2/glext.h>
+#include "gl2.h"
+#include "gl2ext.h"
 #include <stdio.h>
 
 static jclass bufferClass;
