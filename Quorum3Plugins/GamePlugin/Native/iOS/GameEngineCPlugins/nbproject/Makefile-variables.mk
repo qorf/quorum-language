bac1284cb3f8dc0f191958157fffc4b2b09833fb
alleew <wallee777@hotmail.com>
2015-11-04 16:52:49 -0800

    Added the C files to the hints branch. The included project contains the JNI code needed for IOS graphics, but is missing a pair of libraries and therefore won't compile successfully yet.

---
+++ Quorum3Plugins/GamePlugin/Native/iOS/GameEngineCPlugins/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist
+CND_ARTIFACT_NAME_Debug=libGameEngineCPlugins.a
+CND_ARTIFACT_PATH_Debug=dist/libGameEngineCPlugins.a
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=GameEngineCPlugins.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/GameEngineCPlugins.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=libgameenginecplugins.a
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/libgameenginecplugins.a
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=GameEngineCPlugins.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/GameEngineCPlugins.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
