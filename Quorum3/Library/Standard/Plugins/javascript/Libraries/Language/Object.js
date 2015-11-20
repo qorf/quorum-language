6703905d3cb4766b7c0b8f0138ffb2608cd0e6ff
stefika <stefika@192.168.17.6>
2015-11-20 13:16:58 -0800

    Fixed more test cases and made significant improvements to inheritance in JavaScript.

--- Quorum3/Library/Standard/Plugins/javascript/Libraries/Language/Object.js
+++ Quorum3/Library/Standard/Plugins/javascript/Libraries/Language/Object.js
@@ -3,3 +3,7 @@ function plugins_quorum_Libraries_Language_Object_() {
         return 0;
     };
 }
+
+function global_number_converter_(value) {
+    return value % 1 == 0 ? value.toFixed(1) : value;
+}
