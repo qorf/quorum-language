9480b457e240a5330fec67c1e7d950aa6d4f7996
stefika <stefika@192.168.17.6>
2015-11-20 10:36:34 -0800

    Array plugin function.

--- Quorum3/Library/Standard/Plugins/javascript/Libraries/Language/Types/Boolean.js
+++ Quorum3/Library/Standard/Plugins/javascript/Libraries/Language/Types/Boolean.js
@@ -8,6 +8,10 @@ function plugins_quorum_Libraries_Language_Types_Boolean_(optional) {
     this.SetValueNative$quorum_boolean = function (bool) {
         boolean = bool;
     };
+    
+    this.GetHashCode = function() {
+        return 0;
+    }
 }
 
 Boolean.prototype.GetText = function() {
