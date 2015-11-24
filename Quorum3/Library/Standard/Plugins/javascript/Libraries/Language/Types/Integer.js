0fa26a604187bf16f3ecf937c647b650d15dcb60
Patrick Daleiden <pmdaleiden@gmail.com>
2015-11-24 02:09:46 -0800

    system functions in Integer.js

--- Quorum3/Library/Standard/Plugins/javascript/Libraries/Language/Types/Integer.js
+++ Quorum3/Library/Standard/Plugins/javascript/Libraries/Language/Types/Integer.js
@@ -8,4 +8,41 @@ function plugins_quorum_Libraries_Language_Types_Integer_(optional) {
     this.SetValueNative$quorum_integer = function (value) {
         integer = value;
     };
+    
+    this.GetHashCode = function() {
+        return 0;
+    };
 }
+
+Number.prototype.GetNumber = function() {
+  return  this.integer;
+};
+
+Number.prototype.GetText = function() {
+  return  this.integer;
+};
+
+Number.prototype.GetMaximumValue = function() {
+  return  Number.MAX_VALUE;
+};
+
+Number.prototype.GetMaximumValue = function() {
+  return  Number.MIN_VALUE;
+};
+
+Number.prototype.Equals$quorum_integer = function(value) {
+    return this.valueOf().integer === value;
+};
+
+Number.prototype.Compare$quorum_integer = function(value) {
+    if (this.valueOf().integer === value) {
+        //return EQUAL
+    } else if (this.valueOf().integer < value) {
+        //return SMALLER
+    } else {
+        //return LARGER
+    }
+};
+Number.prototype.GetValue = function() {
+    return this.valueOf().integer;
+};
