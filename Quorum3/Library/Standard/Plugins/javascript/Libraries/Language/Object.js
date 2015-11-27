4ced7cfea9bcb59dc42415b0ea4d4d134a66f659
stefika <stefika@192.168.17.6>
2015-11-26 22:55:41 -0800

    Up to about 85% on passing test cases now.

--- Quorum3/Library/Standard/Plugins/javascript/Libraries/Language/Object.js
+++ Quorum3/Library/Standard/Plugins/javascript/Libraries/Language/Object.js
@@ -17,3 +17,36 @@ function global_InstanceOf(variable, name) {
     }
     return false;
 }
+
+function global_parseInteger(text) {
+    text = text.trim();
+    if(text == null) {
+        return NaN;
+    }
+    if(text.length == 0) {
+        return NaN;
+    }
+    var index = 0;
+    var result = 0;
+    var isMinus = false;
+    if(text.charAt(index) == '+') {
+        index = 1;
+    } else if(text.charAt(index) == '-') {
+        isMinus = true;
+        index = 1;
+    }
+    for (; index < text.length; index++) {
+        var char = text.charCodeAt(index) - 48;
+        if(char >= 0 && char < 10) {
+            result = result * 10;
+            result = result + char;
+        } else { //unlike typical javascript, throw an exception from Quorum
+            var error = new quorum_Libraries_Language_Errors_CastError_();
+            throw error;
+        }
+    }
+    if(isMinus) {
+        result = result * -1;
+    }
+    return result;
+}
