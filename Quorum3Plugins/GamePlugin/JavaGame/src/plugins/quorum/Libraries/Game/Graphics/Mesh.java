89f21aa57bb69b66be0fed159c253cfaa32e86be
alleew <wallee777@hotmail.com>
2016-04-25 16:48:22 -0700

    Updated the Mesh plugin to support BoundingBox calculation.

--- Quorum3Plugins/GamePlugin/JavaGame/src/plugins/quorum/Libraries/Game/Graphics/Mesh.java
+++ Quorum3Plugins/GamePlugin/JavaGame/src/plugins/quorum/Libraries/Game/Graphics/Mesh.java
@@ -5,11 +5,17 @@
  */
 package plugins.quorum.Libraries.Game.Graphics;
 
+import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
 import plugins.quorum.Libraries.Game.GameState;
+import plugins.quorum.Libraries.Game.GameRuntimeError;
 import plugins.quorum.Libraries.Game.libGDX.ShaderProgram;
+import quorum.Libraries.Compute.Matrix4_;
+import quorum.Libraries.Compute.Vector3;
 
 import quorum.Libraries.Game.Graphics.Mesh_;
+import quorum.Libraries.Game.BoundingBox_;
+import quorum.Libraries.Game.Graphics.VertexAttribute_;
 
 /**
  *
@@ -19,6 +25,9 @@ public class Mesh
 {
     public java.lang.Object me_ = null;
     private quorum.Libraries.Game.Graphics.Mesh quorumMesh = null;
+    
+    // Static vector used during calculations.
+    private static Vector3 calcVector = new Vector3();
     
     /*
     An action used by the Quorum mesh class during creation. This will ensure
@@ -105,4 +114,106 @@ public class Mesh
         if (quorumMesh.indices.GetSize() > 0)
             ((quorum.Libraries.Game.Graphics.IndexBufferObject)quorumMesh.indices).plugin_.Unbind();
     }
+    
+    public BoundingBox_ CalculateBoundingBox(BoundingBox_ box)
+    {
+        box.Invalidate();
+        
+        int numVertices = quorumMesh.GetVerticesCount();
+        if (numVertices == 0)
+            throw new GameRuntimeError("There were no vertices defined for this Mesh!");
+        
+        final FloatBuffer verts = ((quorum.Libraries.Game.Graphics.VertexBufferObject)quorumMesh.GetVertexData()).plugin_.GetBuffer();
+        
+        VertexAttribute_ posAttrib = quorumMesh.GetVertexAttributes().GetAttribute(quorumMesh.GetVertexAttributes().Get_Libraries_Game_Graphics_VertexAttributes__POSITION_());
+        final int offset = posAttrib.Get_Libraries_Game_Graphics_VertexAttribute__offset_() / 4;
+        final int vertexSize = quorumMesh.vertices.GetAttributes().Get_Libraries_Game_Graphics_VertexAttributes__vertexSize_();
+        int index = offset;
+        
+        switch (posAttrib.Get_Libraries_Game_Graphics_VertexAttribute__componentCount_())
+        {
+            case 1:
+                for (int i = 0; i < numVertices; i++)
+                {
+                    box.Extend(verts.get(index), 0, 0);
+                    index += vertexSize;
+                }
+                break;
+                
+            case 2:
+                for (int i = 0; i < numVertices; i++)
+                {
+                    box.Extend(verts.get(index), verts.get(index + 1), 0);
+                    index += vertexSize;
+                }
+                break;
+                
+            case 3:
+                for (int i = 0; i < numVertices; i++)
+                {
+                    box.Extend(verts.get(index), verts.get(index + 1), verts.get(index + 2));
+                    index += vertexSize;
+                }
+                break;
+        }
+        
+        return box;
+    }
+    
+    public BoundingBox_ ExtendBoundingBox(BoundingBox_ box, int offset, int count, Matrix4_ transform)
+    {
+        int numIndices = quorumMesh.GetIndicesCount();
+        
+        if (offset < 0 || count < 1 || offset + count > numIndices)
+            throw new GameRuntimeError("Invalid parameter(s) to ExtendBoundingBox - offset = " + offset + ", count = " + count + "max = " + numIndices);
+
+        final FloatBuffer verts = ((quorum.Libraries.Game.Graphics.VertexBufferObject)quorumMesh.vertices).plugin_.GetBuffer();
+        final ShortBuffer index = ((quorum.Libraries.Game.Graphics.IndexBufferObject)quorumMesh.indices).plugin_.GetBuffer();
+        final VertexAttribute_ posAttrib = quorumMesh.GetVertexAttributes().GetAttribute(quorumMesh.GetVertexAttributes().Get_Libraries_Game_Graphics_VertexAttributes__POSITION_());
+        
+        final int posOffset = posAttrib.Get_Libraries_Game_Graphics_VertexAttribute__offset_() / 4;
+        final int vertexSize = quorumMesh.vertices.GetAttributes().Get_Libraries_Game_Graphics_VertexAttributes__vertexSize_() / 4;
+        final int end = offset + count;
+        
+        switch (posAttrib.Get_Libraries_Game_Graphics_VertexAttribute__componentCount_())
+        {
+            case 1:
+                for (int i = offset; i < end; i++)
+                {
+                    final int idx = index.get(i) * vertexSize + posOffset;
+                    calcVector.Set(verts.get(idx), 0, 0);
+                    if (transform != null)
+                        calcVector.Multiply(transform);
+                    
+                    box.Extend(calcVector);
+                }
+                break;
+                
+            case 2:
+                for (int i = offset; i < end; i++)
+                {
+                    final int idx = index.get(i) * vertexSize + posOffset;
+                    calcVector.Set(verts.get(idx), verts.get(idx + 1), 0);
+                    if (transform != null)
+                        calcVector.Multiply(transform);
+                    box.Extend(calcVector);
+                }
+                break;
+                
+            case 3: 
+                for (int i = offset; i < end; i++)
+                {
+                    final int idx = index.get(i) * vertexSize + posOffset;
+                    calcVector.Set(verts.get(idx), verts.get(idx + 1), verts.get(idx + 2));
+                    if (transform != null)
+                        calcVector.Multiply(transform);
+                    box.Extend(calcVector);
+                }
+            
+                break;
+        }
+        
+        return box;
+    }
+    
 }
