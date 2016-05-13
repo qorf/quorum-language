4c8a33e8b2eb3f250116841ff9785c836f34b264
Bill <Bill@Bill-PC.local>
2016-05-12 17:01:11 -0700

    Made the Model plugin able to cache and retrieve the base dimensions of a ModelBlueprint.

--- Quorum3Plugins/GamePlugin/JavaGame/src/plugins/quorum/Libraries/Game/Graphics/Model.java
+++ Quorum3Plugins/GamePlugin/JavaGame/src/plugins/quorum/Libraries/Game/Graphics/Model.java
@@ -6,10 +6,12 @@
 package plugins.quorum.Libraries.Game.Graphics;
 
 import java.util.Hashtable;
+import quorum.Libraries.Compute.Vector3_;
 import quorum.Libraries.System.File_;
 import quorum.Libraries.Game.Graphics.Model_;
 import quorum.Libraries.Game.Graphics.ModelLoader;
 import quorum.Libraries.Game.Graphics.ModelBlueprint_;
+import quorum.Libraries.Game.BoundingBox;
 
 /**
  *
@@ -21,6 +23,8 @@ public class Model
     
     private final static ModelLoader loader = new ModelLoader();
     private final static Hashtable<String, ModelBlueprint_> hashTable = new Hashtable();
+    private final static Hashtable<ModelBlueprint_, Vector3_> dimensionsTable = new Hashtable();
+    private final static BoundingBox calcBox = new BoundingBox();
     
     public ModelBlueprint_ GetCachedBlueprint(File_ file)
     {
@@ -31,5 +35,16 @@ public class Model
             hashTable.put(file.GetAbsolutePath(), blueprint);
         }
         return blueprint;
+    }
+    
+    public Vector3_ GetCachedDimensions(ModelBlueprint_ blueprint)
+    {
+        Vector3_ vector = dimensionsTable.get(blueprint);
+        if (vector == null)
+        {
+            vector = blueprint.CalculateBoundingBox(calcBox).GetDimensions();
+            dimensionsTable.put(blueprint, vector);
+        }
+        return vector;
     }
 }
