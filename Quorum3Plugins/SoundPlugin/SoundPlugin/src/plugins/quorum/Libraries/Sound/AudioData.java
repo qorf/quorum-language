be2849a51a50a83a6399bb35c5478bb67357a4fd
alleew <wallee777@hotmail.com>
2017-02-02 16:57:24 -0800

    Fixed an issue in desktop audio where the SetFade action still calculated audio position using the old z-coordinate system.

--- Quorum3Plugins/SoundPlugin/SoundPlugin/src/plugins/quorum/Libraries/Sound/AudioData.java
+++ Quorum3Plugins/SoundPlugin/SoundPlugin/src/plugins/quorum/Libraries/Sound/AudioData.java
@@ -267,7 +267,7 @@ public abstract class AudioData extends DesktopData
         
         this.x = 0;
         this.y = (float)Math.sin((fade + 1) * (float)Math.PI / 2);
-        this.z = (float)Math.cos((fade - 1) * (float)Math.PI / 2);
+        this.z = -(float)Math.cos((fade - 1) * (float)Math.PI / 2);
         
         manager.SetSoundFade(soundID, fade);
     }
