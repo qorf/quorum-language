628b2364b7d99fe2c8c9857c0c3d46c8e31124e0
alleew <wallee777@hotmail.com>
2017-02-07 16:57:06 -0800

    Began work on input for the web. Also fixed an issue with the Scale action in the OrthographicCamera that caused it to adjust the camera in the wrong direction.

---
+++ Quorum3/Library/Standard/Plugins/javascript/Libraries/Game/InputMonitor.js
@@ -0,0 +1,34 @@
+function plugins_quorum_Libraries_Game_InputMonitor_()
+{
+    this.IsKeyPressed$quorum_integer = function(keyCode)
+    {
+        
+    };
+    
+    this.IsMouseButtonPressed$quorum_integer = function(buttonCode)
+    {
+        
+    };
+    
+    this.GetMouseX = function()
+    {
+        
+    };
+    
+    this.GetMouseY = function()
+    {
+        
+    };
+    
+    this.IsClicked = function()
+    {
+        
+    };
+    
+    this.IsScrolled = function()
+    {
+        
+    };
+}
+
+
---
+++ Quorum3/Library/Standard/Plugins/javascript/Libraries/Game/WebInput.js
@@ -0,0 +1,43 @@
+function plugins_quorum_Libraries_Game_WebInput_()
+{
+    if (!plugins_quorum_Libraries_Game_WebInput_.initialized_plugins_quorum_Libraries_Game_WebInput_)
+    {
+        plugins_quorum_Libraries_Game_WebInput_.KeyDown = function()
+        {
+
+        };
+        
+        plugins_quorum_Libraries_Game_WebInput_.KeyUp = function()
+        {
+            
+        };
+        
+        plugins_quorum_Libraries_Game_WebInput_.MouseDown = function()
+        {
+            
+        };
+        
+        plugins_quorum_Libraries_Game_WebInput_.MouseUp = function()
+        {
+            
+        };
+        
+        plugins_quorum_Libraries_Game_WebInput_.MouseMove = function()
+        {
+            
+        };
+        
+        document.addEventListener('keydown', plugins_quorum_Libraries_Game_WebInput_.KeyDown, false);
+        document.addEventListener('keyup', plugins_quorum_Libraries_Game_WebInput_.KeyUp, false);
+    
+        plugins_quorum_Libraries_Game_WebInput_.initialized_plugins_quorum_Libraries_Game_WebInput_ = true;
+    }
+    
+    var mouseEvents = [];
+    var keyboardEvents = [];
+    
+    this.CheckForEvents$quorum_Libraries_Containers_Array$quorum_Libraries_Containers_Array = function(mEvents, kEvents)
+    {
+        
+    };
+}
