64d8d88c1793218c135e133dd3ba760f7b0b86e1
alleew <alleew@occam.cs.unlv.edu>
2017-03-07 16:56:33 -0800

    Added GLSL code for the skybox shader for desktop.

--- Quorum3Plugins/GamePlugin/JavaGame/src/plugins/quorum/Libraries/Game/Graphics/SkyboxShader.java
+++ Quorum3Plugins/GamePlugin/JavaGame/src/plugins/quorum/Libraries/Game/Graphics/SkyboxShader.java
@@ -5,12 +5,43 @@
  */
 package plugins.quorum.Libraries.Game.Graphics;
 
+import org.lwjgl.opengl.GL11;
+
 /**
  *
  * @author William Allee
  */
 public class SkyboxShader 
 {
+    static
+    {
+        System.out.println("OpenGL version: " + GL11.glGetString(GL11.GL_VERSION));
+    }
+    
+    public static final String vertexShader = 
+        "attribute vec3 position;\n" +
+        "varying vec3 textureCoordinates;\n" +
+        "\n" +
+        "uniform mat4 projection;\n" +
+        "uniform mat4 view;\n" +
+        "\n" +
+        "void main()\n" +
+        "{\n" +
+        "    gl_Position =   projection * view * vec4(position, 1.0);  \n" +
+        "    texureCoordinates = position;\n" +
+        "}";
+    
+    public static final String fragmentShader = 
+        "attribute vec3 textureCoordinates;\n" +
+        "varying vec4 color;\n" +
+        "\n" +
+        "uniform samplerCube skybox;\n" +
+        "\n" +
+        "void main()\n" +
+        "{    \n" +
+        "    gl_FragColor = texture(skybox, textureCoordinates);\n" +
+        "}";
+    
     float[] skyboxVertices = {
         -1.0f,  1.0f, -1.0f,
         -1.0f, -1.0f, -1.0f,
