e1b3f4e38a4f6ba8e92167a64fcd0fd5dc533d4c
daleiden <daleiden@unlv.nevada.edu>
2018-04-12 15:14:49 -0700

    JavaScript Plugin shell working. Need to complete attaching request objects and then test.

--- Quorum3/Library/Standard/Plugins/javascript/Libraries/Network/NetworkConnection.js
+++ Quorum3/Library/Standard/Plugins/javascript/Libraries/Network/NetworkConnection.js
@@ -1,13 +1,61 @@
 function plugins_quorum_Libraries_Network_NetworkConnection_(quorumConnection) {
-    this.me_ = quorumConnection;
+    var connection = quorumConnection;
     
-    this.Request = function (url) {
-        request = this.me_.GetNetworkRequest();
-        console.log(request.GetRequestType());
-        
-        response = this.me_.GetNewResponseEvent();
-        response.SetResponseText$quorum_text("This is from JavaScript\n" + request.GetRequestType());
-        this.me_.SetResponse$quorum_Libraries_Network_NetworkResponseEvent(response);
+    this.Request = function () {
+        request = connection.GetNetworkRequest();
+        if (request.GetRequestType() === "POST") {
+            Post(request);
+        } else if (request.GetRequestType() === "GET") {
+            Get(request);
+        } else if (request.GetRequestType() === ("HEAD")) {
+            Head(request);
+        } else if (request.GetRequestType() === ("PATCH")) {
+            Patch(request);
+        } else if (request.GetRequestType() === ("DELETE")) {
+            Delete(request);
+        } else if (request.GetRequestType() === ("PUT")) {
+            Put(request);
+        }
     };
+    
+    function Post(request) {
+        ReturnResponse("Post Response: " + request.GetRequestType());
+        var http = new XMLHttpRequest();
+        
+        request.ResetHeaderIterator();
+        while (request.HasNextHeader()) {
+            var key = request.GetNextHeaderKey();
+            var value = request.GetNextHeaderValue(key);
+            http.setRequestHeader(key, value);
+        }
+    };
+    
+    function Get(request) {
+    };
+    
+    function Head(request) {
+    };
+    
+    function Patch(request) {
+    };
+    
+    function Delete(request) {
+    };
+    
+    function Put(request) {
+    };
+    
+    function ReturnResponse(responseText) {
+        response = connection.GetNewResponseEvent();
+        response.SetWebAddress$quorum_text("url");
+        response.SetStatusCode$quorum_integer(200);
+        response.SetStatusText$quorum_text("OK");
+        response.SetEncoding$quorum_text("encoding type");
+        response.SetContentType$quorum_text("content type");
+        response.SetResponseMessage$quorum_text("response message");
+        response.SetResponseText$quorum_text(responseText);
+        connection.SetResponse$quorum_Libraries_Network_NetworkResponseEvent(response);
+        
+    }
 }
 
