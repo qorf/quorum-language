19ad588d5bf0e3129894dcc923d38c2391467816
Andreas Stefik <stefika@gmail.com>
2020-07-24 13:50:11 -0700

    Added in stubs for the dialog provider. I'll implement it next, but it recognizes it now at least

--- Plugins/AccessibilityPlugin/Native/Windows/src/DialogProvider.cpp
+++ Plugins/AccessibilityPlugin/Native/Windows/src/DialogProvider.cpp
@@ -6,5 +6,48 @@ DialogProvider::DialogProvider(_In_ DialogControl* control) : ProviderT(control)
 
 CONTROLTYPEID DialogProvider::GetControlType() const noexcept
 {
-	return UIA_PaneControlTypeId;
+	return UIA_WindowControlTypeId;
+}
+
+bool DialogProvider::IsPatternSupported(PATTERNID patternId) const noexcept
+{
+	return (patternId == UIA_WindowPatternId);
+}
+
+IFACEMETHODIMP DialogProvider::Close() noexcept
+{
+	return E_NOTIMPL;
+}
+
+IFACEMETHODIMP DialogProvider::get_CanMaximize(_Out_ BOOL* retVal) noexcept
+{
+	return E_NOTIMPL;
+}
+IFACEMETHODIMP DialogProvider::get_CanMinimize(_Out_ BOOL* retVal) noexcept
+{
+	return E_NOTIMPL;
+}
+IFACEMETHODIMP DialogProvider::get_IsModal(_Out_ BOOL* retVal) noexcept
+{
+	return E_NOTIMPL;
+}
+IFACEMETHODIMP DialogProvider::get_IsTopmost(_Out_ BOOL* retVal) noexcept
+{
+	return E_NOTIMPL;
+}
+IFACEMETHODIMP DialogProvider::get_WindowInteractionState(_Out_ WindowInteractionState* retVal) noexcept
+{
+	return E_NOTIMPL;
+}
+IFACEMETHODIMP DialogProvider::get_WindowVisualState(_Out_ WindowVisualState* retVal) noexcept
+{
+	return E_NOTIMPL;
+}
+IFACEMETHODIMP DialogProvider::SetVisualState(_In_ WindowVisualState retVal) noexcept
+{
+	return E_NOTIMPL;
+}
+IFACEMETHODIMP DialogProvider::WaitForInputIdle(_Out_ int  milliseconds, _Out_ BOOL* pRetVal) noexcept
+{
+	return E_NOTIMPL;
 }
--- Plugins/AccessibilityPlugin/Native/Windows/src/DialogProvider.h
+++ Plugins/AccessibilityPlugin/Native/Windows/src/DialogProvider.h
@@ -2,11 +2,21 @@
 #include "DialogControl.h"
 #include "ProviderT.h"
 
-class DialogProvider : public ProviderT<DialogProvider, DialogControl>
+class DialogProvider : public ProviderT<DialogProvider, DialogControl, IWindowProvider>
 {
 public:
 	DialogProvider(_In_ DialogControl* control);
 	
 	// Overridden functions from ProviderT.
+	bool IsPatternSupported(PATTERNID patternId) const noexcept;
 	CONTROLTYPEID GetControlType() const noexcept;
+	IFACEMETHODIMP Close() noexcept;
+	IFACEMETHODIMP get_CanMaximize(_Out_ BOOL* retVal) noexcept;
+	IFACEMETHODIMP get_CanMinimize(_Out_ BOOL* retVal) noexcept;
+	IFACEMETHODIMP get_IsModal(_Out_ BOOL* retVal) noexcept;
+	IFACEMETHODIMP get_IsTopmost(_Out_ BOOL* retVal) noexcept;
+	IFACEMETHODIMP get_WindowInteractionState(_Out_ WindowInteractionState* retVal) noexcept;
+	IFACEMETHODIMP get_WindowVisualState(_Out_ WindowVisualState* retVal) noexcept;
+	IFACEMETHODIMP SetVisualState(_In_ WindowVisualState retVal) noexcept;
+	IFACEMETHODIMP WaitForInputIdle(_Out_ int  milliseconds, _Out_ BOOL* pRetVal) noexcept;
 };
