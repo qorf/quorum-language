c5ac3e340a431a01fa9c9e39a7aa108eaba8c640
Andreas Stefik <stefika@gmail.com>
2021-06-25 15:20:48 -0700

    Updated the build file and added two new stub plugins.

---
+++ Plugins/AccessibilityPlugin/Java/AccessibilityManager/src/plugins/quorum/Libraries/Interface/Accessibility/MacAccessibility.java
@@ -0,0 +1,61 @@
+package plugins.quorum.Libraries.Interface.Accessibility;
+
+import quorum.Libraries.Interface.Controls.Button_;
+import quorum.Libraries.Interface.Controls.TextField_;
+import quorum.Libraries.Interface.Controls.ToggleButton_;
+import quorum.Libraries.Interface.Events.ControlActivationEvent_;
+import quorum.Libraries.Interface.Events.FocusEvent_;
+import quorum.Libraries.Interface.Events.MenuChangeEvent_;
+import quorum.Libraries.Interface.Events.ProgressBarValueChangedEvent_;
+import quorum.Libraries.Interface.Events.SelectionEvent_;
+import quorum.Libraries.Interface.Events.TextChangeEvent_;
+import quorum.Libraries.Interface.Events.TreeChangeEvent_;
+import quorum.Libraries.Interface.Events.TreeTableChangeEvent_;
+import quorum.Libraries.Interface.Events.WindowFocusEvent_;
+import quorum.Libraries.Interface.Item_;
+
+/**
+ *
+ * @author andreasstefik
+ */
+public class MacAccessibility {
+    public void  NameChanged(Item_ item) {}
+
+    public void  DescriptionChanged(Item_ item) {}
+
+    public void  TextFieldUpdatePassword(TextField_ field) {}
+
+    public void  Update() {}
+
+    public void  ProgressBarValueChanged(ProgressBarValueChangedEvent_ progress) {}
+
+    public void  SelectionChanged(SelectionEvent_ event) {}
+
+    public void  ButtonActivated(Button_ button) {}
+
+    public void  ToggleButtonToggled(ToggleButton_ button) {}
+
+    public void  FocusChanged(FocusEvent_ event) {}
+
+    public void  Add(Item_ item) {}
+
+    public void  Remove(Item_ item) {}
+
+    public void  MenuChanged(MenuChangeEvent_ event) {}
+
+    public void  TreeChanged(TreeChangeEvent_ event) {}
+
+    public void  TreeTableChanged(TreeTableChangeEvent_ event) {}
+
+    public void  ControlActivated(ControlActivationEvent_ event) {}
+
+    public void  TextChanged(TextChangeEvent_ event) {}
+
+    public void  WindowFocusChanged(WindowFocusEvent_ event) {}
+
+    public void  Notify(Item_ item, String value) {}
+
+    public void  Notify(Item_ item, String value, int notificationType) {}
+
+    public void  Shutdown() {}
+}
