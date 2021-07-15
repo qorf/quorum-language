66d1d8328d4f3bfb611bd40662569698fa8e7ad5
Andreas Stefik <stefika@gmail.com>
2021-07-14 17:09:38 -0700

    Added a first version of native support on Mac

--- Natives/Source/Mac/Mac.m
+++ Natives/Source/Mac/Mac.m
@@ -1,22 +1,10 @@
-/*
- * Copyright © 2021 Michal K. Owsiak. All rights reserved.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- */
-
 #include <stdio.h>
 #include "jni.h"
 #import <Foundation/Foundation.h>
 
-JNIEXPORT void JNICALL Java_javaapplication6_JavaApplication6_sayHello
-  (JNIEnv* env, jobject thisObject) {
+JNIEXPORT void JNICALL Java_plugins_quorum_Libraries_Interface_Accessibility_MacAccessibility_InitializeAccessibilityNative
+  (JNIEnv* env, jobject thisObject, jlong window, jstring title) {
     printf("Hi");
 }
-  
+
+
