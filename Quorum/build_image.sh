d0f7d7c9a0d6c71b29ede4dfe7636ae146be62bc
Andreas Stefik <stefika@gmail.com>
2021-12-08 20:26:58 -0800

    Added the new procedure for the docker push, which includes arm processors for Mac.

--- Quorum/build_image.sh
+++ Quorum/build_image.sh
@@ -1,1 +1,2 @@
-docker build --no-cache -f Dockerfile.quorum.build -t andreasstefik/quorum:9.5 .
+docker buildx create --name quorum_builder --use
+docker buildx build --no-cache -f Dockerfile.quorum.build -t andreasstefik/quorum:9.5.1 . --platform linux/amd64,linux/arm64 --push
