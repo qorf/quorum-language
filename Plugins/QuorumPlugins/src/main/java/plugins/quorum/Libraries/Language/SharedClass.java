ddd7e3a0ce418c313a9b34225f78375cbd6a4e09
Andreas Stefik <stefik@gmail.com>
2022-06-21 15:32:39 -0700

    Added a shared class plugin so the concept can be removed from object. Next commit is to remove it, then move to removing the Object plugin.

--- Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Language/SharedClass.java
+++ Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Language/SharedClass.java
@@ -1,26 +1,16 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package plugins.quorum.Libraries.Language;
 
 import java.util.HashMap;
 
-/**
- * 
- * @author Andreas Stefik
+/*
+    This class manages shared classes in Quorum. It is a helper that is not directly exposed from the Quorum side.
+    As such, one should not create a class with this exact package and name, like any plugin.
  */
-public class Object {
-    public java.lang.Object me_ = null;
+public class SharedClass {
     public final static HashMap<String, java.lang.Object> STATIC_OBJECTS = new  HashMap<String, java.lang.Object>();
-    public int GetHashCode() {
-        return me_.hashCode();
-    }
-
     public static java.lang.Object GetStaticClass(String key) {
         return STATIC_OBJECTS.get(key);
     }
-    
     public static void PutStaticClass(String key, java.lang.Object value) {
         STATIC_OBJECTS.put(key, value);
     }
