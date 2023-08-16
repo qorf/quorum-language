c936ac2b688d50ae224d8dea09b6c62c68634941
Matt Campbell <mattcampbell@pobox.com>
2023-08-16 16:35:39 -0500

    Unoptimized textbox support; may be completely busted

--- Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/accesskit/TextFieldKit.java
+++ Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/accesskit/TextFieldKit.java
@@ -3,14 +3,11 @@ package plugins.quorum.Libraries.Interface.Accessibility.accesskit;
 import dev.accesskit.*;
 import java.util.ArrayList;
 import java.util.Collections;
-import quorum.Libraries.Containers.Number32BitArray;
-import quorum.Libraries.Containers.Number32BitArray_;
-import quorum.Libraries.Interface.Controls.TextBox_;
 import quorum.Libraries.Interface.Controls.TextField_;
 import quorum.Libraries.Interface.Selections.TextFieldSelection_;
 import quorum.Libraries.Interface.Item_;
 
-public class TextFieldKit extends ItemKit{
+public class TextFieldKit extends TextKitBase {
     private boolean textDirty = true;
 
     public TextFieldKit() {
@@ -84,59 +81,19 @@ public class TextFieldKit extends ItemKit{
         Item_ item = GetItem();
         if(item != null && item instanceof TextField_) {
             TextField_ field = (TextField_) item;
-            NodeBuilder builder = new NodeBuilder(Role.INLINE_TEXT_BOX);
-            builder.setTextDirection(TextDirection.LEFT_TO_RIGHT);
             String s = field.GetText();
-            builder.setValue(s);
-            ArrayList<Byte> characterLengths = new ArrayList<Byte>();
-            ArrayList<Byte> wordLengths = new ArrayList<Byte>();
-            byte wordLength = 0;
-            // FUTURE: handle combining characters
-            for (int i = 0; i < s.length(); ) {
-                if (i > 0 && field.IsBeginningOfToken(i)) {
-                    wordLengths.add(wordLength);
-                    wordLength = 0;
-                }
-                int codePoint = s.codePointAt(i);
-                byte byteCount = UTF8ByteCount(codePoint);
-                characterLengths.add(byteCount);
-                wordLength += byteCount;
-                i += Character.charCount(codePoint);
-            }
-            wordLengths.add(wordLength);
-
-            //This is slow and silly, but fast to write, so it will get us prototyping.
-            byte[] charLen = new byte[characterLengths.size()];
-            byte[] wordLen = new byte[wordLengths.size()];
-
-            for(int i = 0; i < characterLengths.size(); i++) {
-                charLen[i] = characterLengths.get(i).byteValue();
-            }
-
-            for(int i = 0; i < wordLengths.size(); i++) {
-                wordLen[i] = wordLengths.get(i).byteValue();
-            }
-            builder.setCharacterLengths(charLen);
-            builder.setWordLengths(wordLen);
-
+            NodeBuilder builder = BuildLineBase(0, s);
             return builder.build();
         }
         return null;
     }
 
-    private static byte UTF8ByteCount(int codePoint) {
-        if (codePoint <= 0x7f) {
-            return 1;
+    @Override
+    boolean IsBeginningOfToken(int lineIndex, int i) {
+        if (lineIndex != 0) {
+            throw new IllegalArgumentException("non-zero line index");
         }
-        if (codePoint <= 0x7ff) {
-            return 2;
-        }
-        if (codePoint <= 0xffff) {
-            return 3;
-        }
-        if (codePoint <= 0x10fff) {
-            return 4;
-        }
-        throw new IllegalArgumentException("code point out of range");
+        TextField_ field = (TextField_)GetItem();
+        return field.IsBeginningOfToken(i);
     }
 }
--- Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/accesskit/TextboxKit.java
+++ Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/accesskit/TextboxKit.java
@@ -1,15 +1,23 @@
 package plugins.quorum.Libraries.Interface.Accessibility.accesskit;
 
 import dev.accesskit.*;
-import quorum.Libraries.Containers.Number32BitArray;
-import quorum.Libraries.Interface.Controls.RadioButton_;
+import java.util.ArrayList;
+import java.util.Collections;
+import quorum.Libraries.Containers.MultipleLineText_;
 import quorum.Libraries.Interface.Controls.TextBox_;
-import quorum.Libraries.Interface.Controls.TextField_;
+import quorum.Libraries.Interface.Selections.TextBoxSelection_;
 import quorum.Libraries.Interface.Item_;
 
-public class TextboxKit extends ItemKit{
+public class TextboxKit extends TextKitBase {
+    // TODO: smart dirty line tracking
+    private boolean textDirty = true;
+
     public TextboxKit() {
         SetRole(Role.TEXT_FIELD);
+    }
+
+    private NodeId GetLineID(int lineIndex) {
+        return new NodeId(lineIndex, GetItem().GetHashCode());
     }
 
     public Node Build() {
@@ -21,21 +29,102 @@ public class TextboxKit extends ItemKit{
             builder.setName(item.GetName());
             if(item instanceof TextBox_) { //technically not compiler guaranteed. You can set the code to anything.
                 TextBox_ box = (TextBox_) item;
-                String s = box.GetText(); //this would get all text in the box
-                int position = box.GetCaretPosition();//global caret position as a raw index from 0
+                TextBoxSelection_ selection = box.GetSelection();
+                int start = selection.GetStartIndex();
+                int end = selection.GetEndIndex();
+                if(selection.IsEmpty()) {
+                    start = box.GetCaretPosition();
+                    end = box.GetCaretPosition();
+                }
+                MultipleLineText_ value = box.GetMultipleLineText();
+                int startLineIndex = value.LineIndexFromCharacterIndex(start);
+                int startIndexInLine = start - value.CharacterIndexFromLineIndex(startLineIndex);
+                NodeId startLineID = GetLineID(startLineIndex);
+                TextPosition startPosition = new TextPosition(startLineID, startIndexInLine);
+                int endLineIndex = value.LineIndexFromCharacterIndex(end);
+                int endIndexInLine = end - value.CharacterIndexFromLineIndex(endLineIndex);
+                NodeId endLineID = GetLineID(endLineIndex);
+                TextPosition endPosition = new TextPosition(endLineID, endIndexInLine);
+                TextPosition anchor, focus;
+                if (selection.IsEmpty() || selection.IsCaretAtEnd()) {
+                    anchor = startPosition;
+                    focus = endPosition;
+                } else {
+                    anchor = endPosition;
+                    focus = startPosition;
+                }
+                builder.setTextSelection(new TextSelection(anchor, focus));
             }
+            BuildChildren(builder);
             return builder.build();
         }
         return null;
     }
 
-    public float[] GetCharacterWidths(TextBox_ box, int line) {
-        Number32BitArray array = (Number32BitArray) box.GetCharacterWidths(line);
-        return array.plugin_.floats;
+    public void SetTextDirty() {
+        // TODO: smart dirty line tracking
+        textDirty = true;
     }
 
-    public float[] GetCharacterXPositions(TextBox_ box, int line) {
-        Number32BitArray array = (Number32BitArray) box.GetCharacterXPositions(line);
-        return array.plugin_.floats;
+    @Override
+    public Iterable<NodeId> GetInternalChildren() {
+        TextBox_ box = (TextBox_)GetItem();
+        MultipleLineText_ value = box.GetMultipleLineText();
+        int lineCount = value.GetLineCount();
+        ArrayList<NodeId> result = new ArrayList<NodeId>();
+        for (int i = 0; i < lineCount; i++) {
+            result.add(GetLineID(i));
+        }
+        return result;
+    }
+
+    @Override
+    public Iterable<NodeId> GetDirtyInternalChildren() {
+        // TODO: smart dirty line tracking
+        if (!textDirty) {
+            return Collections.emptySet();
+        }
+        return GetInternalChildren();
+    }
+
+    @Override
+    public void ClearDirtyInternalChildren() {
+        // TODO: smart dirty line tracking
+        textDirty = false;
+    }
+
+    @Override
+    public Node BuildInternalChild(NodeId id) {
+        if (id.high != (long)(GetItem().GetHashCode())) {
+            throw new IllegalArgumentException("unexpected child ID");
+        }
+        int lineIndex = (int)(id.low);
+        Item_ item = GetItem();
+        if(item != null && item instanceof TextBox_) {
+            TextBox_ box = (TextBox_) item;
+            MultipleLineText_ value = box.GetMutipleLineText();
+            MultipleLineText_ line = value.GetLine(lineIndex);
+            String s = line.GetText();
+            if ((lineIndex + 1) == value.GetLineCount()) {
+                if (s.endsWith("\r\n")) {
+                    s = s.substring(0, s.length() - 2);
+                } else if (s.endsWith("\r") || s.endsWith("\n")) {
+                    s = s.substring(0, s.length() - 1);
+                }
+            } else if (!s.endsWith("\r") && !s.endsWith("\n")) {
+                s += value.GetLineSeparator();
+            }
+            NodeBuilder builder = BuildLineBase(lineIndex, s);
+            return builder.build();
+        }
+        return null;
+    }
+
+    @Override
+    boolean IsBeginningOfToken(int lineIndex, int indexInLine) {
+        TextBox_ box = (TextBox_)GetItem();
+        MultipleLineText_ value = box.GetMultipleLineText();
+        int i = value.CharacterIndexFromLineIndex(lineIndex) + indexInLine;
+        return box.IsBeginningOfToken(i);
     }
 }
