4dcbf4264787aa6bdf0ac6d9806490b46d46716a
Andreas Stefik <stefik@gmail.com>
2023-09-13 13:28:20 -0700

    Took an attempt at text field. It doesn't do what I expect, but maybe it's in the right direction.

--- Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/IOS/TextFieldIOS.java
+++ Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/IOS/TextFieldIOS.java
@@ -6,7 +6,7 @@ import org.robovm.apple.uikit.*;
 import quorum.Libraries.Interface.Controls.Button_;
 import quorum.Libraries.Interface.Controls.TextField_;
 
-public class TextFieldIOS extends ItemIOS implements UITextFieldDelegate {
+public class TextFieldIOS extends ItemIOS implements UIKeyInput, UITextInputTraits {
     public boolean isAccessibilityElement() {
         return true;
     }
@@ -22,68 +22,167 @@ public class TextFieldIOS extends ItemIOS implements UITextFieldDelegate {
     }
 
     @Override
-    public boolean shouldBeginEditing(UITextField textField) {
-        System.out.println("shouldBeginEditing");
-        return false;
+    public boolean canBecomeFirstResponder() {
+        return true;
     }
 
     @Override
-    public void didBeginEditing(UITextField textField) {
-        System.out.println("shouldBeginEditing");
+    public boolean canResignFirstResponder() {
+        return true;
+    }
+
+    public void Focus() {
+        System.out.println("Focus set to " + item.GetName());
+        becomeFirstResponder();
+    }
+
+    public void FocusLost() {
+        System.out.println("Lost focus from " + item.GetName());
+        resignFirstResponder();
     }
 
     @Override
-    public boolean shouldEndEditing(UITextField textField) {
-        System.out.println("shouldEndEditing");
-        return false;
+    public boolean hasText() {
+        return true;
     }
 
     @Override
-    public void didEndEditing(UITextField textField) {
-        System.out.println("didEndEditing");
+    public void insertText(String text) {
+        System.out.println(text);
     }
 
     @Override
-    public void didEndEditing(UITextField textField, UITextFieldDidEndEditingReason reason) {
-        System.out.println("didEndEditing");
+    public void deleteBackward() {
+        System.out.println("Delete backwards");
     }
 
     @Override
-    public boolean shouldChangeCharacters(UITextField textField, NSRange range, String string) {
-        System.out.println("shouldChangeCharacters");
-        return false;
+    public UITextAutocapitalizationType getAutocapitalizationType() {
+        return UITextAutocapitalizationType.None;
     }
 
     @Override
-    public void textFieldDidChangeSelection(UITextField textField) {
-        System.out.println("textFieldDidChangeSelection");
+    public void setAutocapitalizationType(UITextAutocapitalizationType v) {
+
     }
 
     @Override
-    public boolean shouldClear(UITextField textField) {
-        System.out.println("shouldClear");
-        return false;
+    public UITextAutocorrectionType getAutocorrectionType() {
+        return UITextAutocorrectionType.Default;
     }
 
     @Override
-    public boolean shouldReturn(UITextField textField) {
-        System.out.println("shouldReturn");
-        return false;
+    public void setAutocorrectionType(UITextAutocorrectionType v) {
+
     }
 
     @Override
-    public UIMenu getEditMenu(UITextField textField, NSRange range, NSArray<UIMenuElement> suggestedActions) {
-        System.out.println("getEditMenu");
+    public UITextSpellCheckingType getSpellCheckingType() {
+        return UITextSpellCheckingType.Default;
+    }
+
+    @Override
+    public void setSpellCheckingType(UITextSpellCheckingType v) {
+
+    }
+
+    @Override
+    public UITextSmartQuotesType getSmartQuotesType() {
         return null;
     }
 
     @Override
-    public void willPresentEditMenu(UITextField textField, UIEditMenuInteractionAnimating animator) {
-        System.out.println("willPresentEditMenu");
+    public void setSmartQuotesType(UITextSmartQuotesType v) {
+
     }
 
     @Override
-    public void willDismissEditMenu(UITextField textField, UIEditMenuInteractionAnimating animator) {
-        System.out.println("willDismissEditMenu");
+    public UITextSmartDashesType getSmartDashesType() {
+        return UITextSmartDashesType.Default;
+    }
+
+    @Override
+    public void setSmartDashesType(UITextSmartDashesType v) {
+
+    }
+
+    @Override
+    public UITextSmartInsertDeleteType getSmartInsertDeleteType() {
+        return UITextSmartInsertDeleteType.Default;
+    }
+
+    @Override
+    public void setSmartInsertDeleteType(UITextSmartInsertDeleteType v) {
+
+    }
+
+    @Override
+    public UIKeyboardType getKeyboardType() {
+        return UIKeyboardType.Default;
+    }
+
+    @Override
+    public void setKeyboardType(UIKeyboardType v) {
+
+    }
+
+    @Override
+    public UIKeyboardAppearance getKeyboardAppearance() {
+        return UIKeyboardAppearance.Default;
+    }
+
+    @Override
+    public void setKeyboardAppearance(UIKeyboardAppearance v) {
+
+    }
+
+    @Override
+    public UIReturnKeyType getReturnKeyType() {
+        return null;
+    }
+
+    @Override
+    public void setReturnKeyType(UIReturnKeyType v) {
+
+    }
+
+    @Override
+    public boolean enablesReturnKeyAutomatically() {
+        return false;
+    }
+
+    @Override
+    public void setEnablesReturnKeyAutomatically(boolean v) {
+
+    }
+
+    @Override
+    public boolean isSecureTextEntry() {
+        return false;
+    }
+
+    @Override
+    public void setSecureTextEntry(boolean v) {
+
+    }
+
+    @Override
+    public UITextContentType getTextContentType() {
+        return null;
+    }
+
+    @Override
+    public void setTextContentType(UITextContentType v) {
+
+    }
+
+    @Override
+    public UITextInputPasswordRules getPasswordRules() {
+        return null;
+    }
+
+    @Override
+    public void setPasswordRules(UITextInputPasswordRules v) {
+
     }
 }
