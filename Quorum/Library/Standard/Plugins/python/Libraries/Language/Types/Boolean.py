6149b3e2b70eb20525737cdb0db208d31606f790
Andreas Stefik <stefik@gmail.com>
2024-01-16 20:44:29 -0800

    Next

--- Quorum/Library/Standard/Plugins/python/Libraries/Language/Types/Boolean.py
+++ Quorum/Library/Standard/Plugins/python/Libraries/Language/Types/Boolean.py
@@ -1,3 +1,8 @@
+def Bool_(value):
+	val = quorum_Libraries_Language_Types_Boolean_()
+	val.SetValue__quorum_boolean(value)
+	return val
+
 class plugins_quorum_Libraries_Language_Types_Boolean_:
 	def __init__(self, value = False):
 		self.myBoolean = value
@@ -10,3 +15,4 @@ class plugins_quorum_Libraries_Language_Types_Boolean_:
 			return 1231
 		else:
 			return 1237
+
