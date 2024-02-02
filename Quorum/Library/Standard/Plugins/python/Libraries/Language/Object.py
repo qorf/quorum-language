52c863b1d841b52ba20d1eebeb4e7e9d3ec24a68
Andreas Stefik <stefik@gmail.com>
2024-02-02 11:18:38 -0800

    Fixed a series of issues for when compiling with micropython.

--- Quorum/Library/Standard/Plugins/python/Libraries/Language/Object.py
+++ Quorum/Library/Standard/Plugins/python/Libraries/Language/Object.py
@@ -20,6 +20,45 @@ def Global_Cast_Text_To_Boolean(value):
 	else:
 		raise Exception("Error: Cast Error: An error occurred while casting")
 		
+def Global_Cast_To_Text(value):
+	return str(value)
+	
+def Global_Cast_Text_To_Integer(value):
+	return int(value)
+	
+def Global_Cast_Text_To_Number(value):
+	return float(value)
+
+def Global_Cast_Boolean_To_Text(value):
+	if value == True:
+		return "true"
+	else:
+		return "false"
+
+def Global_Cast_Boolean_To_Number(value):
+	if value == True:
+		return float(1.0)
+	else:
+		return float(0.0)
+
+def Global_Cast_Boolean_To_Integer(value):
+	if value == True:
+		return int(1)
+	else:
+		return int(0)
+		
+def Global_Cast_Integer_To_Text(value):
+	return str(value)
+	
+def Global_Cast_Number_To_Text(value):
+	return str(value)
+	
+def Global_Cast_Integer_To_Number(value):
+	return float(value)
+	
+def Global_Cast_Number_To_Integer(value):
+	return int(value)
+		
 def  Global_GetValue_(value, type):
 	if (value == None):
 		if(type == "number" or type == "boolean" or type == "integer"):
