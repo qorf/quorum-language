f19807e3c3c7b19c489ff8c595c87ebb4bb2865b
Andreas Stefik <stefik@gmail.com>
2024-02-03 00:28:56 -0800

    Tons of fixes across the systems, but especially to integers.

--- Quorum/Library/Standard/Plugins/python/Libraries/Language/Types/Integer.py
+++ Quorum/Library/Standard/Plugins/python/Libraries/Language/Types/Integer.py
@@ -11,37 +11,64 @@ class plugins_quorum_Libraries_Language_Types_Integer_:
 		self.integer = value
 
 	def GetHashCode(self):
-		return self.integer
+		return hash(self.integer)
     
 	def BitCount(self):
-		return 32
+		return bin(self.integer).count("1")
 
 	def HighestOneBit(self):
-		return 0
+		if self.integer == 0:
+			return -1 
+
+		return 2 ** (self.integer.bit_length() - 1)
 		
 	def LowestOneBit(self):
-		return 0
+		if self.integer == 0:
+			return -1  # Special case for zero
+
+		position = 0
+		while self.integer & 1 == 0:
+			self.integer >>= 1
+			position += 1
+
+		return 2 ** position
 		
 	def LeadingZeros(self):
-		return 0;
+		return 32 - self.integer.bit_length()
 
 	def TrailingZeros(self):
-		return 0
+		if self.integer == 0:
+			return self.integer.bit_length()  # Special case for zero
+
+		count = 0
+		while self.integer & 1 == 0:
+			self.integer >>= 1
+			count += 1
+
+		return count
 
 	def Reverse(self):
-		return 0
+		binary = format(self.integer & 0xFFFFFFFF, '032b')
+		binary = binary[::-1]
+		if binary[0] == '1':
+			reversed = ''.join(['1' if bit == '0' else '0' for bit in binary])
+			result = -(int(reversed, 2) + 1)
+		else:
+			result = int(binary, 2)
+		return result
 
 	def RotateLeft__quorum_integer(self, value):
-		return 0
-        
+		return  self.integer << value
+		        
 	def RotateRight__quorum_integer(self, value):
-		return 0
-
+		bits = 32
+		return ((self.integer >> value) | (self.integer << (bits - value))) & ((1 << bits) - 1)
+		
 	def GetBinary(self):
-		return 0
+		return bin(self.integer)[2:]
 
 	def GetHex(self):
-		return 0
+		return hex(self.integer)
 		
 	def GetOctal(self):
-		return 0
+		return oct(self.integer)[2:]
