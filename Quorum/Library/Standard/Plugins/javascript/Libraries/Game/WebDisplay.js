5457e394a0d226027d98a6edfefbf2e4ba0f33d3
William Allee <wallee777@hotmail.com>
2024-06-28 16:59:30 -0700

    Added copy-to-clipboard support for Games in Javascript. Also added partial support for pasting from the clipboard, so it can paste text that was copied within the same Game.

--- Quorum/Library/Standard/Plugins/javascript/Libraries/Game/WebDisplay.js
+++ Quorum/Library/Standard/Plugins/javascript/Libraries/Game/WebDisplay.js
@@ -8,6 +8,11 @@ function plugins_quorum_Libraries_Game_WebDisplay_()
     var deltaTime = 0;
     var totalTime = 0;
     var renderingRequested = false;
+
+    // TBD: This needs to be addressed as part of getting the clipboard info!
+    // A temporary half-measure for clipboard access. Lets the user copy and paste within the same application, even
+    // if they can't paste text from outside the application.
+    var localClipboard = "";
     
     this.SetConfigurationNative$quorum_Libraries_Game_WebConfiguration = function(config)
     {
@@ -132,5 +137,16 @@ function plugins_quorum_Libraries_Game_WebDisplay_()
         else
             return false;
     };
+
+    this.GetClipboard = function()
+    {
+        return localClipboard;
+    }
+
+    this.SetClipboard$quorum_text = function(value)
+    {
+        localClipboard = value;
+        navigator.clipboard.writeText(value);
+    }
 }
 
