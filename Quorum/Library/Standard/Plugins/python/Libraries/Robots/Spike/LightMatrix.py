9dc983650e3f49e0a588ca2b960ac98999b5cd4b
Andreas Stefik <stefik@gmail.com>
2024-07-20 10:32:30 -0700

    Finished the light matrix.

--- Quorum/Library/Standard/Plugins/python/Libraries/Robots/Spike/LightMatrix.py
+++ Quorum/Library/Standard/Plugins/python/Libraries/Robots/Spike/LightMatrix.py
@@ -6,3 +6,15 @@ class plugins_quorum_Libraries_Robots_Spike_LightMatrix_:
 		
 	async def Write__quorum_text(self, value):
 		await light_matrix.write(value)
+
+	def GetPixelNative__quorum_integer__quorum_integer(self, x, y):
+		return light_matrix.get_pixel(x, y)
+	
+	def SetPixelNative__quorum_integer__quorum_integer__quorum_integer(self, x, y, intensity):
+		return light_matrix.set_pixel(x, y, intensity)
+	
+	def ShowImageNative__quorum_integer(self, image):
+		light_matrix.show_image(image)
+
+	def ClearNative(self):
+		light_matrix.clear()
