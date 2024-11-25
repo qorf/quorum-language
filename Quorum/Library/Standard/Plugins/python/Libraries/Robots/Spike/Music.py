49ee1b34f6b9d0821c07b647ca1df112b4675dfb
wyliez1 <wyliez1@unlv.nevada.edu>
2024-11-24 18:28:34 -0800

    Fixed music library

---
+++ Quorum/Library/Standard/Plugins/python/Libraries/Robots/Spike/Music.py
@@ -0,0 +1,11 @@
+from app import music
+
+class plugins_quorum_Libraries_Robots_Spike_Music_:
+    def __init__(self):
+        pass
+
+    def PlayDrumNative__quorum_integer(self, drum):
+        music.play_drum(drum)
+
+    def PlayInstrumentNative(self, instrument, note, duration):
+        music.play_instrument(instrument, note duration)
