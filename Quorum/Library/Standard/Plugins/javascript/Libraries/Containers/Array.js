65cc13b21a769930862c09fa729940707964b9be
Andreas Stefik <stefik@gmail.com>
2025-02-12 16:45:04 -0800

    Array Attempt.

--- Quorum/Library/Standard/Plugins/javascript/Libraries/Containers/Array.js
+++ Quorum/Library/Standard/Plugins/javascript/Libraries/Containers/Array.js
@@ -1,20 +1,22 @@
-function plugins_quorum_Libraries_Containers_Array_() {
-    this.array_ = [];
-    this.size = 0;
-    this.resizable = true;
-    this.DEFAULT_CAPACITY = 10;
-    this.capacity = this.DEFAULT_CAPACITY;
-    this.DEFAULT_RESIZE = 2;
-
-    this.SetNative$quorum_integer$quorum_Libraries_Language_Object = function (index, value) {
+class plugins_quorum_Libraries_Containers_Array_ {
+    constructor() {
+        this.array_ = [];
+        this.size = 0;
+        this.resizable = true;
+        this.DEFAULT_CAPACITY = 10;
+        this.capacity = this.DEFAULT_CAPACITY;
+        this.DEFAULT_RESIZE = 2;
+    }
+    
+    SetNative$quorum_integer$quorum_Libraries_Language_Object(index, value) {
         this.array_[index] = value;
     };
 
-    this.GetNative$quorum_integer = function (index) {
+    GetNative$quorum_integer(index) {
         return this.array_[index];
     };
 
-    this.SetSizeNative$quorum_integer = function (size) {
+    SetSizeNative$quorum_integer(size) {
         if(this.array_.length > size) {
             var array2_ = [];
             for(i = 0; i < size; i++) {
@@ -36,11 +38,11 @@ function plugins_quorum_Libraries_Containers_Array_() {
         this.capacity = size;
     };
 
-    this.SetSizeNoFillNative$quorum_integer = function (size) {
+    SetSizeNoFillNative$quorum_integer(size) {
         this.SetSizeNative$quorum_integer(size); //should this be the same in JS mode?
     };
     
-    this.AddNative$quorum_integer$quorum_Libraries_Language_Object = function (index, value) {
+    AddNative$quorum_integer$quorum_Libraries_Language_Object(index, value) {
         if(this.size + 1 >= this.capacity && this.resizable) {
             this.capacity = this.capacity * this.DEFAULT_RESIZE;
         } else if(this.size + 1 >= this.capacity && !this.resizable) {
@@ -50,7 +52,7 @@ function plugins_quorum_Libraries_Containers_Array_() {
         this.array_.splice(index, 0, value);
     };
 
-    this.AddNative$quorum_Libraries_Language_Object = function (value) {
+    AddNative$quorum_Libraries_Language_Object(value) {
         if(this.size >= this.capacity && this.resizable) {
             this.capacity = this.capacity * this.DEFAULT_RESIZE;
         }
@@ -63,18 +65,18 @@ function plugins_quorum_Libraries_Containers_Array_() {
         this.size = this.size + 1;
     };
 
-    this.RemoveAtNative$quorum_integer = function (index) {
+    RemoveAtNative$quorum_integer(index) {
         var value = this.array_.splice(index, 1);
         value = value[0];
         this.size = this.size - 1;
         return value;
     };
 
-    this.GetMaxSize = function () {
+    GetMaxSize() {
         return this.capacity;
     };
 
-    this.SetMaxSize$quorum_integer = function (size) {
+    SetMaxSize$quorum_integer(size) {
         if(this.capacity > size) {
             var array2_ = [];
             if(this.size > size) {
@@ -90,35 +92,35 @@ function plugins_quorum_Libraries_Containers_Array_() {
         }
     };
 
-    this.GetSize = function () {
+    GetSize() {
         return this.size;
     };
 
-    this.GetAutoResize = function () {
+    GetAutoResize() {
         return this.resizable;
     };
 
-    this.SetAutoResize$quorum_boolean = function (resize) {
+    SetAutoResize$quorum_boolean(resize) {
         this.resizable = resize;
     };
 
-    this.IsEmpty = function () {
+    IsEmpty = function () {
         return this.size == 0;
     };
 
-    this.ClearContents$quorum_integer$quorum_integer = function (start, stop)
+    ClearContents$quorum_integer$quorum_integer(start, stop)
     {
         for (var i = start; i < stop; i++) {
             this.array_[i] = null;
         }
     };
 
-    this.Empty = function () {
+    Empty() {
         this.array_.length = 0;
         this.size = 0;
     };
     
-    this.Empty$quorum_boolean = function (value) {
+    Empty$quorum_boolean(value) {
         if(value) {
             Empty();
         } else {
@@ -127,74 +129,80 @@ function plugins_quorum_Libraries_Containers_Array_() {
     };
 };
 
-function plugins_quorum_Libraries_Containers_Number32BitArray_() {
-    this.array_ = null;
+class plugins_quorum_Libraries_Containers_Number32BitArray_ {
+    constructor() {
+        this.array_ = null;
+    }
 
-    this.Get$quorum_integer = function(index)
+    Get$quorum_integer(index)
     {
         return this.array_[index];
     };
 
-    this.Set$quorum_integer$quorum_number = function(index, value) 
+    Set$quorum_integer$quorum_number(index, value) 
     {
         this.array_[index] = value;
     };
 
-    this.GetSize = function()
+    GetSize()
     {
         return this.array_.length;
     };
     
-    this.SetSize$quorum_integer = function(size)
+    SetSize$quorum_integer(size)
     {
         this.array_ = new Float32Array(size);
     };
 
 };
 
-function plugins_quorum_Libraries_Containers_Number64BitArray_() {
-    this.array_ = null;
+class plugins_quorum_Libraries_Containers_Number64BitArray_ {
+    constructor() {
+        this.array_ = null;
+    }
 
-    this.Get$quorum_integer = function(index)
+    Get$quorum_integer(index)
     {
         return this.array_[index];
     };
 
-    this.Set$quorum_integer$quorum_number = function(index, value) 
+    Set$quorum_integer$quorum_number(index, value) 
     {
         this.array_[index] = value;
     };
 
-    this.GetSize = function()
+    GetSize()
     {
         return this.array_.length;
     };
     
-    this.SetSize$quorum_integer = function(size)
+    SetSize$quorum_integer(size)
     {
         this.array_ = new Float64Array(size);
     };
 };
 
-function plugins_quorum_Libraries_Containers_Integer32BitArray_() {
-    this.array_ = null;
+class plugins_quorum_Libraries_Containers_Integer32BitArray_ {
+    constructor() {
+        this.array_ = null;
+    }
 
-    this.Get$quorum_integer = function(index)
+    Get$quorum_integer(index)
     {
         return this.array_[index];
     };
 
-    this.Set$quorum_integer$quorum_integer = function(index, value) 
+    Set$quorum_integer$quorum_integer(index, value) 
     {
         this.array_[index] = value;
     };
 
-    this.GetSize = function()
+    GetSize()
     {
         return this.array_.length;
     };
     
-    this.SetSize$quorum_integer = function(size)
+    SetSize$quorum_integer(size)
     {
         this.array_ = new Int32Array(size);
     };
