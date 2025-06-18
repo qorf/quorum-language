dffcc0bd6877cec693be6e56cbda17e9c5425bde
andreas-stefik <30560627+andreas-stefik@users.noreply.github.com>
2025-06-18 13:59:13 -0700

    Proof of concept for Vex Robotics.

---
+++ Quorum/Library/Standard/Plugins/python/Libraries/Robots/Vex/Aim/Robot.py
@@ -0,0 +1,9 @@
+from vex import *
+from vex_globals import *
+
+class plugins_quorum_Libraries_Robots_Vex_Aim_Robot_:
+	def __init__(self):
+		robot=Robot()
+	
+	def MoveFor__quorum_number__quorum_number(self, distance, angle):
+		robot.move_for(distance, angle)
