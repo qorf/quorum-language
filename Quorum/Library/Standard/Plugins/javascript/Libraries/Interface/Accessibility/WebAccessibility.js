c02196f15bbf0b572200b01d65797d95db32876c
William Allee <wallee777@hotmail.com>
2025-07-11 14:46:27 -0700

    Updated web Quorum apps to use role=application, thereby solving all problems in computer science forever (probably).

--- Quorum/Library/Standard/Plugins/javascript/Libraries/Interface/Accessibility/WebAccessibility.js
+++ Quorum/Library/Standard/Plugins/javascript/Libraries/Interface/Accessibility/WebAccessibility.js
@@ -95,7 +95,7 @@ function plugins_quorum_Libraries_Interface_Accessibility_WebAccessibility_(acce
         root = document.createElement("div");
         root.setAttribute("aria-label", title);
         root.setAttribute("tabindex", "-1");
-        root.setAttribute("role", "dialog");
+        root.setAttribute("role", "application");
         root.setAttribute("aria-modal", true);
         root.hidden = true;
 
