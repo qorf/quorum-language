6aa3d8f5ea723eff1fe9b6affc2ce8bd870310a6
andreas-stefik <30560627+andreas-stefik@users.noreply.github.com>
2025-08-27 13:15:00 -0700

    Update to Toggle Button for the new version.

--- Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/accesskit/ToggleButtonKit.java
+++ Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/accesskit/ToggleButtonKit.java
@@ -1,9 +1,35 @@
 package plugins.quorum.Libraries.Interface.Accessibility.accesskit;
 
+import dev.accesskit.Node;
+import dev.accesskit.Rect;
 import dev.accesskit.Role;
+import dev.accesskit.Toggled;
+import quorum.Libraries.Interface.Controls.Checkbox_;
+import quorum.Libraries.Interface.Item_;
 
 public class ToggleButtonKit extends ItemKit{
     public ToggleButtonKit() {
         SetRole(Role.BUTTON);
+    }
+
+    public Node Build() {
+        Item_ item = GetItem();
+        if(item != null) {
+            Rect rect = GetBoundingRectangle();
+            Node builder = new Node(GetRole());
+            builder.setBounds(rect);
+            builder.setLabel(item.GetName());
+            if(item instanceof Checkbox_) { //technically not compiler guaranteed. You can set the code to anything.
+                Checkbox_ box = (Checkbox_) item;
+                boolean state = box.GetToggleState();
+                if(state) {
+                    builder.setToggled(Toggled.TRUE);
+                } else {
+                    builder.setToggled(Toggled.FALSE);
+                }
+            }
+            return builder;
+        }
+        return null;
     }
 }
